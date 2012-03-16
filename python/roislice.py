371dd935bf94307fc2f93c4ee818a0691469f214
Randal Burns <randal@cs.jhu.edu>
2012-03-16 17:12:50 -0400

    Changes on rio to be pushed to tjb.

--- python/roislice.py
+++ python/roislice.py
@@ -9,28 +9,25 @@ import roi
 #  Draw an ROI XY slice using matplotlib.pyplot as a pcolor graph
 #
 
-#
-# main
-#
 def main ():
 
   parser = argparse.ArgumentParser(description='Draw the ROI map of a brain.')
   parser.add_argument('roixmlfile', action="store")
   parser.add_argument('roirawfile', action="store")
-  # RBTODO how to make 0,1,2 the legal values
-  parser.add_argument('--dimensions', action="store", default="xy")
+  parser.add_argument('--dimensions', action="store", default="xy", help="xy, xz, or yz")
   parser.add_argument('slice', action="store")
 
   result = parser.parse_args()
 
+  # Read the XML file and then the data
   try:
     roix = roi.ROIXML( result.roixmlfile )
     rois = roi.ROIData ( result.roirawfile, roix.getShape() ) 
   except:
-    # RBTODO give a meaningful error 
     print "Failed to parse ROI files at: ", result.roixmlfile, result.roixmlfile
     assert 0
 
+  # cut out the specified slice
   if result.dimensions == "xy":
     matplotlib.pyplot.pcolor ( rois.data[:,:,result.slice] )
   elif result.dimensions == "yz":
@@ -38,9 +35,10 @@ def main ():
   elif result.dimensions == "xz":
     matplotlib.pyplot.pcolor ( rois.data[:,result.slice,:] )
   else:
-    print "Choose xy, xz, or yz as dimensino or leave blank for xy default"
+    print "Choose xy, xz, or yz as dimension or leave blank for xy default"
 
 
+  # display until key press
   raw_input("Press Enter to continue...")
 
 if __name__ == "__main__":
