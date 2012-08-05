009ab3da470dd81164daf51c808fc7586012ed00
Disa Mhembere <disa@jhu.edu>
2012-08-04 23:20:00 -0400

    Programmatic & multi-scan dataset check-in
    Former-commit-id: 7dd6dc8be8d6d0c6f2a34aac00e44675b6dc8e5d

--- MROCPdjangoForm/ocpipeline/urls.py
+++ MROCPdjangoForm/ocpipeline/urls.py
@@ -4,13 +4,15 @@ urls patterns for one-click MR-connectome pipeline
 '''
 
 from django.conf.urls import patterns, include, url
-from ocpipeline.views import hello
+from ocpipeline.views import createProj
 from ocpipeline.views import pipelineUpload
 from ocpipeline.views import success
 from ocpipeline.views import zipProcessedData
 from ocpipeline.views import processInputData
 from ocpipeline.views import confirmDownload
+from ocpipeline.views import upload
 
+#from django.conf.urls.defaults import * # deletable
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,14 +20,18 @@ admin.autodiscover()
 
 urlpatterns = patterns('ocpipeline.views',
     url(r'^$', 'default', name= 'welcome'),
-    url(r'^create/$', 'hello', name= 'form-create-project'),
-    url(r'^create/([a-zA-Z+#-.0-9]+/){5}$','hello', name='prog-create-project'),  # No spaces yet...
+    url(r'^create/$', 'createProj', name= 'form-create-project'),
+    url(r'^create/([a-zA-Z+#-.0-9]+/){5}$','createProj', name='prog-create-project'),  # No spaces yet...
     url(r'^pipelineUpload/$', 'pipelineUpload', name='single-file-upload'),
     url(r'^pipelineUpload/(.)+$', 'pipelineUpload', name='single-file-upload'),
     url(r'^success/$', 'success', name='success-page'),
     url(r'^confirmDownload/$', 'confirmDownload', name='confirm-dwnd-page'),
     url(r'^processInput/$', 'processInputData', name='process-input-data'),
-    url(r'^zipOutput/$','zipProcessedData', name = 'zip-processed-data'), # 2nd param function in view, 3rd param - anything
+    url(r'^zipOutput/$','zipProcessedData', name = 'zip-processed-data'), # 2nd param function is view, 3rd param - anything
+    url(r'^multiFileProcess/','multiFileProcess',name = 'process-multi-file'),
+    url(r'^multiUpload(.+$)', 'multiUpload', name ='multi-upload'),
+    #url(r'upload/$', upload, name='uploadify_upload'), # Uploadify
+    #(r'^uploadify/', include('uploadify.urls')),
     # Examples
     # url(r'^$', 'myapp.views.home', name='home'),
     # url(r'^myapp/', include('myapp.foo.urls')),
