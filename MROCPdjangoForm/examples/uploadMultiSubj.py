86d75ee1719bbc8e421f59b29e11ebfbd86a432f
Disa Mhembere <disa@jhu.edu>
2012-09-11 15:11:50 -0400

    Full Check-in
    Former-commit-id: 60e3100f093c330da457d644ec945a27f4daa65a [formerly f1e8290ea0975824bde24bf67b5c14d146d6045a] [formerly 7371aa5eda88ed94f5f84345bf5141ff3a4cf07d [formerly 7ddaa78ef2c250dd81992efb5dc967a5ce622d0b]]
    Former-commit-id: 3139c997d096340a97c442600e3e0a3239d50db1
    Former-commit-id: bc64a9f914bcad7ccfad53d273446b5f3326caa2

---
+++ MROCPdjangoForm/examples/uploadMultiSubj.py
@@ -0,0 +1,91 @@
+import urllib2
+import argparse
+import sys
+import os
+
+import zipfile
+import tempfile
+
+import webbrowser
+
+def getFiberID(fiberfn):
+    '''
+    Assumptions about the data made here as far as file naming conventions
+    '''
+    if fiberfn.endswith('/'):
+      fiberfn = fiberfn[:-1] # get rid of trailing slash
+    return fiberfn.split('/')[-1][:-9]
+
+def main():
+
+  parser = argparse.ArgumentParser(description='Upload a mulitple subjects to MROCP via \
+                                   a single dir that must match bg1/MRN')
+  parser.add_argument('url', action="store", help='url must be in the form: upload/{project}/{site}/{subject}/{session}/yes|no, \
+                where the last param is either \'yes\' or \'no\', but not both. \'yes\' adds .mat files for graph invariant result & .csv for graphs, \'no\' does not   ')
+  parser.add_argument('dataDir', action="store", help ='Data directory must have a fiber\
+                      subdirectory with tract files & an roi subdirectory with rois')
+  
+  result = parser.parse_args()
+    
+  fiberDir = os.path.join(result.dataDir, 'fiber')
+  roiDir = os.path.join(result.dataDir, 'roi')
+  
+  for fiber_fn in os.listdir(fiberDir):
+    fiber_fn = os.path.join(fiberDir,fiber_fn)
+    
+    if not (os.path.exists(fiber_fn)):
+      print "Either fiber_fn not found!"
+      sys.exit(0)
+      
+    roi_xml_fn = os.path.join(roiDir, getFiberID(fiber_fn) + 'roi.xml')
+    roi_raw_fn = os.path.join(roiDir, getFiberID(fiber_fn) + 'roi.raw')
+      
+    if not (os.path.exists(roi_xml_fn) or os.path.exists(roi_raw_fn)):
+      print "Rois not found!"
+      sys.exit(0)
+    
+    # Create a temporary file to store zip contents in memory
+    tmpfile = tempfile.NamedTemporaryFile()
+    zfile = zipfile.ZipFile ( tmpfile.name, "w" )
+
+    zfile.write(fiber_fn)
+    zfile.write(roi_xml_fn)
+    zfile.write(roi_raw_fn)
+    zfile.close()
+
+    tmpfile.flush()
+    tmpfile.seek(0)
+
+#  Testing only: check the contents of a zip file.
+#
+#    rzfile = zipfile.ZipFile ( tmpfile.name, "r" )
+#    ret = rzfile.printdir()
+#    ret = rzfile.testzip()
+#    ret = rzfile.namelist()
+#    import pdb; pdb.set_trace()
+    
+    result.url = result.url if result.url.endswith('/') else result.url + '/' # 
+    
+    try:
+      req = urllib2.Request ( result.url + getFiberID(fiber_fn)[:-1], tmpfile.read() )  # concatenate project with assigned scanID & call url
+      response = urllib2.urlopen(req)
+    except urllib2.URLError, e:
+      print "Failed URL", result.url
+      print "Error %s" % (e.read()) 
+      sys.exit(0)
+
+    the_page = response.read()
+    print "Success with id %s" % the_page
+  
+  ''' Open up a tab in your browser to view results'''
+  redir = '/'
+  for Dir in result.url.split('/')[-4:]:
+    if (Dir != result.url.split('/')[-1]):
+      redir += Dir + '/'
+    else:
+      redir += Dir
+  
+  webbrowser.open('http://www.openconnecto.me/data/projects/disa/OCPprojects' + redir)
+
+if __name__ == "__main__":
+  main()
---
+++ MROCPdjangoForm/ocpipeline/zipper.py
@@ -0,0 +1,60 @@
+'''
+@author : Disa Mhembere
+Create a ZIP file on disk and transmit it in chunks of 8KB,                 
+    without loading the whole file into memory.                            
+'''
+
+import os
+import tempfile, zipfile
+import argparse
+
+def zipFilesFromFolders(dirName = None, multiTuple = []):
+    '''
+    dirName - any folder
+    '''
+    temp = tempfile.TemporaryFile()
+    myzip = zipfile.ZipFile(temp ,'w', zipfile.ZIP_DEFLATED)
+    
+    
+    if (multiTuple):
+        for dirName in multiTuple:
+            if dirName[0] != '.': # ignore metadata
+                dirName = os.path.join(multiTuple, dirName)
+                filesInOutputDir = os.listdir(dirName)
+                
+                for thefolder in filesInOutputDir:
+                    if thefolder[0] != '.': # ignore metadata
+                        dataProdDir = os.path.join(dirName, thefolder)
+                        for thefile in os.listdir(dataProdDir):
+                            filename =  os.path.join(dataProdDir, thefile)
+                            myzip.write(filename, thefile) # second param of write determines name output
+                            print "Compressing: " + thefile
+        myzip.close()
+        return temp
+        
+    
+    filesInOutputDir = os.listdir(dirName)
+    
+    for thefolder in filesInOutputDir:
+        if thefolder[0] != '.': # ignore metadata
+            dataProdDir = os.path.join(dirName, thefolder)
+            for thefile in os.listdir(dataProdDir):
+                filename =  os.path.join(dataProdDir, thefile)
+                myzip.write(filename, thefile) # second param of write determines name output
+                print "Compressing: " + thefile
+
+    myzip.close()
+    return temp
+
+def main():
+    
+    parser = argparse.ArgumentParser(description='Zip the contents of an entire directory & place contents in single zip File')
+    parser.add_argument('dirName', action='store')
+    parser.add_argument('--multiTuple', action='store')
+    
+    result = parser.parse_args()
+    
+    zipFilesFromFolders(result.dirName, result.multiTuple)
+    
+if __name__ == '__main__':
+    main()
