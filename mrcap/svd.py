78e5041fd0469ba07032c7e691f846e87cf57770
Disa Mhembere <disa@jhu.edu>
2012-09-22 02:14:54 -0400

    single value decomposition
    Former-commit-id: 56f2d32c378025c772bb47d8ff1fdc6a6feb228c [formerly 847c34d75afd3e77487c3b9ca0c82d703ba771d5] [formerly 96dd88f9099735c402cefa39b433009ae1004202 [formerly ad9fd1884682a26ff85950abe17cb770ea5d13e9]]
    Former-commit-id: 6e72583bc0dbd0e3ba42c7ac26f49741d89e69ce
    Former-commit-id: 5b9e663aebbddab6197f01c6531f2356b0909891

---
+++ mrcap/svd.py
@@ -0,0 +1,48 @@
+'''
+@author: dmhembe1, dsussman
+
+Run SVD on a fibergraph. This is a wrapper for Embed.py and uses lcc.py
+Uses Embed.py found at: https://github.com/jovo/PyGraphStat/blob/master/code/Embed.py
+'''
+
+import lcc
+import Embed
+import numpy as np
+import argparse
+
+def embed_graph(ccfn, roiRootName, fgfn, embedfn, dim=10):
+    """
+    ccfn - connected components file with numpy arrray
+    roiRootName - roi files root name
+    fgfn - mat file with the fibergraph
+    embedfn - full file name of output file to be saved
+    dim - desired dimension for the embedding
+    """
+    
+    vcc = lcc.ConnectedComponent(fn =ccfn)
+    # Load the fibergraph
+    fg = lcc._load_fibergraph(roiRootName, fgfn) 
+    # Now get the subgraph for the lcc, binarize and symmetrize
+    G = vcc.induced_subgraph(fg.spcscmat)
+    G.data = np.ones_like(G.data) # Binarize
+    G = G+G.T # Symmetrize
+
+    e = Embed.Embed(dim, matrix=Embed.self_matrix)
+    np.save(embedfn, e.embed(G).get_scaled())
+
+def main():
+    
+    parser = argparse.ArgumentParser(description='Use the largest connected components to run SVD on a graph')
+    parser.add_argument('ccfn', action="store")
+    parser.add_argument('roiRootName', action="store")
+    parser.add_argument('fgfn', action = 'store')
+    parser.add_argument('embedfn', action = 'store')
+    
+    parser.add_argument('--dim', action="store", type=int, default=10)
+    result = parser.parse_args()
+    
+    embed_graph(result.ccfn, result.roiRootName, result.fgfn, result.embedfn, result.dim)
+    
+if __name__ == '__main__':  
+    main()
+ 
