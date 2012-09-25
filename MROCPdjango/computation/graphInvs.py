b23217245f61881add65df7c0c0892e7c7a0d646
Disa Mhembere <disa@jhu.edu>
2012-09-25 04:13:32 -0400

    Num traingles
    Former-commit-id: e0908dc9dc85b4e8695752852e10d25da4351884 [formerly 86cc5090c41f09560829413292bb5123aaca8f38] [formerly 11a68ebb021cfb7f590ba54ea17a6c1ac5ac432a [formerly 43300850c8042e86290b08a0a7fa0dd74aecc81e]]
    Former-commit-id: 33e546b7b25060778602e354226ae4856d25c0a5
    Former-commit-id: 8166a3362966f9abac430f60e3526db2a52a9151

--- MROCPdjango/computation/graphInvs.py
+++ MROCPdjango/computation/graphInvs.py
@@ -10,6 +10,8 @@ import scipy.io as sio
 import numpy as np
 import os
 import argparse
+from ocpipeline.views import getFiberPath
+
 
 import scipy.sparse.linalg.eigen.arpack as arpack
 from collections import Counter
@@ -81,16 +83,19 @@ class graph():
     '''
     Calculate several graph invariants
     1. The maximum average degree (MAD) of the graph
-    2. 
+    2. The degree of each vertex
+    3. The Scan Statistic 1
+    4. 
     '''
-    self.vertices = np.zeros(self.G_sparse.shape[0])
     
-    # Create vertex list
-    self.vertices = []
-    for v in range(self.G_sparse.shape[0]):
-      self.vertices.append(vertex(v))
+    #self.vertices = np.zeros(self.G_sparse.shape[0])
+    
+    #Create vertex list
+    #self.vertices = []
+    #for v in range(self.G_sparse.shape[0]):
+     # self.vertices.append(vertex(v))
       
-    self.vertices = np.array(self.vertices) # make list a numpy array
+    #self.vertices = np.array(self.vertices) # make list a numpy array
     
     ############ Graph Invariant calcs ############
     ''' Calc Maximum Average Degree of the graph'''
@@ -101,8 +106,8 @@ class graph():
     print 'Calculating scan statistic 1...'
     self.calcScanStat()
     
-    self.printVertDegrees() # print all degrees
-    self.printVertSS1() # print SS1
+    #self.printVertDegrees() # print all degrees
+    #self.printVertSS1() # print SS1
     
     ''' Calc scan statistic 2'''
     
@@ -123,35 +128,19 @@ class graph():
     self.maxAveDeg = (np.max(arpack.eigs(self.G_sparse, which='LR')[0])).real # get eigenvalues, then +ve max REAL part is MAD eigenvalue estimation
     print "\n Calculating MAD took: ", (clock() - start), "secs"
   
-  ####################
-  # INDUCED SUBGRAPH #
-  ####################
-  def calcScanStat(self, N=1):
+  def getAveDegree(self, vertDegArr):
     '''
-    Determine scan statistic of neighborhood n = N
-    '''  
-    for vertx in self.vertices: 
-    
-      # Count edges
-      col = vertx.ind; # index corresponds to vertex number
-      vertx.indSubgrEdgeNum = self.G_sparse.indptr[col+1] - self.G_sparse.indptr[col] # wheel vertex edges
+    Calculate the Average Degree of a vertex  
+    Returns:
+    =========
+    The Average degree of a vertex
+    '''
+    return np.average(vertDegreArr)
       
-      #import pdb; pdb.set_trace()
-      # This happens to also be the vertex degree
-      vertx.degree = self.G_sparse.indptr[col+1] - self.G_sparse.indptr[col] # wheel vertex edges
-      
-      adj = self.G_sparse.indices[self.G_sparse.indptr[col]:self.G_sparse.indptr[col+1]] # Rows of nnz entries - corresponding to vertices
-      '''
-      Count the list of all matches
-      Going to adj[idx:] ensures no double counting
-      '''
-      for idx, v in enumerate (adj):
-	vertx.indSubgrEdgeNum += len(list( (Counter(self.G_sparse.indices[self.G_sparse.indptr[v]:\
-					    self.G_sparse.indptr[v+1]]) & Counter(adj[idx:])).elements()))
-
-      # Write all edge numbers to file then del
-      
-	
+  ####################
+  # TODO DM: FIX these #
+  ####################
+  
   def printVertSS1(self):
     '''
     Print the Scan statistic 1
@@ -167,7 +156,7 @@ class graph():
     print '\nVertx Degrees \n=======================\n'
     for idx, vert in enumerate (self.vertices):
       print 'Vertex: %d, Degree 1: %d' % (vert.ind, vert.degree)
-	
+  
   
   def calcNumTriangles(self):
     '''
@@ -186,7 +175,68 @@ class graph():
     Path length between each pair of vertices
     '''
     pass
- 
+
+###################
+# SCAN STATISTICS #
+###################
+  
+def calcScanStat(self, G, G_fn='',N=1):
+  '''
+  G - Sparse adjacency matrix (rep a graph)
+  N - Scan statistic number i.e 1 or 2 ONLY
+  '''
+  if (N == 2):
+    G = G.dot(G)+G
+  
+  vertDeg = np.zeros(G.shape[0]) # Vertex degrees of all vertices
+  indSubgrEdgeNum = np.zeros(G.shape[0]) # Induced subgraph edge number i.e scan statistic
+  
+  for vertx in range (G.shape[0]):
+    nbors = G[:,vertx].nonzero()[0]
+    vertxDeg[vertx] = nbors.shape[0] # degree of each vertex
+    nborsAdjMat = G[:,nbors][nbors,:]
+    indSubgrEdgeNum[vertx] = nborsAdjMat.nnz # scan stat 1
+  
+  '''write to file '''
+  if not (G_fn):
+    np.save(getFiberPath(G_fn) + 'scanstat'+N+'.npy', indSubgrEdgeNum) # save location wrong!
+    np.save(getFiberPath(G_fn) + 'degree'+N+'.npy', vertxDeg)  # save location wrong!
+  else:
+    np.save('scanstat'+N+'.npy', indSubgrEdgeNum)
+    np.save('degree'+N+'.npy', vertxDeg)
+    
+  del vertxDeg, indSubgrEdgeNum
+
+def calcNumTriangles(ss1Array, degArray, G_fn = ''):
+  if not isinstance(ss1Array, np.ndarray):
+    try:
+      ss1Array = np.load(ss1Array)
+    except:
+      print "ERROR: File not found: %s OR not valid format" % ss1Array
+      sys.exit(-1)
+  
+  if not isinstance(degArray, np.ndarray):
+    try:
+      degArray = np.load(degArray)
+    except:
+      print "File not found: %s" % degArray
+      sys.exit(-1)
+  
+  if (len(degArray) == len(ss1Array)):
+    print "Array lengths unequal"
+    sys.exit(-1)
+  
+  triangles = np.zeros(len(degArray))
+  for idx in range (degArray):
+    numTri = ss1Array[idx] - degArray[idx]
+    if numTri < 0:
+      print 'There is an error in index %d. This num cannot be negative' % idx
+      
+    triangles[idx] = numTri
+    
+  np.save('triangles.npy', triangles)  # save location wrong!
+  
+    
 ##########################
   # Vertex Class #
 ##########################
@@ -200,52 +250,20 @@ class vertex():
     self.ind = ind
     self.indSubgrEdgeNum = 0  # induced subgraph
     
-  # Unused currently
-  def getAveDegree(self):
-    '''
-    Calculate the Average Degree of a vertex
-    
-    Returns:
-    =========
-    The Average degree of a vertex
-    '''
-    order = self.degree + 1 # Order of induced sub-graph of vertex i.e vertex cardinality
-    
-    if (self.indSubgrSize > 0):
-      self.aveDeg = (2*self.indSubgrSize)/self.order # Average Degree of graph eq.2.3 p.4
-    # else it remains zero
-    return self.aveDeg
-
-##########################
-  # Edge Class #
-##########################
-
-class edge():
-  
-  def __init__(self):
-    pass
-
 
 def main():
   gr = graph()
-  gr.loadgraphMatx('/Users/dmhembere44/Downloads/Scan/M87102217_smgr.mat', big = False, sym = True, binarize = True)
+  #gr.loadgraphMatx('/Users/dmhembere44/Downloads/Scan/M87102217_smgr.mat', big = False, sym = True, binarize = True)
   #gr.loadgraphMatx('/Users/dmhembere44/Downloads/M87199728_fiber.mat', True, True, True)
-  #gr.loadgraphMatx('/data/projects/MRN/graphs/biggraphs/M87199728_fiber.mat', True, True, False) # server
+  gr.loadgraphMatx('/data/projects/MRN/graphs/biggraphs/M87199728_fiber.mat', True, True, False) # bg1
   gr.calcGraphInv()
   
   #import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
   main()
+
 '''
-neigh = G [:,v].nonzero()[0]
-An = G[:,neigh][neigh,:]
-s1 =  An.sum()
 
-G2 = G.dot(G)+G
-
-Forget scan 2
-
-triange(v) = scan1 - degree
 cc(v) = triangle(v)/(degree choose 2)
 '''
