1ef43f12eddecd81b537dc4c65dd8ad8d82c25fd
Disa Mhembere <disa@jhu.edu>
2012-10-17 14:53:07 -0400

    baseline
    Former-commit-id: 07951ea4e1ce5f036deb479db5a7b9c9c9f3e154 [formerly 19d4f1986c8b6d0b7314f994227e22a87c8c423c] [formerly 72251d69b8f7fb533e0ffe43597f546429c19dbd [formerly 990c7391afda1452cda4bdd90c7a696c862eedc8]]
    Former-commit-id: ef0bde6a271b3798c44b96aa57c0a9ccee204167
    Former-commit-id: a17bcdcea4dfd8b55a274b7544499cc9b0a297af

--- MROCPdjango/computation/testgr.py
+++ MROCPdjango/computation/testgr.py
@@ -1,11 +1,15 @@
 import networkx as nx
 import numpy as np
+import scipy.io as sio
+from scipy.sparse import *
+from scipy import *
+import os
 
 from utilhltcoe import evaluate_graph
 
-def createGraph():
+def createGraph(numNodes = 10):
   
-  numNodes  = 10
+  G_fn = "bench.lcc"+str(numNodes)
   nodeProb = 0.65
   randGr = nx.binomial_graph(numNodes,nodeProb,directed=False)
   
@@ -14,18 +18,18 @@ def createGraph():
   for row in range (len(randGr.adj.items())):
     for ind in ((randGr.adj.items()[row])[1]).keys():
       adjMat[row][ind] = 1
-      
-
-  print "Sanity Check \n============================="
-  print "Size/Number of edges =", randGr.number_of_edges()
-
+  
+  # Make equivalent csc_matrix
+  Gsparse = csc_matrix(adjMat, dtype=float64)
+  sio.savemat(os.path.join("bench",G_fn),{'fibergraph': Gsparse}, appendmat = True)
+  #Gdense = (sio.loadmat(G_fn)['fibergraph']).todense()
+  
   inv = evaluate_graph(randGr, -1)
   printUTIL(inv)
   writeUTIL(inv)
   
-  #print "My calcs"
 def printUTIL(inv):
-  print "\n\n utilhltcoe.py \n==========================="
+  print "\n utilhltcoe.py \n==========================="
   print "Size/Number of edges =", inv[0]
   print "Max degree =", inv[1]
   print "MAD Eigenvalue=", inv[2]
@@ -33,21 +37,22 @@ def printUTIL(inv):
   print "Num triangles", inv[4]
   print "Clustering Coeff", inv [5]
   print "Average Path Length", inv[6]
-  print "Greedy MAD", inv[7]
+  print "Degree = ", inv[7]
+  print "Greedy MAD", inv[8], "\n"
   
 def writeUTIL(inv):
   
-  f = open("benchmark.graph",'w')
-  
+  f = open(os.path.join("bench","bench.lcc.meta"+str(inv[7])),'w')
   f.write("utilhltcoe.py \n===========================\n")
   f.write("\nSize/Number of edges = "+str(inv[0])+"\nMax degree = "+str(inv[1]) )
   f.write("\nMAD Eigenvalue = "+str(inv[2])+"\nScan Stat1 = "+str(inv[3]))
   f.write("\nScan Stat1 = "+str(inv[3]) + "\nNum triangles = "+str(inv[4]))
   f.write("\nClustering Coeff = "+str(inv[5])+"\nAverage Path Length = "+str(inv[6]))
-  f.write("\nGreedy MAD= "+str(inv[7]))
+  f.write("\nDegree = "+str(inv[7]) + "\nGreedy MAD= "+str(inv[8]))
   
   f.close()
 
 if __name__ == '__main__':
-  createGraph()
-  
+  createGraph(10)
+  createGraph(50)
+  createGraph(100)
