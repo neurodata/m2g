05f99f410ea417d9f2e5aa7e9b2510d443bb98ad
Disa Mhembere <disa@jhu.edu>
2012-10-23 14:04:47 -0400

    Programmatic all fixed
    Former-commit-id: 600d821a6ee4cbba19331ad2222a82459e4d2b5d [formerly 32ab32009b229fdb4d3308f9775d8137ad117af5] [formerly aacfce60ca84cf65d3fc8f01897fa4fe16c4bf3c [formerly f9d7797f406771c571fd1896451d1f1b8ce48149]]
    Former-commit-id: 70fa3c77c17a2426c16421b334cafe69e3597c80
    Former-commit-id: 275edaf0ad3ade60414d65656ed19ab9554ebd65

--- MROCPdjango/examples/uploadMultiSubj.py
+++ MROCPdjango/examples/uploadMultiSubj.py
@@ -19,8 +19,8 @@ def getFiberID(fiberfn):
 def main():
 
   parser = argparse.ArgumentParser(description='Upload a mulitple subjects to MROCP via a single dir that must match bg1/MRN\
-		   . Base url -> http://www.mrbrain.cs.jhu.edu/')
-  parser.add_argument('url', action="store", help='url must be in the form: upload/{project}/{site}/{subject}/{session}/yes|no\      , where the last param is either \'yes\' or \'no\', but not both. \'yes\' adds .mat files for graph invariant result & \	    .csv for graphs, \'no\' does not')
+		   . Base url -> http://www.mrbrain.cs.jhu.edu/disa/upload')
+  parser.add_argument('url', action="store", help='url must be in the form: {project}/{site}/{subject}/{session}/yes|no\      , where the last param is either \'yes\' or \'no\', but not both. \'yes\' adds .mat files for graph invariant result & \	    .csv for graphs, \'no\' does not')
   parser.add_argument('dataDir', action="store", help ='Data directory must have a fiber\
                       subdirectory with tract files & an roi subdirectory with rois')
   
--- MROCPdjango/examples/uploadsubj.py
+++ MROCPdjango/examples/uploadsubj.py
@@ -12,8 +12,8 @@ import webbrowser
 
 def main():
 
-  parser = argparse.ArgumentParser(description='Upload a subject to MROCP. Base url -> http://mrbrain.cs.jhu.edu/')
-  parser.add_argument('url', action="store", help='url must be in the form  upload/{project}/{site}/{subject}/ \\			{session}/{scanID}/yes|no, where the last param is either \'yes\' or \'no\', but not both. \
+  parser = argparse.ArgumentParser(description='Upload a subject to MROCP. Base url -> http://mrbrain.cs.jhu.edu/disa/upload')
+  parser.add_argument('url', action="store", help='url must be in the form  {project}/{site}/{subject}/ \\			{session}/{scanID}/yes|no, where the last param is either \'yes\' or \'no\', but not both. \
 		\'yes\' adds .mat files for graph invariant result & .csv for graphs, \'no\' does not')
   parser.add_argument('fiberfile', action="store")
   parser.add_argument('roixmlfile', action="store")
@@ -68,7 +68,7 @@ def main():
     else:
       redir += Dir
   
-  webbrowser.open('http://www.openconnecto.me/data/projects/disa/OCPprojects' + redir)
+  webbrowser.open('http://mbrain.cs.jhu.edu/data/projects/disa/OCPprojects' + redir)
   
 if __name__ == "__main__":
   main()
