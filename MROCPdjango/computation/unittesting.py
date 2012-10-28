26ae7aecdb5320a69fbad7a5d8d154761ab4d51d
Disa Mhembere <disa@jhu.edu>
2012-10-27 21:40:14 -0400

    unit tests time for new triangle counting
    Former-commit-id: 36ef0d7f3347bda3d379352993a5c85710db09ca [formerly 774ae88365288c611bd4a39efae7c9ae9be8f60a] [formerly d9d60eca2d2ad2d3869e033916f93784424b6c47 [formerly 4e66f89c1ec80c41d1051f280ce0aa34824e2702]]
    Former-commit-id: 00a444dcbe1acd7629061ef34262a00639dfd8d8
    Former-commit-id: f338369769a56d9dbe9b7204635b9c64dd14812f

--- MROCPdjango/computation/unittesting.py
+++ MROCPdjango/computation/unittesting.py
@@ -4,12 +4,6 @@ import os
 #Global filenames
 
 class test():
-  aplArr = "aplArr"
-  ccArr = "ccArr"
-  degArr = "degArr"
-  scanStatArr = "scanStatArr"
-  triArr = "triArr"   
-  
   def __init__(self, G_fn, dataDir, numNodes, ss1_fn = None, deg_fn = None, tri_fn = None, ccArr_fn = None):
     self.G_fn = G_fn
     self.numNodes = numNodes
@@ -18,66 +12,53 @@ class test():
     self.tri_fn = tri_fn
     self.ccArr_fn = ccArr_fn
     self.dataDir = dataDir
+    self.benchdir = 'bench'
     
     if not os.path.exists(dataDir):
       os.makedirs(dataDir)
   
-  def testDegree():
+  def testDegree(self):
     #for num in [10,50,100]:
-    #deg_fn = os.path.join(benchdirNm,str(self.numNodes),self.degArr)
+    #deg_fn = os.path.join(benchdir,str(self.numNodes),self.degArr)
     
     test = np.load(self.deg_fn)
-    bench = np.load(os.path.join(benchdirNm, self.numNodes,"degArr.npy"))
-    np.testing.assert_equal(bench,test,"Disa: Inequality testing DEGREE failure on %d test" % self.num, False)
+    bench = np.load(os.path.join(self.benchdir, str(self.numNodes),"degArr.npy"))
+    np.testing.assert_equal(bench, test, "Disa: Inequality testing DEGREE failure on %d test" % self.numNodes, False)
+    
+    print "\n****Successful degree test****"
   
-  def testSS1():
+  def testSS1(self):
     #for num in [10,50,100]:
-    #scanStatArr_fn = os.path.join(benchdirNm,str(self.num),self.scanStatArr)
+    #scanStatArr_fn = os.path.join(self.benchdir,str(self.numNodes),self.scanStatArr)
 
     test = np.load(self.ss1_fn)    
-    bench = np.load(os.path.join(benchdirNm, self.numNodes, "scanStatArr.npy"))
+    bench = np.load(os.path.join(self.benchdir, str(self.numNodes), "scanStatArr.npy"))
 
-    np.testing.assert_equal(bench,test,"Disa: Inequality testing SCANSTAT1 failure on %d test" % self.num, False)
+    np.testing.assert_equal(bench, test, "Disa: Inequality testing SCANSTAT1 failure on %d test" % self.numNodes, False)
+    
+    print "\n****Successful test scan stat test****"
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''  
-  def testTriangles():
+  def testTriangles(self):
     #for num in [10,50,100]:      
-    triArr_fn = os.path.join(benchdirNm,str(self.num),self.triArr)
+    #triArr_fn = os.path.join(self.benchdir,str(self.numNodes),self.triArr)
       
-    np.load(triArr)
-    np.load("test_triangles.npy")
+    test = np.load(self.tri_fn)
+    bench = np.load(os.path.join(self.benchdir, str(self.numNodes), "triArr.npy"))
     
-    np.testing.assert_equal(bench,test,"Disa: Inequality testing TRIANGLE failure on %d test" % self.num, False)
+    np.testing.assert_equal(bench,test,"Disa: Inequality testing TRIANGLE failure on %d test" % self.numNodes, False)
     
-  
+  '''
   def testClustCoeff():
     #for num in [10,50,100]:      
-    ccArr_fn = os.path.join(benchdirNm,str(self.num),self.ccArr)
-    testcArr_fn = os.path.join(benchdirNm,str(self.num),"test_clustcoeff.npy")
+    ccArr_fn = os.path.join(self.benchdir,str(self.numNodes),self.ccArr)
+    testcArr_fn = os.path.join(self.benchdir,str(self.numNodes),"test_clustcoeff.npy")
       
     np.load(ccArr)
     np.load(testcArr_fn)
     
-    np.testing.assert_equal(bench,test,"Disa: Inequality testing TRIANGLE failure on %d test" % self.num, False)
+    np.testing.assert_equal(bench,test,"Disa: Inequality testing TRIANGLE failure on %d test" % self.numNodes, False)
   
-  # REMEMBER TO CHECK MAX AVERAGE DEGREE
+  # print "\n\n****REMEMBER TO CHECK MAX AVERAGE DEGREE*****"
   
   def testAPL():
     pass
