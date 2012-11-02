320bd86f20af54eaeefeb4df19f8ec1ec7ab941e
Disa Mhembere <dmhembe1@jhu.edu>
2012-11-02 05:18:14 -0400

    Division of labor & some grant visualization code
    Former-commit-id: 2e34667b3d7d077eb730d492351006667eb3f1f2 [formerly 68ebbdc2b30fa3c1e73c2090e8a3ae8a82993db9] [formerly d31ae60dc692cad2ff994c09f37bdc1d3fb9fdfb [formerly ddfbf3b9ed58893cc966d9632b36da31f32df8c5]]
    Former-commit-id: 2043631aa1b9b1b0a0a641eaf585ccd021c1dba7
    Former-commit-id: eb40ad8a8bb3224af40fbc18e3bf7f4d15f2ba4a

---
+++ MROCPdjango/computation/maxavgdeg.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+# Author: Disa Mhembere, Johns Hopkins University
+# 10/2/2012
+# Calculate the estimated maximum average degree as top eigenvalue
+# 1 indexed
+import os
+
+import scipy.io as sio
+import scipy.sparse.linalg.eigen.arpack as arpack
+import numpy as np
+
+from getBaseName import getBaseName
+from loadAdjMatrix import loadAdjMat
+import argparse
+  
+def getMaxAveDegree(G_fn, lcc_fn = None, roiRootName = None, toDir = None, saveTop50 = True):
+  '''
+  Calc the Eigenvalue Max Average Degree of the graph
+  Note this is an estimation and is guaranteed to be greater than or equal to the true MAD
+  G_fn - filename of the graph .npy
+  lcc_fn - largest connected component of the graph. If none then this is a test case. .npy file
+  roiRootName - full path of roi + root (i.g. /Users/disa/roi/MXXX_roi)
+  '''
+  print "\nCalcuting maximum average degree..."
+  
+  if lcc_fn:
+    G = loadAdjMat(G_fn, lcc_fn, roiRootName)
+    
+  # test case
+  else:    
+    try:
+      G = sio.loadmat(G_fn)['fibergraph']
+    except:
+      print "file not found %s" % G_fn
+
+  if toDir:
+    eigvl_fn = os.path.join(toDir, getBaseName(G_fn) + '_eigvl.npy')
+    MAD_fn = os.path.join(toDir, getBaseName(G_fn) + '_MAD.npy')
+    
+  else: # test
+    eigvl_fn = os.path.join('bench', str(G.shape[0]), getBaseName(G_fn) + '_eigvl.npy')
+    MAD_fn = os.path.join('bench', str(G.shape[0]), getBaseName(G_fn) + '_MAD.npy')
+      
+  numEigs = 50 if (G.shape[0] > 51) else G.shape[0]-2 # Number of eigenvalues to compute
+  
+  topEigs = (arpack.eigs(G, k = numEigs, which='LR')[0]).real # get eigenvalues, then +ve max REAL part is MAD eigenvalue estimation
+  maxAveDeg = np.max(topEigs) 
+  
+  print "Saving Top 50 eigenvalues"
+  np.save(MAD_fn ,maxAveDeg)
+  
+  if (saveTop50):
+    np.save(eigvl_fn, topEigs)
+  
+  print "Maxium average degree = ", maxAveDeg
+
+def main():
+    
+    parser = argparse.ArgumentParser(description='Calculate Max Avg Degree estimate as max eigenvalue for biggraphs')
+    parser.add_argument('eigValDir', action='store',help='The full path of directory containing .npy eigenvalue arrays')
+    parser.add_argument('G_fn', action='store',help='Full filename sparse graph (.mat)')
+    parser.add_argument('lcc_fn', action='store',help='Full filename of largest connected component (.npy)')
+    parser.add_argument('roiRootName', action='store',help='Full path of roi director + baseName')
+    parser.add_argument('toDir', action='store', help='Full path of directory where you want .png figure file to go')
+    parser.add_argument('-s', '--saveTop50', help='Save the top 50 results to a file' )
+    
+    result = parser.parse_args()
+    if (results.saveTop5):
+      getMaxAveDegree(result.G_fn, result.lcc_fn, result.roiRootName, result.toDir)
+    else:
+      getMaxAveDegree(result.G_fn, result.lcc_fn, result.roiRootName, result.toDir, False)
+
+if __name__ == '__main__':
+  main()
+  
