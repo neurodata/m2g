fe676dd0ce892d5268adbeea37d530b045b0442d
Disa Mhembere <dmhembe1@jhu.edu>
2012-11-05 01:31:05 -0500

    Minor
    Former-commit-id: a8a6c87cc766fa50d723df7e513cf41ec7f1fc32 [formerly f3bbc9a29eb03a4077fe8ddce70937e49b307714] [formerly 41b3d8b3a1ac6f5e0038688f1750901bf1d9106c [formerly e2f3ab77cd8f86c5de084364bd7dcdafa5d7b09d]]
    Former-commit-id: 5e2d210b4c435ef79f84a45027a7c237250a527d
    Former-commit-id: bc230383b964fd80731e398d5d1f3e3e77e8e924

--- MROCPdjango/computation/loadAdjMatrix.py
+++ MROCPdjango/computation/loadAdjMatrix.py
@@ -28,13 +28,13 @@ def loadAdjMat(G_fn, lcc_fn, roiRootName = None):
     G = G+G.T # Symmetrize
   except:
     if not os.path.exists(roiRootName):
-      print "%s Doesn't exist" % roiRootName
+      print "%s Roi: Doesn't exist" % roiRootName
     
     if not os.path.exists(lcc_fn):
-      print "%s Doesn't exist" % lcc_fn
+      print "%s Lcc: Doesn't exist" % lcc_fn
       
     if not os.path.exists(G_fn):
-      print "%s Doesn't exist" % G_fn
+      print "%s Graph: Doesn't exist" % G_fn
     
     if os.path.exists(roiRootName) and os.path.exists(lcc_fn) and os.path.exists(G_fn):
       print "****Some wild Problem loading real lcc & graph****"
@@ -51,7 +51,7 @@ def getRoiRoot(G_fn):
   roiRoot = '/'
   for i in G_fn.split('/')[1:-3]:
     roiRoot = os.path.join(roiRoot, i)
-  roiRoot = os.path.join(roiRoot, "base", "roi")
+  roiRoot = os.path.join(roiRoot, "base","roi")
   return roiRoot
   
 def main():
