fda8d44d214953a51337fa61a3cd43632c472864
Disa Mhembere <dmhembe1@jhu.edu>
2012-11-08 12:00:58 -0500

    plotting scripts
    Former-commit-id: 6326ba3e386d4a3e6d386840b96ac9c48ff6e0e6 [formerly 8578794a1e3fc0d5f51b869f2d62351dce0da82a] [formerly 0c0e1a39d3338a5327477ba788a3cf2b7f8bf786 [formerly 591df98921d67ee1101d134d17a1b79a626ec94a]]
    Former-commit-id: 641c7d66a5b776e14867b736ab9b42fa918e5b1a
    Former-commit-id: 07da7c4d3649fe9c7f5feb58092cfab1d416f0f4

--- MROCPdjango/computation/plotting/distPlot.py
+++ MROCPdjango/computation/plotting/distPlot.py
@@ -12,24 +12,40 @@ import sys
 import glob
 import argparse
 
-def plotInvDist(invDir, toDir):
+import numpy as np
+from scipy import interpolate
+
+def plotInvDist(invDir, toDir, numBins =10):
   
   if not os.path.exists(invDir):
     print "%s does not exist" % invDir
     sys.exit(1)
   
-  for arrfn in glob.glob(os.path.join(invDir,'*')):
-      try:
-        arr = np.load(arrfn)
-      except:
-        print "Ivariant file not found %s" % arrfn
-      n, bins, patches = plt.hist(arr, bins=10, range=None, normed=False, weights=None, cumulative=False, \
-               bottom=None, histtype='stepfilled', align='mid', orientation='vertical', \
-               rwidth=None, log=False, color=None, label=None, hold=None)
-      
+  for arrfn in glob.glob(os.path.join(invDir,'*.npy')):
+    try:
+      arr = np.load(arrfn)
+      #import pdb; pdb.set_trace()
+    except:
+      print "Ivariant file not found %s"  % arrfn
+    plt.figure(1)
+    n, bins, patches = plt.hist(arr, bins=numBins , range=None, normed=False, weights=None, cumulative=False, \
+             bottom=None, histtype='stepfilled', align='mid', orientation='vertical', \
+             rwidth=None, log=False, color=None, label=None, hold=None)
+  
+    n = np.append(n,0)
+    plt.figure(2)
+    # Flat follow
+    plt.plot(bins, n, 'r--', linewidth=1)
+    
+    # Interpolation
+    #f = interpolate.interp1d(bins, n, kind='cubic') 
+    
+    #x = np.arange(bins[0],bins[-1],0.01) # vary linspc
+    #plt.plot(x, f(x),'-b' ,linewidth=1)
+    
   plt.title('Invariant distribution')  
-  plt.ylabel('y label')
-  plt.xlabel('x label')
+  plt.ylabel('Raw count')
+  plt.xlabel('Vertex')
   plt.show()
   
   if not os.path.exists(toDir):
@@ -42,9 +58,10 @@ def main():
     parser = argparse.ArgumentParser(description='Plot distribution of invariant arrays of several graphs')
     parser.add_argument('invDir', action='store',help='The full path of directory containing .npy invariant arrays')
     parser.add_argument('toDir', action='store', help='Full path of directory where you want .png figure file to go')
+    parser.add_argument('numBins', type = int, action='store', help='Number of bins')
     
     result = parser.parse_args()
-    plotInvDist(result.invDir, result.toDir)
+    plotInvDist(result.invDir, result.toDir, result.numBins)
 
 if __name__ == '__main__':
   main()
