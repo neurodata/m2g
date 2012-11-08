00b5c1c69a2be9dc316158f3ea4f6fb3c0dedc55
Disa Mhembere <dmhembe1@jhu.edu>
2012-11-08 12:00:58 -0500

    plotting scripts
    Former-commit-id: 6326ba3e386d4a3e6d386840b96ac9c48ff6e0e6 [formerly 8578794a1e3fc0d5f51b869f2d62351dce0da82a] [formerly 0c0e1a39d3338a5327477ba788a3cf2b7f8bf786 [formerly 591df98921d67ee1101d134d17a1b79a626ec94a]]
    Former-commit-id: 641c7d66a5b776e14867b736ab9b42fa918e5b1a
    Former-commit-id: 07da7c4d3649fe9c7f5feb58092cfab1d416f0f4

--- MROCPdjango/computation/plotting/plotEigs.py
+++ MROCPdjango/computation/plotting/plotEigs.py
@@ -9,25 +9,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import sys
+from glob import glob
 
 def plotEigs(eigValDir, toDir):
   
   if not os.path.exists(eigValDir):
     print "%s does not exist" % eigValDir
     sys.exit(1)
+  
   n = 0
-  for eigValInstance in os.lisdir(eigValDir):
-    if not eigValInstance.startswith("."):
-      
-      try:
-        eigv = np.load(eigValInstance)
-      except:
-        print "Eigenvalue array"
-      
-      if n == 0:
-        n = len(eigv)
-        plt.plot([range(1,n+1)], eigv,'-b')
-        #plt.axis([1, n+1]) # DM TODO: Axis scaling 
+  for eigValInstance in glob(os.path.join(eigValDir,"*.npy")):
+    try:
+      eigv = np.load(eigValInstance)
+    except:
+      print "Eigenvalue array"
+    
+    #if n == 0:
+    n = len(eigv)
+    plt.plot([range(1,n+1)], eigv,'-b')
+    #plt.axis([1, n+1]) # DM TODO: Axis scaling 
       
   plt.title('Top 50 largest eigenvalues')  
   plt.ylabel('Eigenvalue number')
