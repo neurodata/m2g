4f37c3dd40fa4cfd0d8aa53accde3946c2cccb74
Disa Mhembere <dmhembe1@gmail.com>
2012-12-02 21:19:35 -0500

    scan stat parallel
    Former-commit-id: 8cbc29d4ffadb07a5d7c6df8ee54a3e9ac82badc [formerly 59338c2c376bf027ef8c5fce34764b85c8d2a40f] [formerly b08fa96ffd01ed507a47eebeb1bdd9b628931cf3 [formerly f15efdd8c9c92e7535b12d936572b47fab9a0e86]]
    Former-commit-id: bb9fc5442d607a907571039556c2bc55ad0ce954
    Former-commit-id: 4a38371408629efdd6b1aa9651b2948e4d6577dc

--- MROCPdjango/computation/scanstat_degrParr.py
+++ MROCPdjango/computation/scanstat_degrParr.py
@@ -41,28 +41,34 @@ def calcScanStat_Degree(G_fn, G = None, lcc_fn = None, roiRootName = None, ssDir
   numNodes = G.shape[0]  
   vertxDeg = np.zeros(numNodes) # Vertex degrees of all vertices
   indSubgrEdgeNum = np.zeros(numNodes) # Induced subgraph edge number i.e scan statistic
-  
+
   cpu1 = range(numNodes/4)
   cpu2 = range(cpu1[-1]+1,cpu1[-1]+(numNodes/4))
   cpu3 = range(cpu2[-1]+1,cpu2[-1]+(numNodes/4))
   cpu4 = range(cpu3[-1]+1, numNodes)
   
+  ppservers = ()
+  job_server = pp.Server(ppservers=ppservers)
+  import pdb; pdb.set_trace() 
+  
   job_server = pp.Server()
-  
+  job_server.set_ncpus(4) 
   start = time()
-  j1 = job_server.submit(popSSarray, (cpu1,G,) , (zeros_like,),("numpy",))
-  j2 = job_server.submit(popSSarray, (cpu2,G, ), (zeros_like,),("numpy",)) 
-  j3 = job_server.submit(popSSarray, (cpu3,G, ), (zeros_like,),("numpy",))
-  j4 = job_server.submit(popSSarray, (cpu4,G, ), (zeros_like,),("numpy",)) 
-  
-  indSubgrEdgeNum[cpu1[0]:cpu1[-1]] = j1()
-  indSubgrEdgeNum[cpu2[0]:cpu2[-1]] = j2()
-  indSubgrEdgeNum[cpu3[0]:cpu3[-1]] = j3()
-  indSubgrEdgeNum[cpu4[0]:cpu4[-1]] = j4()
-  
+
+  j1 = job_server.submit(popSSarray, (cpu1,G,1) , (),("numpy","scipy","math",))
+  j2 = job_server.submit(popSSarray, (cpu2,G,2), (),("numpy","scipy","math",)) 
+  j3 = job_server.submit(popSSarray, (cpu3,G,3), (),("numpy","scipy","math",))
+  j4 = job_server.submit(popSSarray, (cpu4,G,4), (),("numpy","scipy","math",)) 
+  #import pdb; pdb.set_trace() 
+  job_server.wait()
+  indSubgrEdgeNum[cpu1[0]:cpu1[-1]], vertxDeg[cpu1[0]:cpu1[-1]] = j1()
+  indSubgrEdgeNum[cpu2[0]:cpu2[-1]], vertxDeg[cpu2[0]:cpu2[-1]] = j2()
+  indSubgrEdgeNum[cpu3[0]:cpu3[-1]], vertxDeg[cpu3[0]:cpu3[-1]] = j3()
+  indSubgrEdgeNum[cpu4[0]:cpu4[-1]], vertxDeg[cpu4[0]:cpu4[-1]] = j4()
+
+  #j1 = popSSarray(cpu1,G,1);
   print "Time taken Scan Stat 1: %f secs" % float(time() - start)
-  import pdb; pdb.set_trace()
-  
+
   '''write to file '''
   if ssDir:
     ss1_fn = os.path.join(ssDir, getBaseName(G_fn) + '_scanstat'+str(N)+'.npy')
@@ -78,9 +84,20 @@ def calcScanStat_Degree(G_fn, G = None, lcc_fn = None, roiRootName = None, ssDir
   del vertxDeg, indSubgrEdgeNum
   return [ss1_fn, deg_fn, G.shape[0]] #return scan stat1, degree of each node, the number of nodes in the graph
 
-def popSSarray(rangeNodes, G):
-  indSubgrEdgeNum = zeros_like(len(rangeNodes))
+def popSSarray(rangeNodes, G, processorNum):
+  #import numpy
+  #from math import ceil
+  print "Process %d start..." % processorNum
+  percNodes = int(len(rangeNodes)*0.1)
+  mulNodes = float(len(rangeNodes))
+
+  indSubgrEdgeNum = numpy.zeros(len(rangeNodes))
+  vertxDeg = numpy.zeros(len(rangeNodes))
+
   for vertx in rangeNodes:
+    if (vertx > 0 and (vertx% (percNodes) == 0)):
+      print "Processor %d: %.1f%% complete..." % (processorNum ,math.ceil((vertx/mulNodes)*100))
+
     nbors = G[:,vertx].nonzero()[0] 
     vertxDeg[vertx] = nbors.shape[0] # degree of each vertex 
     
@@ -90,8 +107,8 @@ def popSSarray(rangeNodes, G):
       indSubgrEdgeNum[vertx] = nbors.shape[0] + (nborsAdjMat.nnz/2.0)  # scan stat 1 # Divide by two because of symmetric matrix
     else:
       indSubgrEdgeNum[vertx] = 0 # zero neighbors hence zero cardinality enduced subgraph
-  return ssArrPortion
-
+  return indSubgrEdgeNum
+  print "Processor %d 100%% complete" % processorNum
 
 def main():
     
