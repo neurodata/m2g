76a7c2f9cd8526aa2a9e11d099b59fda5617bfd1
Disa Mhembere <dmhembe1@jhu.edu>
2013-01-06 01:50:49 -0800

    Formal Documentation begins
    Former-commit-id: 6eca84fdf80d0c3d52367616e3529b2fc4ada8a3 [formerly 51a086322515418998984fd435e7b6b17b7d78de] [formerly c8d7ad53b4025b6c21af582b26da6438ad010b03 [formerly fe7d880e3336e29b4b941613f32bf7521a6c306f]]
    Former-commit-id: f94c67f26c4cc03549377a959da72ec975b19faf
    Former-commit-id: c6bda26e770d9d8bda67a50c0db748a7d6e35e6b

--- MROCPdjango/ocpipeline/filesorter.py
+++ MROCPdjango/ocpipeline/filesorter.py
@@ -1,35 +1,52 @@
-'''
+#!/usr/bin/python
+
+"""
 @author: Disa Mhembere
-Insert the correct files into the processing tools
+@organization: Johns Hopkins University
+@contact: disa@jhu.edu
+
+@summary: Insert the correct files into the processing tools
 This script checks file extensions through regular expression
 It assumes no more than 1 file type per upload
-'''
+"""
+
 import re
 
 def checkFileExtGengraph(files):
-   for fileName in files:
-            
-       if re.match(re.compile( r'.+\.xml'), fileName) != None:
-            xmlFileName = fileName
-            
-       if re.match(re.compile( r'.+\.dat'), fileName) != None:
-            datFileName = fileName
-            
-       if re.match( re.compile( r'.+\.raw'), fileName) != None:
-            rawFileName = fileName
-   
-   return [xmlFileName, datFileName, rawFileName]
-   
+  '''
+  Check the file extensions of a set of files used in MROCP
+
+  @param files: the files to check
+  @type files: string
+  '''
+  for fileName in files:
+
+    if re.match(re.compile( r'.+\.xml'), fileName) != None:
+      xmlFileName = fileName
+
+    if re.match(re.compile( r'.+\.dat'), fileName) != None:
+      datFileName = fileName
+
+    if re.match( re.compile( r'.+\.raw'), fileName) != None:
+      rawFileName = fileName
+
+  return [xmlFileName, datFileName, rawFileName]
+
 def checkFileExtSVD(files = 0):
-   print "STUB for SVD check files"
-   
+  '''
+  @todo
+  '''
+  print "STUB for SVD check files"
+
 def checkFilesLCC(files = 0):
-   print "STUB for lcc check files"
+  '''
+  @todo
+  '''
+  print "STUB for lcc check files"
 
 def main():
-   checkFileExtSVD()
-   checkFilesLCC()
-   
+  checkFileExtSVD()
+  checkFilesLCC()
+
 if __name__ == '__main__':
    main()
-
