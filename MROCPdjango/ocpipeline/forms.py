caa1380e65a9a7568e9156245f57bdf4371dd158
Disa Mhembere <dmhembe1@jhu.edu>
2013-01-16 14:12:09 -0500

    buildgraph url add
    Former-commit-id: a6d4a510177f3c671200e12f7fdb505db05c13d4 [formerly 7c3f2a3d8d1ed4c8f03bf0ff2c9da2c617af2b69] [formerly eddec4e569fc3f66f207a6b0e1507212eac9edf9 [formerly 36ddd74515acabed06739849086cc0506739219e]]
    Former-commit-id: 09150e9d841b37c1568ea43b9380c33f7bc87f22
    Former-commit-id: 7e816b3a692ede1ed114cf9762bd4cd2471b2707

--- MROCPdjango/ocpipeline/forms.py
+++ MROCPdjango/ocpipeline/forms.py
@@ -11,12 +11,6 @@ from django import forms # for UploadFileForm
 from django.forms.fields import MultipleChoiceField
 from django.forms.widgets import RadioSelect, CheckboxSelectMultiple
 
-#from models import Document
-
-##########################################
-##########################################
-##########################################
-
 class BuildGraphForm(forms.Form):
     '''
     This form will be used for:
@@ -72,41 +66,6 @@ class BuildGraphForm(forms.Form):
     Select_Invariants_you_want_computed = forms.MultipleChoiceField(required=False,
     widget=CheckboxSelectMultiple, choices=INVARIANT_CHOICES)
 
-##########################################
-##########################################
-##########################################
-
-class DocumentForm(forms.Form):
-    '''
-    This form will be used for uploading derivative files
-
-    @cvar INVARIANT_CHOICES: The current invariants that can be computed by the MROCP
-
-    @var docfile: Fiber tract file
-    @type docfile: string
-
-    @var roi_raw_file: Fiber tract xml file
-    @type roi_raw_file: string
-
-    @var roi_xml_file: Fiber tract roi file
-    @type roi_xml_file: string
-    '''
-    docfile = forms.FileField(label='Select fiber.dat file', help_text='                          ', required = True)
-    roi_raw_file = forms.FileField(label='Select roi.raw file', help_text='                          ', required = True)
-    roi_xml_file = forms.FileField(label='Select roi.xml file', help_text='                          ', required = True)
-
-    INVARIANT_CHOICES = (('SS1', 'Scan Statistic 1',), ('TriCnt', 'Triangle Count',), \
-        ('CC', 'Clustering co-efficient',), ('MAD', 'Maximum Average Degree',) \
-        ,('Deg', 'Vertex Degree',), ('Eigs', 'Top 100 Eigenvalues',), \
-        ('SS2', 'Scan Statistic 2 [Not yet available]',), ('APL', 'Average Path Length [Not yet available]',),\
-        ('GDia', 'Graph Diameter [Not yet available]',))
-
-    Select_Invariants_you_want_computed = forms.MultipleChoiceField(required=False,
-    widget=CheckboxSelectMultiple, choices=INVARIANT_CHOICES)
-
-    #class Meta:
-    #    model = Document
-
 class ConvertForm(forms.Form):
     '''
     This form will be used for uploading an invariant/graph and converting it to another file format
@@ -130,34 +89,6 @@ class ConvertForm(forms.Form):
 
     Select_conversion_format = forms.MultipleChoiceField(required=True, \
     widget=CheckboxSelectMultiple, choices=FORMAT_CHOICES)
-
-class DataForm(forms.Form):
-    '''
-    This form will be used for routing data correctly when building graphs.
-    It also is used in naming projects etc..
-
-    @var UserDefprojectName: The name of the project
-    @type UserDefprojectName: string
-
-    @var site: The site where scan was done
-    @type site: string
-
-    @var subject: The ID corresponding to the subject in question
-    @type subject: string
-
-    @var session: The session ID/name
-    @type session: string
-
-    @var scanId: The scan ID of the scan session
-    @type scanId: string
-    '''
-
-    UserDefprojectName  = forms.CharField(label='Project name   ', help_text='                          ', max_length=255, required = True, error_messages={'required': 'Please enter your Project name'})
-    site = forms.CharField(label='Enter Site   ', help_text='                          ', max_length=255, required = True)
-    subject = forms.CharField(label='Enter Subject ID  ', help_text='                          ', max_length=255, required = True)
-    session = forms.CharField(label='Enter Session ID ', help_text='                          ', max_length=255, required = True)
-    scanId = forms.CharField(label='Scan ID  ', help_text='                          ', max_length=255, required = True)
-
 
 class GraphUploadForm(forms.Form):
     '''
--- MROCPdjango/ocpipeline/models.py
+++ MROCPdjango/ocpipeline/models.py
@@ -16,10 +16,6 @@ from django.contrib import admin
 import os
 from time import strftime, localtime
 
-##########################################
-##########################################
-##########################################
-
 class BuildGraphModel(models.Model):
     '''
     upload_to location dynamically altered in view
@@ -35,36 +31,11 @@ class BuildGraphModel(models.Model):
     session = models.CharField(max_length=255,)
     scanId = models.CharField(max_length=255)
 
-##########################################
-##########################################
-##########################################
-
-class Document(models.Model):
-    '''upload_to location dynamically altered in view
-    (This is a little hacky & can be done better using a custom manager)
-    see: https://docs.djangoproject.com/en/dev/ref/models/instances/?from=olddocs
-    '''
-    docfile = models.FileField(upload_to = (' '))
-
-    def __unicode__(self):
-        return self.name
-
 class ConvertModel(models.Model):
     '''
     upload_to location dynamically altered in view
     '''
     filename = models.FileField(upload_to = (' '))
-
-    def __unicode__(self):
-        return self.name
-
-class Data(models.Model):
-    projectName  = models.CharField(max_length=255)
-    site = models.CharField(max_length=255,)
-    subject = models.CharField(max_length=255,)
-    session = models.CharField(max_length=255,)
-    scanId = models.CharField(max_length=255)
-
 
     def __unicode__(self):
         return self.name
@@ -76,4 +47,6 @@ class OK(models.Model):
     def __unicode__(self):
         return self.name
 
-admin.site.register(Document)
+admin.site.register(BuildGraphModel)
+admin.site.register(ConvertModel)
+admin.site.register(OK)
