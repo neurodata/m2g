ec86a19d41e86c5d983343c9db84ff9da4ed10db
Disa Mhembere <dmhembe1@jhu.edu>
2013-01-27 12:26:47 -0500

    num vertices fix
    Former-commit-id: a9f230df219e8ba058f9a24203f229b194e1c45f [formerly 8009a74548f3e9bdc44d612fca920fc8311ab1f0] [formerly 332d7b65449aa3fcf49580ad9c455808e298b593 [formerly fd45fa27fca4c529244c3b949dfda34cee9bf831]]
    Former-commit-id: d6f5521c57b68ff6d8a253768846a7df6fc47ad2
    Former-commit-id: e63455ec95868996741ff3c3e980fe9b98502b4e

---
+++ MROCPdjango/computation/numVerticeAndEdges.py
@@ -0,0 +1,42 @@
+import scipy.io as sio
+from glob import glob
+import os
+import numpy as np
+import argparse
+
+def main():
+
+  parser = argparse.ArgumentParser(description='Get GLOBAL number of edges and vertices for directory of graphs')
+  parser.add_argument('grDir', action='store',help='Full filename sparse graph (.mat) directory')
+  parser.add_argument('saveDir', action='store', help='Full path of directory where you want .npy array resulting files to go')
+
+  result = parser.parse_args()
+  countVerticesAndEdges(result.grDir, result.saveDir)
+
+def root(arrfn):
+  return (arrfn.split('/')[-1]).split('_')[0]
+
+def countVerticesAndEdges(grDir, saveDir):
+
+  numVertDict = {}
+  numEdgesDict = {}
+
+  if not os.path.exists(saveDir):
+    os.makedirs(saveDir)
+    print 'Directory created --> %s'
+
+  for f in glob(os.path.join(grDir,"*")):
+    print "Processing", f
+    G = sio.loadmat(f)['fibergraph']
+    numVertDict[root(f)] = G.shape[0]
+
+    edgeCount = 0 # Individual count of edge per graph
+    for vertex in range(G.shape[0]):
+      edgeCount += G[vertex].nnz
+    numEdgesDict[root(f)] = edgeCount
+
+  np.save(os.path.join(saveDir, 'numVericesDict'), np.array(numVericesDict)) # save the number of vertices
+  np.save(os.path.join(saveDir, 'numEdgesDict'), np.array(numEdgesDict)) # save the number of edges
+
+if __name__ == '__main__':
+  main()
