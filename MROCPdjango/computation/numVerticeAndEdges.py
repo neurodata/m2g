91a6e6a2f659327a949e334b553d1e4081bb8a28
Disa Mhembere <dmhembe1@jhu.edu>
2013-01-31 02:05:33 -0500

    temp hack
    Former-commit-id: 0ea9991bd8bc2dfe92ca4af316f124698ea5838e [formerly 0be010f4aa999073a229c43c591e061ba10ad540] [formerly 8b0324e156ffcdc205a760992579990b7f489a39 [formerly ae38e8a2986f144cd3c1b16a1296996ac73c3a5f]]
    Former-commit-id: 5fb10b99f745fe10b57236b4c639df8b6a7065f5
    Former-commit-id: b2457ed3aabd4de5427e8e2e6997e43e12ab3221

--- MROCPdjango/computation/numVerticeAndEdges.py
+++ MROCPdjango/computation/numVerticeAndEdges.py
@@ -3,6 +3,8 @@ from glob import glob
 import os
 import numpy as np
 import argparse
+import loadAdjMatrix
+from getBaseName import getBaseName
 
 def main():
 
@@ -11,7 +13,9 @@ def main():
   parser.add_argument('saveDir', action='store', help='Full path of directory where you want .npy array resulting files to go')
 
   result = parser.parse_args()
-  countVerticesAndEdges(result.grDir, result.saveDir)
+  #countVerticesAndEdges(result.grDir, result.saveDir)
+  countLCCVerticesAndEdges(result.grDir, result.saveDir)
+
 
 def root(arrfn):
   return (arrfn.split('/')[-1]).split('_')[0]
@@ -20,6 +24,9 @@ def countVerticesAndEdges(grDir, saveDir):
 
   numVertDict = {}
   numEdgesDict = {}
+
+  numVertLCCDict = {}
+  numVertLCCDict = {}
 
   if not os.path.exists(saveDir):
     os.makedirs(saveDir)
@@ -37,6 +44,29 @@ def countVerticesAndEdges(grDir, saveDir):
 
   np.save(os.path.join(saveDir, 'numVerticesDict'), np.array(numVertDict)) # save the number of vertices
   np.save(os.path.join(saveDir, 'numEdgesDict'), np.array(numEdgesDict)) # save the number of edges
+
+
+def countLCCVerticesAndEdges(grDir, saveDir):
+
+  numVertLCCDict = {}
+  numEdgeLCCDict = {}
+
+  if not os.path.exists(saveDir):
+    os.makedirs(saveDir)
+    print 'Directory created --> %s'
+
+  for f in glob(os.path.join(grDir,"*")):
+    print "Processing %s .." % f
+    G = loadAdjMat(f,'/data/projects/MR/MRN/connectedcomp/'+ getBaseName(f) + '_concomp.npy')
+    numVertDict[root(f)] = G.shape[0]
+
+    #edgeCount = 0 # Individual count of edge per graph
+    #for vertex in range(G.shape[0]):
+    #  edgeCount += G[vertex].nnz
+    #numEdgesDict[root(f)] = edgeCount
+
+  np.save(os.path.join(saveDir, 'numLCCVerticesDict'), np.array(numVertLCCDict)) # save the number of vertices
+  #np.save(os.path.join(saveDir, 'numEdgesDict'), np.array(numEdgesDict)) # save the number of edges
 
 if __name__ == '__main__':
   main()
