017b0250ab0f7f6395320c0f3672a687c5b92acc
Disa Mhembere <dmhembe1@jhu.edu>
2013-02-03 04:19:29 -0500

    big & small graphs func data plotter
    Former-commit-id: 362bc717e56d711761ed4632433acfa1e6fe559c [formerly 493e0ce9c28296eb401915227498bafe29d3f0ef] [formerly c846bce9b6492ef5c4a61d2ae6bff7e347c75bb2 [formerly 38764a1ecbdf392f6df02283eee3288c7ca48535]]
    Former-commit-id: 60dfb390c4b4d7df0c8a3ead2aeef5663fa9546d
    Former-commit-id: 5d18b7832ee109617473203e473f21902f7a42ef

--- MROCPdjango/computation/plotting/fdaplots.py
+++ MROCPdjango/computation/plotting/fdaplots.py
@@ -51,28 +51,46 @@ def newPlotStdMean(invDir, pngName, char, numBins =100, function = 'mean', numLC
     print "%s does not exist" % invDir
     sys.exit(1)
 
-  pl.figure(2)
-  fig_gl, axes = pl.subplots(nrows=nrows, ncols=ncols)
+  #pl.figure(2)
+  #fig_gl, axes = pl.subplots(nrows=nrows, ncols=ncols)
 
   for idx, drcty in enumerate (invDirs):
 
-    matricesArray = assembleAggMatrices(glob(os.path.join(invDir, drcty,'*.npy')), char, matDimZero)
+    matricesArray, binvals = assembleAggMatrices(glob(os.path.join(invDir, drcty,'*.npy')), char, matDimZero, numBins = numBins)
     processingArrs = perfOpOnMatrices(matricesArray, function, takeLog = False)
-
-    for each_file in glob(os.path.join(invDir, drcty,'*.npy')):
-      pass
 
     for proccCnt, arr in enumerate (processingArrs):
       fig = pl.figure(2)
       fig.subplots_adjust(hspace=.6)
 
       ax = pl.subplot(nrows,ncols,idx+1)
-
       plot_color = getPlotColor(proccCnt, allmat = False)
 
+      x = np.arange(binvals[0],binvals[1],(binvals[1] - binvals[0])/len(arr))
+
+      if len(x) < len (arr):
+        while (not len(x) == len(arr)):
+          x = np.append(x, (x[-1] + (x[-1]-x[-2])))
+          print "Expanding x"
+
+      if len(x) > len(arr):
+        while len(x) > len(arr):
+          x = x[:-1]
+          print "Truncating x"
+
       if function == 'mean':
-        #pl.plot(x, interp*100, color = plot_color, linewidth=1)
-        pl.plot(arr*100, color = plot_color, linewidth=1)
+        if (proccCnt == 0 and idx == 1):
+          pl.plot(x, arr*100, color = plot_color, linewidth=1, label = 'male')
+
+          plt.legend(bbox_to_anchor=(0.7, 1.3), loc=2, prop={'size':8}, borderaxespad=0.)
+          print "Adding male legend"
+        if (proccCnt == 1 and idx == 1):
+          pl.plot(x, arr*100, color = plot_color, linewidth=1, label = 'female')
+          plt.legend(bbox_to_anchor=(0.7, 1.3), loc=2, prop={'size':8}, borderaxespad=0.)
+          print "Adding female legend"
+        else:
+          pl.plot(x, arr*100, color = plot_color, linewidth=1)
+
       elif function == 'stddev':
         pl.plot(arr, color = plot_color, linewidth=1)
         #pl.plot(x, interp, color = plot_color, linewidth=1)
@@ -82,10 +100,10 @@ def newPlotStdMean(invDir, pngName, char, numBins =100, function = 'mean', numLC
           if numLCCVerticesfn:
             pass
             #plt.axis([0, 35, 0, 0.04])
-            #ax.set_yticks(scipy.arange(0,0.04,0.01))
+            ax.set_yticks(scipy.arange(0,4,1))
           else:
-            pass
-            #ax.set_yticks(scipy.arange(0,7,2))
+            #pass
+            ax.set_yticks(scipy.arange(0,8,2))
         elif function == 'stddev':
           pass
           #ax.set_yticks(scipy.arange(0,0.07,0.02))
@@ -101,11 +119,11 @@ def newPlotStdMean(invDir, pngName, char, numBins =100, function = 'mean', numLC
       if idx == 1:
         if function == 'mean':
           if numLCCVerticesfn:
-            #ax.set_yticks(scipy.arange(0,0.03,0.01))
+            ax.set_yticks(scipy.arange(0,2,0.5))
             pass
           else:
-            pass
-            #ax.set_yticks(scipy.arange(0,10,2))
+            #pass
+            ax.set_yticks(scipy.arange(0,8,2))
         elif function == 'stddev':
           pass
           #ax.set_yticks(scipy.arange(0,0.15,0.03))
@@ -115,11 +133,11 @@ def newPlotStdMean(invDir, pngName, char, numBins =100, function = 'mean', numLC
       if idx == 2:
         if function == 'mean':
           if numLCCVerticesfn:
-            pass
-            #ax.set_yticks(scipy.arange(0,0.03,0.01))
+            #pass
+            ax.set_yticks(scipy.arange(0,2.5,1))
           else:
-            pass
-            #ax.set_yticks(scipy.arange(0,8,2))
+            #pass
+            ax.set_yticks(scipy.arange(0,5,1))
         elif function == 'stddev':
           pass
           #ax.set_yticks(scipy.arange(0,0.08,0.02))
@@ -134,11 +152,12 @@ def newPlotStdMean(invDir, pngName, char, numBins =100, function = 'mean', numLC
       if idx == 3:
         if function == 'mean':
           if numLCCVerticesfn:
-            pass
-            #ax.set_yticks(scipy.arange(0,0.04,0.01))
+            #pass
+            ax.set_yticks(scipy.arange(0,3,1))
           else:
-            pass
-            #ax.set_yticks(scipy.arange(0,6,2))
+            #pass
+            ax.set_yticks(scipy.arange(0,6,2))
+            ax.set_xticks(scipy.arange(0,4,1))
         if function == 'stddev':
           pass
           #ax.set_yticks(scipy.arange(0,0.08,0.02))
@@ -147,27 +166,25 @@ def newPlotStdMean(invDir, pngName, char, numBins =100, function = 'mean', numLC
         pl.xlabel('Log Degree')
 
   #### Eigenvalues ####
-  '''
+
   if not numLCCVerticesfn:
     numEigs = 68
   else:
     numEigs = 100
 
   ax = pl.subplot(nrows, ncols, 5)
-  matricesArray = assembleAggMatrices(glob(os.path.join(invDir, EigDir,'*.npy')), char, numEigs, eig = True)
+  matricesArray, binvals = assembleAggMatrices(glob(os.path.join(invDir, EigDir,'*.npy')), char, numEigs, eig = True)
   processingArrs = perfOpOnMatrices(matricesArray, function, False)
 
   for proccCnt, arr in enumerate (processingArrs):
     plot_color = getPlotColor(proccCnt, allmat = False)
 
-    fig = pl.figure(2)
-    fig.subplots_adjust(hspace=.6)
-
     if function == 'mean':
       if numLCCVerticesfn:
-        pass
+        ax.set_yticks(scipy.arange(0,7,2))
       else:
-        ax.set_yticks(scipy.arange(-0.2,0.2,0.1))
+        pass
+        #ax.set_yticks(scipy.arange(-0.2,0.2,0.1))
       pl.plot(range(1,len(arr)+1), arr/10000, color=plot_color)
     elif function == 'stddev':
       ax.set_yticks(scipy.arange(0,35,10))
@@ -211,7 +228,6 @@ def newPlotStdMean(invDir, pngName, char, numBins =100, function = 'mean', numLC
 
   for proccCnt, arr in enumerate (processingArrs):
     pl.figure(1)
-
     arr = np.log(np.array(arr)[np.array(arr).nonzero()]) # NOTE THIS CHANGE
 
     n, bins, patches = pl.hist(arr, bins=10 , range=None, normed=False, weights=None, cumulative=False, \
@@ -219,16 +235,18 @@ def newPlotStdMean(invDir, pngName, char, numBins =100, function = 'mean', numLC
              rwidth=None, log=False, color=None, label=None, hold=None)
 
     n = np.append(n,0)
-    fig = pl.figure(2)
-    fig.subplots_adjust(hspace=.5)
+    #fig = pl.figure(2)
+    pl.figure(2)
+    #fig.subplots_adjust(hspace=.5)
 
     pl.ylabel('Frequency')
 
     pl.xlabel('Log Global Edge Number')
     if numLCCVerticesfn:
-      pass
+      ax.set_yticks(scipy.arange(0,15,3))
+      ax.set_xticks(scipy.arange(17,18.2,0.2))
     else:
-      #ax.set_yticks(scipy.arange(0,15,3))
+      ax.set_yticks(scipy.arange(0,15,3))
       #ax.set_xticks(scipy.arange(800,1250,200))
       pass
 
@@ -241,36 +259,9 @@ def newPlotStdMean(invDir, pngName, char, numBins =100, function = 'mean', numLC
 
     plot_color = getPlotColor(proccCnt, allmat = False)
     pl.plot(x, interp,color = plot_color ,linewidth=1)
-  '''
 
   pl.savefig(pngName+'.pdf')
   print '~**** Done  ****~'
-
-############################################################################
-############################################################################
-
-
-
-def main():
-  parser = argparse.ArgumentParser(description='Plot distribution of invariant arrays of several graphs')
-  parser.add_argument('invDir', action='store',help='The full path of directory containing .npy invariant arrays')
-  parser.add_argument('pngName', action='store', help='Full path of directory of resulting png file')
-  parser.add_argument('numBins', type = int, action='store', help='Number of bins')
-  parser.add_argument('char', action='store', help='Characteristic on which to partition data: gender or class')
-
-  parser.add_argument('-b', '--big', action='store', help='if working on big graphs pass in numLCCVertices.npy full with this param')
-
-  result = parser.parse_args()
-
-  #plotInvDist(result.invDir, result.pngName, result.numBins, result.char)
-  #plotstdmean(result.invDir, result.pngName, "gender", result.numBins)
-  #func(result.invDir)
-  if result.big:
-    newPlotStdMean(result.invDir, result.pngName, result.char, result.numBins, function = 'mean', numLCCVerticesfn = result.big) # '/mnt/braingraph1data/projects/MR/MRN/invariants/big/Globals/numLCCVerticesDict.npy'
-  #plotstdmean(result.invDir, result.pngName, result.char, result.numBins, function = 'mean') # !!!! NOTE HARDCODE!!!! #
-  else:
-    newPlotStdMean(result.invDir, result.pngName, result.char, result.numBins, function = 'mean')
-    #newPlotErrStdMean(result.invDir, result.pngName, result.char, result.numBins, function = 'mean')
 
 #########################################
 #########################################
@@ -303,7 +294,7 @@ def getMaxVertices(globalVertFn):
 #########################################
 #########################################
 
-def assembleAggMatrices(drctyArray, char, matRowLen, eig = False):
+def assembleAggMatrices(drctyArray, char, matRowLen, numBins = 100, eig = False):
   charDict, zero_type, one_type, two_type = csvtodict(char = char)
 
   allmat = np.zeros(shape=(len(charDict),matRowLen)); allcnt = 0
@@ -312,44 +303,51 @@ def assembleAggMatrices(drctyArray, char, matRowLen, eig = False):
 
   if char == 'class':
     twomat = np.zeros(shape=(len(two_type),matRowLen)); twocnt = 0
+    max2LenArray = float('-inf')
+
   minbin = float('inf')
   maxbin = float('-inf')
+
+  max0LenArray = float('-inf')
+  max1LenArray = float('-inf')
+
   for arrfn in drctyArray:
     try:
       arr = np.load(arrfn)
-      arr = np.log(arr[arr.nonzero()])
+      if not eig:
+        arr = np.log(arr[arr.nonzero()])
       print "Processing %s..., length --> %d" % (arrfn, len(arr))
     except:
       print "[ERROR]: Line %s: Invariant file not found %s"  % (lineno(),arrfn)
 
     if eig:
-      arr = (np.sort(arr)[::-1]) # reverse sort array
+      interp = (np.sort(arr)[::-1]) # reverse sort array
 
-    #pl.figure(1)
-    n, bins, patches = pl.hist(arr, bins=numBins , range=None, normed=False, weights=None, cumulative=False, \
-             bottom=None, histtype='stepfilled', align='mid', orientation='vertical', \
-             rwidth=None, log=False, color=None, label=None, hold=None)
+    else:
+      pl.figure(1)
+      n, bins, patches = pl.hist(arr, bins=numBins , range=None, normed=False, weights=None, cumulative=False, \
+               bottom=None, histtype='stepfilled', align='mid', orientation='vertical', \
+               rwidth=None, log=False, color=None, label=None, hold=None)
 
-    n = np.append(n,0)
-    n = n/float(sum(n))
+      n = np.append(n,0)
+      n = n/float(sum(n))
 
-    #fig = pl.figure(2)
-    #fig.subplots_adjust(hspace=.5)
+      f = interpolate.interp1d(bins, n, kind='cubic')
+      x = np.arange(bins[0],bins[-1],0.03) # vary linspc
 
-    f = interpolate.interp1d(bins, n, kind='cubic')
-    x = np.arange(bins[0],bins[-1],0.03) # vary linspc
+      interp = f(x)
+      ltz = interp < 0
+      interp[ltz] = 0
 
-    interp = f(x)
-    ltz = interp < 0
-    interp[ltz] = 0
+      minbin  = bins[0] if bins[0] < minbin else minbin
+      maxbin = bins[-1] if bins[-1] > maxbin else maxbin
 
-    # Unused
-    minbin  = bins[0] if bins[0] < minbin else minbin
-    maxbin = bins[-1] if bins[-1] > maxbin else maxbin
-    # Unused
+      #min0bin  = bins[0] if bins[0] < min0bin else min0bin
+      #max0bin = bins[-1] if bins[-1] > max0bin else max0bin
 
-    if len(arr) > matRowLen:
-      print "SERIOUS ERROR! THIS SHOULD NEVER HAPPEN!"
+    if len(interp) > matRowLen:
+      interp = interp[:(matRowLen-1)]
+      print "File: %s, Truncated interpolation --> %d idx" % (arrfn,  matRowLen - (len(interp)))
 
     if len(interp) < matRowLen:
       #Pad array with zeros
@@ -362,15 +360,18 @@ def assembleAggMatrices(drctyArray, char, matRowLen, eig = False):
     if charDict[root(arrfn)] == '0':
       zeromat[zerocnt]  = interp
       zerocnt += 1
+      max0LenArray = interp.nonzero()[0][-1] if interp.nonzero()[0][-1] > max0LenArray else max0LenArray
 
     elif charDict[root(arrfn)] == '1':
       onemat[onecnt] = interp
       onecnt += 1
+      max1LenArray = interp.nonzero()[0][-1] if interp.nonzero()[0][-1] > max1LenArray else max1LenArray
 
     if char == 'class':
       if charDict[root(arrfn)] == '2':
         twomat[twocnt] = interp
         twocnt += 1
+        max2LenArray = interp.nonzero()[0][-1] if interp.nonzero()[0][-1] > max2LenArray else max2LenArray
 
   if char == 'class':
     #return [allmat, zeromat, onemat, twomat]
@@ -379,10 +380,11 @@ def assembleAggMatrices(drctyArray, char, matRowLen, eig = False):
   if char == 'gender':
     returnMats = [zeromat, onemat]
 
-  for idx, mat in enumerate(returnMats):
-    returnMats[idx] = trimMatrix(mat)
-  return returnMats
+  maxLenArrays = [max0LenArray, max1LenArray]
 
+  for idx, mat in enumerate(returnMats):
+    returnMats[idx] = trimMatrix(mat, maxLenArrays[idx])
+  return [returnMats, [minbin,maxbin]]
 
 #########################################
 #########################################
@@ -407,14 +409,15 @@ def perfOpOnMatrices(matricesArray, function, takeLog):
 #########################################
 #########################################
 
-def trimMatrix(mat):
-  matdim1range = range(mat.shape[1])[::-1]
-  while 1:
-    if np.array_equal(mat[:,-1], np.zeros_like(mat[:,-1])):
-      mat = mat[:,:-1]
-    else:
-      break
-  return mat
+def trimMatrix(mat, maxLenArray):
+  #matdim1range = range(mat.shape[1])[::-1]
+  #while 1:
+  #  if np.array_equal(mat[:,-1], np.zeros_like(mat[:,-1])):
+  #    mat = mat[:,:-1]
+  #    print 'removing zeros'
+  #  else:
+  #    break
+  return mat[:,:(maxLenArray-1)]
 
 #########################################
 #########################################
@@ -461,6 +464,43 @@ def csvtodict(fn ='/home/disa/code/mrn_covariates_n120-v4.csv', char = 'class'):
 
 #########################################
 #########################################
+
+def getPlotColor(proccCnt, allmat = True):
+  if allmat:
+    if proccCnt == 0: # All
+      return 'red'
+    if proccCnt == 1: # zero
+      return 'grey'
+    if proccCnt == 2: # one
+      return 'blue'
+    if proccCnt == 3: # two
+      return 'green'
+  else:
+    if proccCnt == 0: # zero
+      return 'grey'
+    if proccCnt == 1: # one
+      return 'blue'
+    if proccCnt == 2: # two
+      return 'green'
+
+#########################################
+#########################################
+
+def main():
+  parser = argparse.ArgumentParser(description='Plot distribution of invariant arrays of several graphs')
+  parser.add_argument('invDir', action='store',help='The full path of directory containing .npy invariant arrays')
+  parser.add_argument('pngName', action='store', help='Full path of directory of resulting png file')
+  parser.add_argument('numBins', type = int, action='store', help='Number of bins')
+  parser.add_argument('char', action='store', help='Characteristic on which to partition data: gender or class')
+
+  parser.add_argument('-b', '--big', action='store', help='if working on big graphs pass in numLCCVertices.npy full with this param')
+
+  result = parser.parse_args()
+
+  if result.big:
+    newPlotStdMean(result.invDir, result.pngName, result.char, result.numBins, function = 'mean', numLCCVerticesfn = result.big) # '/mnt/braingraph1data/projects/MR/MRN/invariants/big/Globals/numLCCVerticesDict.npy'
+  else:
+    newPlotStdMean(result.invDir, result.pngName, result.char, result.numBins, function = 'mean')
 
 if __name__ == '__main__':
   main()
