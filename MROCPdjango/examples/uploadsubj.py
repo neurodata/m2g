fbe1137d0cdf2bf1f3207c834601bb79c7a4088b
Disa Mhembere <dmhembe1@jhu.edu>
2013-02-08 02:21:12 -0500

    check for project exists fix and biggraph tested
    Former-commit-id: 314a5a293c09360052f3d5429877e3a84530b709 [formerly 7f910f51f7df5cfd1885549fd3152d6ad6e634fc] [formerly 5c5b886375cb280662772b297512e0466d6adcbb [formerly 26349c5ffb37ec7a2ac38542a9f6d29cf4ba3f29]]
    Former-commit-id: 728bea0f8dde214226dec6a091c36abe721bff70
    Former-commit-id: ff9f6610c4e14c73a1d4014edc153806be8f6722

--- MROCPdjango/examples/uploadsubj.py
+++ MROCPdjango/examples/uploadsubj.py
@@ -1,7 +1,7 @@
 import urllib2
 import argparse
 import sys
- 
+
 import zipfile
 import tempfile
 
@@ -13,13 +13,17 @@ import webbrowser
 def main():
 
   parser = argparse.ArgumentParser(description='Upload a subject to MROCP. Base url -> http://mrbrain.cs.jhu.edu/disa/upload')
-  parser.add_argument('url', action="store", help='url must be in the form  {project}/{site}/{subject}/ \\			{session}/{scanID}/yes|no, where the last param is either \'yes\' or \'no\', but not both. \
-		\'yes\' adds .mat files for graph invariant result & .csv for graphs, \'no\' does not')
+  parser.add_argument('url', action="store", help='url must have NO SPACES & must be in the form  http://mrbrain.cs.jhu.edu/disa/upload/{projectName}/{site}/{subject}/{session}/{scanID}/{s|b} where s= smallgraph OR b = biggraph')
+  parser.add_argument('-i', '--invariants', action="store", help='OPTIONAL: comma separated list of invariant types. E.g cc,tri,deg,mad for \
+                      clustering coefficient, triangle count, degree & maximum average degree')
+
   parser.add_argument('fiberfile', action="store")
   parser.add_argument('roixmlfile', action="store")
   parser.add_argument('roirawfile', action="store")
 
   result = parser.parse_args()
+
+  result.url = result.url if result.url.endswith('/') else result.url + '/' #
 
   try:
 
@@ -42,34 +46,36 @@ def main():
 #    ret = rzfile.testzip()
 #    ret = rzfile.namelist()
 #    import pdb; pdb.set_trace()
-    
+
   except:
     print "Invalid file name. Check the filenames: " + result.fiberfile,  result.roixmlfile,  result.roirawfile
     sys.exit(0)
 
   try:
-    req = urllib2.Request ( result.url, tmpfile.read() ) 
+    if (result.invariants):
+      req = urllib2.Request ( result.url + result.invariants , tmpfile.read() ) # Important to concatenate these in this order
+    else:
+      req = urllib2.Request ( result.url, tmpfile.read() ) # Important to concatenate these in this order
     response = urllib2.urlopen(req)
   except urllib2.URLError, e:
     print "Failed URL", result.url
-    print "Error %s" % (e.read()) 
+    print "Error %s" % (e.read())
     sys.exit(0)
 
   the_page = response.read()
   print "Success with id %s" % the_page
-  
+
   ''' Open up a tab in your browser to view results'''
   redir = '/'
   result.url = result.url[:-1] if result.url.endswith('/') else result.url # remove trailing backslash
-  
+
   for i, Dir in enumerate(result.url.split('/')[-6:-1]):
     if (i != len(result.url.split('/')[-6:-1])-1):
       redir += Dir + '/'
     else:
       redir += Dir
-  
+
   webbrowser.open('http://mbrain.cs.jhu.edu/data/projects/disa/OCPprojects' + redir)
-  
+
 if __name__ == "__main__":
   main()
-
