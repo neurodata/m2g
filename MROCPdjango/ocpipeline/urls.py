585b9504d3d941e92807bf17deea8de1a84647b8
Disa Mhembere <dmhembe1@jhu.edu>
2013-03-04 21:13:02 -0500

    https fixes
    Former-commit-id: 49907f18622c35ff3204546c20157bdaf56d8201 [formerly 2b150b1b995e42239c705eac009bb26511a96652] [formerly 52515108706a481f72e4ba5b60738d69cd42ba74 [formerly 7da31e1a64903dab49af8c43ec04e642112da455]]
    Former-commit-id: 7908ef3b461c4c83a01e85e924d80701698af420
    Former-commit-id: 097aa0c7a78b0afee9410b70b5b2ae7dc497a4a3

--- MROCPdjango/ocpipeline/urls.py
+++ MROCPdjango/ocpipeline/urls.py
@@ -25,7 +25,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('ocpipeline.views',
     url(r'^$', 'default', name= 'default'),
-    url(r'^welcome$', 'welcome', name= 'welcome'),
+    url(r'^welcome/$', 'welcome', name= 'welcome'),
     url(r'^success/$', 'success', name='success-page'),
     url(r'^confirmdownload/$', 'confirmDownload', name='confirm-dwnd-page'),
     url(r'^processinput/$', 'processInputData', name='process-input-data'),
