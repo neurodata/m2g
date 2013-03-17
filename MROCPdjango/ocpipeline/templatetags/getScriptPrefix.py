1d03f854c1018e1c80745519dd7ac345287de618
Disa Mhembere <dmhembe1@jhu.edu>
2013-03-17 17:26:41 -0400

    dynamic script prefix test
    Former-commit-id: 4bc2d98518a251678fad75450caa508472138f29 [formerly 165965ea3c8bf2aae0f8a2bd53243c75c839b36c] [formerly 17de3ed9bdbfe526fb4be0e99357b555648c1bc2 [formerly 5b5ca978c3f46cde1531c66fe00213a2153638a2]]
    Former-commit-id: 237aeacf46cdcf447f51f3c030939c13469ed822
    Former-commit-id: 1fb0e2e5b3bfaea4ecd5c3c24ea964a2f2ea5d41

---
+++ MROCPdjango/ocpipeline/templatetags/getScriptPrefix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+# getScriptPrefix.py
+# Created by Disa Mhembere on 2013-03-17.
+# Email: disa@jhu.edu
+# Copyright (c) 2013. All rights reserved.
+
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.core.urlresolvers import get_script_prefix
+
+register = template.Library()
+
+
+@register.filter(name='getScriptPrefix', is_safe=True)
+@stringfilter
+def getScriptPrefix(tailurl):
+  return get_script_prefix() + tailurl
