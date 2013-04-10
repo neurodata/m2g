355bfcd954f87b538774a34ca4fa7b1b7e578a0e
Disa Mhembere <dmhembe1@jhu.edu>
2013-04-10 02:54:03 -0400

    big graph add
    Former-commit-id: 4456e51fa7b7c407a3ab98c2ce9bb40fc5c68973 [formerly 7bd772d2427b6eba67c2b6923251f4070c0e010a] [formerly aab912a57f28e50bb6146682867be0cba1614543 [formerly 7a863e12bfc1a35f27f558ac84df5f8a8a766a11]]
    Former-commit-id: 24df9177bc5c4c2895b41b38683671695528b074
    Former-commit-id: 5b67bdb5c8403a96e119a7a0d7e7dc32a03a13ad

--- MROCPdjango/computation/composite/examples/example.sh
+++ MROCPdjango/computation/composite/examples/example.sh
@@ -7,16 +7,31 @@ wget http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_roi.
 wget http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_roi.xml
 cd ..
 
+# SMALL
 # Graph generation:
 echo "../graph_exec -h. For help"
 # Build a small graph given fiber streamline: test_fiber, fiber ROI's: test_roi.xml and test_roi.raw
-.././graph_exec ./testdata/test_fiber.dat ./testdata/test_roi.xml ./testdata/test_roi.raw  -S ./testdata
+.././graph_exec ./testdata/test_fiber.dat ./testdata/test_roi.xml ./testdata/test_roi.raw  -S ./testdata/small
 
 # Invariants:
 echo "./inv_exec -h. For help"
-.././inv_exec ./testdata/test_fiber_70_smgr.mat s -D fibergraph -A -S ./testdata
+.././inv_exec ./testdata/small/test_fiber_70_smgr.mat s -D fibergraph -A -S ./testdata/small
 
 # largest connected component & single value:
 echo "./deriv_exec -h. For help"
 # Compute LCC & SVD
-.././deriv_exec ./testdata/test_fiber_70_smgr.mat -v -l -S ./testdata
+.././deriv_exec ./testdata/small/test_fiber_70_smgr.mat -v -l -S ./testdata/small
+
+# BIG
+# Build big graph
+.././graph_exec ./testdata/test_fiber.dat ./testdata/test_roi.xml ./testdata/test_roi.raw  -S ./testdata/big -b -g bigtest_bggr.mat
+
+# largest connected component & single value:
+echo "./deriv_exec -h. For help"
+# Compute LCC & SVD
+.././deriv_exec ./testdata/big/bigtest_bggr.mat -v -l -S ./testdata/big
+
+# Invariants:
+echo "./inv_exec -h. For help"
+.././inv_exec ./testdata/big/bigtest_bggr.mat b -D fibergraph -A -S ./testdata/big -lf ./testdata/big/bigtest_concomp.npy
+
