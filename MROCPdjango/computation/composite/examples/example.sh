77b8912c6f85bcbaeb87cda13dfc8b008eb15d68
Disa Mhembere <dmhembe1@jhu.edu>
2013-06-24 02:03:56 -0400

    example readd
    Former-commit-id: 295d034485093d945662447372598e26c4dd0a76 [formerly 5fa497a817e14b48b34f946c0b3fa1f5bd60dc3d] [formerly c2fe6c91e4f40d7a0155a6cc8a6f2019bfe94d02 [formerly 4ce7636442bbf49b9eee93c44cb8b8eef0d485f7]]
    Former-commit-id: 367e8a0314acd97bf47da288183909079486db28
    Former-commit-id: 9b8d4e65ed3b009eaf34532b506c5670f8d8f6aa

---
+++ MROCPdjango/computation/composite/examples/example.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "Example downloads ./testdata directory from http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/ ..."
+
+# Uncomment if you want to redownload test data
+#mkdir ./testdata
+#cd testdata
+#wget http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_fiber.dat
+#wget http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_roi.raw
+#wget http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_roi.xml
+#cd ..
+
+# SMALL
+# Graph generation:
+echo "../graph_exec -h. For help"
+# Build a small graph given fiber streamline: test_fiber, fiber ROI's: test_roi.xml and test_roi.raw
+.././graph_exec ./testdata/test_fiber.dat ./testdata/test_roi.xml ./testdata/test_roi.raw  -S ./testdata/small
+
+# Invariants:
+echo "./inv_exec -h. For help"
+.././inv_exec ./testdata/small/test_fiber_70_smgr.mat s -D fibergraph -A -S ./testdata/small
+
+# largest connected component & single value:
+echo "./deriv_exec -h. For help"
+# Compute LCC & SVD
+.././deriv_exec ./testdata/small/test_fiber_70_smgr.mat -v -l -S ./testdata/small
+
+# BIG
+# Uncomment to Build big graph WARNING: Can take up to 6 hours to complete
+#.././graph_exec ./testdata/test_fiber.dat ./testdata/test_roi.xml ./testdata/test_roi.raw  -S ./testdata/big -b -g bigtest_bggr.mat
+
+# largest connected component & single value:
+#echo "./deriv_exec -h. For help"
+# Compute LCC & SVD
+#.././deriv_exec ./testdata/big/bigtest_bggr.mat -v -l -S ./testdata/big
+
+# Invariants:
+#echo "./inv_exec -h. For help"
+#.././inv_exec ./testdata/big/bigtest_bggr.mat b -D fibergraph -A -S ./testdata/big -lf ./testdata/big/LCC/bigtest_concomp.npy
+
