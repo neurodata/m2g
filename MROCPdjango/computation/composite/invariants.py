1ac32a5334f742c7be1b47bc8c686e8eaa9b544b
Disa Mhembere <dmhembe1@jhu.edu>
2013-12-31 13:00:09 -0500

    Tests added for benching igraph and mrocp. todo:optimize mrocp
    Former-commit-id: bcb30cd0283be399933fe4bae9915e445afaee74 [formerly 1ebd38e14bef7a06f8171bf64ae878e7d2b2d400] [formerly 1586eb4ca538f42dc61adc542213760c9e106ba9 [formerly 1e857aaed8c139acb1d154ea51fe693a8f0ac89b]]
    Former-commit-id: 489d66fd1aa3731eb392cda6dbe55f30f2991fb7
    Former-commit-id: 467a402ff183e01db5a9fadc729c386ec329b2b6

--- MROCPdjango/computation/composite/invariants.py
+++ MROCPdjango/computation/composite/invariants.py
@@ -168,65 +168,66 @@ def compute(inv_dict, save=True):
 
   # Computation complete - handle the saving now ...
 
-  ''' Top eigenvalues & eigenvectors '''
-  if not inv_dict['eigvl_fn'] and inv_dict['eig'] :
-    eigvDir = os.path.join(inv_dict['save_dir'], "Eigen") #if eigvDir is None else eigvDir
+  if save:
+    ''' Top eigenvalues & eigenvectors '''
+    if not inv_dict['eigvl_fn'] and inv_dict['eig'] :
+      eigvDir = os.path.join(inv_dict['save_dir'], "Eigen") #if eigvDir is None else eigvDir
 
-    # Immediately write eigs to file
-    inv_dict['eigvl_fn'] = os.path.join(eigvDir, getBaseName(inv_dict['graph_fn']) + '_eigvl.npy')
-    inv_dict['eigvect_fn'] = os.path.join(eigvDir, getBaseName(inv_dict['graph_fn']) + '_eigvect.npy')
-    createSave(inv_dict['eigvl_fn'], l.real) # eigenvalues
-    createSave(inv_dict['eigvect_fn'], u) # eigenvectors
-    print 'Eigenvalues and eigenvectors saved as ' + inv_dict['eigvect_fn']
+      # Immediately write eigs to file
+      inv_dict['eigvl_fn'] = os.path.join(eigvDir, getBaseName(inv_dict['graph_fn']) + '_eigvl.npy')
+      inv_dict['eigvect_fn'] = os.path.join(eigvDir, getBaseName(inv_dict['graph_fn']) + '_eigvect.npy')
+      createSave(inv_dict['eigvl_fn'], l.real) # eigenvalues
+      createSave(inv_dict['eigvect_fn'], u) # eigenvectors
+      print 'Eigenvalues and eigenvectors saved as ' + inv_dict['eigvect_fn']
 
-  ''' Triangle count '''
-  if not inv_dict['tri_fn'] and inv_dict['tri']:
-    triDir = os.path.join(inv_dict['save_dir'], "Triangle") #if triDir is None else triDir
-    inv_dict['tri_fn'] = os.path.join(triDir, getBaseName(inv_dict['graph_fn']) + '_triangles.npy')
-    createSave(inv_dict['tri_fn'], tri_array)
-    print 'Triangle Count saved as ' + inv_dict['tri_fn']
+    ''' Triangle count '''
+    if not inv_dict['tri_fn'] and inv_dict['tri']:
+      triDir = os.path.join(inv_dict['save_dir'], "Triangle") #if triDir is None else triDir
+      inv_dict['tri_fn'] = os.path.join(triDir, getBaseName(inv_dict['graph_fn']) + '_triangles.npy')
+      createSave(inv_dict['tri_fn'], tri_array)
+      print 'Triangle Count saved as ' + inv_dict['tri_fn']
 
-  ''' Degree count'''
-  if not inv_dict['deg_fn'] and inv_dict['deg']:
-    degDir = os.path.join(inv_dict['save_dir'], "Degree") #if degDir is None else degDir
-    inv_dict['deg_fn'] = os.path.join(degDir, getBaseName(inv_dict['graph_fn']) + '_degree.npy')
-    createSave(inv_dict['deg_fn'], deg_array)
-    print 'Degree saved as ' + inv_dict['deg_fn']
+    ''' Degree count'''
+    if not inv_dict['deg_fn'] and inv_dict['deg']:
+      degDir = os.path.join(inv_dict['save_dir'], "Degree") #if degDir is None else degDir
+      inv_dict['deg_fn'] = os.path.join(degDir, getBaseName(inv_dict['graph_fn']) + '_degree.npy')
+      createSave(inv_dict['deg_fn'], deg_array)
+      print 'Degree saved as ' + inv_dict['deg_fn']
 
-  ''' MAD '''
-  if inv_dict['mad']:
-    MADdir = os.path.join(inv_dict['save_dir'], "MAD") #if MADdir is None else MADdir
-    inv_dict['mad_fn'] = os.path.join(MADdir, getBaseName(inv_dict['graph_fn']) + '_mad.npy')
-    createSave(inv_dict['mad_fn'], max_ave_deg)
-    print 'Maximum average Degree saved as ' + inv_dict['mad_fn']
+    ''' MAD '''
+    if inv_dict['mad']:
+      MADdir = os.path.join(inv_dict['save_dir'], "MAD") #if MADdir is None else MADdir
+      inv_dict['mad_fn'] = os.path.join(MADdir, getBaseName(inv_dict['graph_fn']) + '_mad.npy')
+      createSave(inv_dict['mad_fn'], max_ave_deg)
+      print 'Maximum average Degree saved as ' + inv_dict['mad_fn']
 
-  ''' Scan Statistic 1'''
-  if inv_dict['ss1']:
-    ss1Dir = os.path.join(inv_dict['save_dir'], "SS1") #if ss1Dir is None else ss1Dir
-    inv_dict['ss1_fn'] = os.path.join(ss1Dir, getBaseName(inv_dict['graph_fn']) + '_scanstat1.npy')
-    createSave(inv_dict['ss1_fn'], ss1_array) # save it
-    print 'Scan 1 statistic saved as ' + inv_dict['ss1_fn']
+    ''' Scan Statistic 1'''
+    if inv_dict['ss1']:
+      ss1Dir = os.path.join(inv_dict['save_dir'], "SS1") #if ss1Dir is None else ss1Dir
+      inv_dict['ss1_fn'] = os.path.join(ss1Dir, getBaseName(inv_dict['graph_fn']) + '_scanstat1.npy')
+      createSave(inv_dict['ss1_fn'], ss1_array) # save it
+      print 'Scan 1 statistic saved as ' + inv_dict['ss1_fn']
 
-  ''' Clustering coefficient '''
-  if inv_dict['cc']:
-    ccDir = os.path.join(inv_dict['save_dir'], "ClustCoeff") #if ccDir is None else ccDir
-    inv_dict['cc_fn'] = os.path.join(ccDir, getBaseName(inv_dict['graph_fn']) + '_clustcoeff.npy')
-    createSave(inv_dict['cc_fn'], cc_array) # save it
-    print 'Clustering coefficient saved as ' + inv_dict['cc_fn']
+    ''' Clustering coefficient '''
+    if inv_dict['cc']:
+      ccDir = os.path.join(inv_dict['save_dir'], "ClustCoeff") #if ccDir is None else ccDir
+      inv_dict['cc_fn'] = os.path.join(ccDir, getBaseName(inv_dict['graph_fn']) + '_clustcoeff.npy')
+      createSave(inv_dict['cc_fn'], cc_array) # save it
+      print 'Clustering coefficient saved as ' + inv_dict['cc_fn']
 
-  ''' Global Vertices '''
-  if inv_dict['ver']:
-    vertDir = os.path.join(inv_dict['save_dir'], "Globals") #if vertDir is None else vertDir
-    inv_dict['ver_fn'] = os.path.join(vertDir, getBaseName(inv_dict['graph_fn']) + '_numvert.npy')
-    createSave(inv_dict['ver_fn'], num_nodes) # save it
-    print 'Global vertices number saved as ' + inv_dict['ver_fn']
+    ''' Global Vertices '''
+    if inv_dict['ver']:
+      vertDir = os.path.join(inv_dict['save_dir'], "Globals") #if vertDir is None else vertDir
+      inv_dict['ver_fn'] = os.path.join(vertDir, getBaseName(inv_dict['graph_fn']) + '_numvert.npy')
+      createSave(inv_dict['ver_fn'], num_nodes) # save it
+      print 'Global vertices number saved as ' + inv_dict['ver_fn']
 
-  ''' Global number of edges '''
-  if inv_dict['edge']:
-    edgeDir = os.path.join(inv_dict['save_dir'], "Globals") #if edgeDir is None else edgeDir
-    inv_dict['edge_fn'] = os.path.join(edgeDir, getBaseName(inv_dict['graph_fn']) + '_numedges.npy')
-    createSave(inv_dict['edge_fn'], edge_count) # save it
-    print 'Global edge number saved as ' + inv_dict['edge_fn']
+    ''' Global number of edges '''
+    if inv_dict['edge']:
+      edgeDir = os.path.join(inv_dict['save_dir'], "Globals") #if edgeDir is None else edgeDir
+      inv_dict['edge_fn'] = os.path.join(edgeDir, getBaseName(inv_dict['graph_fn']) + '_numedges.npy')
+      createSave(inv_dict['edge_fn'], edge_count) # save it
+      print 'Global edge number saved as ' + inv_dict['edge_fn']
 
   #if test: # bench test
   #  tri_fn = os.path.join('bench', str(G.shape[0]), getBaseName(inv_dict['graph_fn']) + '_triangles.npy')
@@ -238,8 +239,11 @@ def compute(inv_dict, save=True):
 
 def populate_inv_dict(arg):
   """ Ensures the invariant dictionary input contains all necessary fields in order to not raise keyError exceptions"""
+
+  #begin = time()
+
   fns = ['ss1_fn', 'tri_fn', 'deg_fn', 'ss2_fn', 'apl_fn', 'ver_fn',
-          'mad_fn', 'gdia_fn', 'cc_fn', 'eigvl_fn', 'eigvect_fn', 'edge_fn']
+          'mad_fn', 'gdia_fn', 'cc_fn', 'eigvl_fn', 'eigvect_fn', 'edge_fn', 'save_dir']
   invs = ['ss1', 'tri', 'deg', 'ss2', 'apl', 'ver', 'eig',
           'mad', 'gdia', 'cc', 'eigvl', 'eigvect', 'edge']
 
@@ -261,6 +265,9 @@ def populate_inv_dict(arg):
   for dat in data:
     if not arg.has_key(dat):
       arg[dat] = None
+
+  #print "Time to populate invariant dict = %.4f" % (time()-begin)
+
   return arg
 
 if __name__ == '__main__':
