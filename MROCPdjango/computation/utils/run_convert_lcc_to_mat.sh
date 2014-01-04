d8a76693581fc8c38cacae2e7417346738b7de0d
Disa <dmhembe1@jhu.edu>
2014-01-04 02:26:09 -0500

    create mat w adj mat for lcc graphs
    Former-commit-id: 48d33d24ffdeab832ce5568e470bff20a6fc8859 [formerly a74c2b5e7b541e8b826e9924c66b072230069a3e] [formerly eafb36a6e0d9a199824d8ef846fe051d06b7f783 [formerly 5ec9f07b31952bd8dcb9fcc396656458c8f65c5c]]
    Former-commit-id: 4bc0c1e99386b88a828a2b667f1335683333cd93
    Former-commit-id: 241b5c3c1f42290deaee09014b33cd587e750e6b

---
+++ MROCPdjango/computation/utils/run_convert_lcc_to_mat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+directories="/data/public/MR/MIGRAINE/KKI-42_MIGRAINE_v1_0_2013-09-25/big_graphs
+/data/public/MR/MIGRAINE/MRN-111_MIGRAINE_v1_0_2013-10-18/big_graphs
+/data/public/MR/MIGRAINE/NKI-24_MIGRAINE_v1_0_2013-09-11/big_graphs
+/data/public/MR/MIGRAINE/NKI-ENH_MIGRAINE_v1_0_2013-10-27/big_graphs
+"
+for f in $directories
+do
+  python convert_lcc_to_mat.py $f
+done
+
