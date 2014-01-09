293f129a1fc0146b81a778be02768e1520550610
Disa Mhembere <dmhembe1@jhu.edu>
2014-01-09 14:15:19 -0500

    Able to build igraphs for big and small graphs
    Former-commit-id: de1502d093ed355779f6f7db9ad6747d7bb8752b [formerly 6e5c97c6b49346a701f687be85e2c1871cc327cc] [formerly 09769063348f2ad386d5baf82380d354404f6698 [formerly 86b0ee39cb7e877e6b11faae088e0f11df4783d4]]
    Former-commit-id: 45e61f8439573aa375409079b93318ef4d6041dc
    Former-commit-id: 89dc61ce76660b7eff9a3b5ce571ac6ee637d83d

--- mrcap/fibergraph_sm.py
+++ mrcap/fibergraph_sm.py
@@ -1,58 +1,35 @@
-from scipy.sparse import lil_matrix, csc_matrix
-from scipy.io import loadmat, savemat
+# Class holding small fibergraphs
+# @author Randal Burns, Disa Mhembere
+
 from mrcap.fiber import Fiber
 import mrcap.roi as roi
-import mrcap.mask as mask
+#import mrcap.mask as mask
 import zindex
 import math
 import itertools
-from cStringIO import StringIO
 import igraph
-from time import time
-"""
- fibgergraph_sm provides the same interfaces as fibergraph but
-  makes the 70 x 70 matrix
+from collections import defaultdict
+from fibergraph import _FiberGraph
 
+# Class functions documented in fibergraph.py
 
-  This routine uses two different sparse matrix representations.
-  The calls need to be performed in the following order
-
-    Create FiberGraph
-    Add Edges (lil format)
-    Complete -- this converts from dictionary of keys format to Column CSC
-    Write or SVD (on csc format)
-
-"""
-
-class FiberGraph:
-  """
-    Sparse matrix representation of a Fiber graph
-  """
-
-  def __init__(self, matrixdim, rois, mask ):
+class FiberGraph(_FiberGraph):
+  def __init__(self, matrixdim, rois, mask):
     """
      Constructor: number of nodes in the graph
        convert it to a maximum element
     """
-
     # Regions of interest
     self.rois = rois
-    self.sorted_edges = [] # Will hold all edges to be added to the graph in self.complete
-
-    # Brainmask
-#    self.mask = mask
+    # Edges
+    self.edge_dict = defaultdict(int) # Will have key=(v1,v2), value=weight
 
     # Get the maxval from the number of rois
 #    self._maxval = rois.maxval()
-    self._maxval = 70
+    self._maxval = 70 # FIXME: Hardcoded
 
-    # ======================================================================== #
     # list of list matrix for one by one insertion
-    self.spcscmat = igraph.Graph(n=self._maxval, directed=True) # make new igraph with adjacency matrix to be (maxval X maxval)
-    # ======================================================================== #
-
-    # empty CSC matrix
-    #self.spcscmat = csc_matrix ( (self._maxval, self._maxval), dtype=float )
+    self.spcscmat = igraph.Graph(n=self._maxval, directed=False) # make new igraph with adjacency matrix to be (maxval X maxval)
 
   def __del__(self):
     """
@@ -60,7 +37,7 @@ class FiberGraph:
     """
     pass
 
-  def add ( self, fiber ):
+  def add (self, fiber):
     """
     Add edges associated with a single fiber of the graph
 
@@ -70,7 +47,7 @@ class FiberGraph:
     """
 
     # Get the set of voxels in the fiber
-    allvoxels = fiber.getVoxels ()
+    allvoxels = fiber.getVoxels()
 
     roilist = []
     # Use only the important voxels
@@ -81,60 +58,10 @@ class FiberGraph:
        roival = self.rois.get(xyz)
        # if it's an roi and in the brain
        if roival:
-         roilist.append ( roi.translate( roival ) )
+         roilist.append(roi.translate(roival))
 
-    roilist = set ( roilist )
-
+    roilist = set(roilist)
     roi_edges = itertools.combinations((roilist),2)
 
     for list_item in roi_edges:
-      self.sorted_edges.append(tuple(sorted(list_item)))
-
-  #
-  # Complete the graph.  Get it ready for analysis.
-  #
-  def complete ( self ):
-    """Done adding fibers.  Prior to analysis"""
-    start = time()
-    print "Adding %d edges to the graph ..." % len(self.sorted_edges)
-    self.spcscmat += self.sorted_edges
-    print "Completed adding edges in %.3f sec" % ( time () - start)
-
-
-  #
-  #  Write the sparse matrix out in a format that can be reingested.
-  #  fout should be an open file handle
-  #
-  def saveToMatlab ( self, key, filename ):
-    """Save the sparse array to disk in the specified file name"""
-
-    if 0 == self.spcscmat.getnnz():
-      print "Call complete after adding all edges"
-      assert 0
-
-    print "Saving key ", key, " to file ", filename
-    savemat( filename , {key: self.spcscmat})
-
-  #
-  #  Write the sparse matrix out in a format that can be reingested.
-  #  fout should be an open file handle
-  #
-  def loadFromMatlab ( self, key, filename ):
-    """Load the sparse array from disk by file name"""
-
-    print "Reading key ", key, " from file ", filename
-
-    # first convert to csc
-    t = loadmat ( filename  )
-    self.spcscmat = t[key]
-
-  # ========================================================================== #
-  def saveToIgraph( self, filename, format="picklez" ):
-    """ Save igraph sparse matrix """
-    self.spcscmat.save( filename, format=format )
-
-  def loadFromIgraph( self, filename, format="picklez" ):
-    """ Load a sparse matrix from igraph as a numpy pickle """
-    igraph.load( filename, format=format )
-
-  # ========================================================================== #
+      self.edge_dict[tuple(sorted(list_item))] += 1
