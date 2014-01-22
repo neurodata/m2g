fefd0bd0a2db92ff798155e284108f43a0ae964e
Disa <dmhembe1@jhu.edu>
2014-01-22 14:38:14 -0500

    small graphs graphml done
    Former-commit-id: edc34e6392674a900d0ef682a68b56ce97ddfd19 [formerly a0c3f41a9174014678cbfac6c374b05fb10c86ad] [formerly 8e461acb727224dfb036ea8afcb9043a57b1f791 [formerly 7150432870dc2e6084e41c2e516cb397a500dc19]]
    Former-commit-id: 238a32188da057033f98b3022abac441c85bd0df
    Former-commit-id: beb6079e987fbd818c40bef7c98807a4236a00a6

--- MROCPdjango/computation/executors/run_csc_to_graphml_on_dir.py
+++ MROCPdjango/computation/executors/run_csc_to_graphml_on_dir.py
@@ -7,26 +7,25 @@
 
 import argparse
 from computation.utils import graphml_adapter
-from computation.utils.loadAdjMatrix import loadAnyMat
+from computation.utils.file_util import loadAnyMat
 from glob import glob
 import os
 
 def main():
   parser = argparse.ArgumentParser(description="Convert a directory of CSC graphs to graphml format")
-  parser.add_argument("directory", action="store", nargs="+", help="The directory(ies) we should run on")
+  parser.add_argument("directory", action="store", nargs="+", help="The directory(ies) we should run on. *Don't add '/' to end of dir name")
   parser.add_argument("-w", "--weighted", action="store_true", help="Pass flag if the graphs are weighted")
   result = parser.parse_args()
 
   for dircty in result.directory:
     new_dir = dircty+"_graphml"
-    print "Making dir %s" % new_dir
-    os.makedirs(new_dir)
+    if not os.path.exists(new_dir): os.makedirs(new_dir); print "Making dir %s ..." % new_dir
+    else: print "Dir %s already exists ..." % new_dir
     for fn in glob(os.path.join(dircty, "*.mat")):
       print "Converting %s ..." % fn
       new_fn = os.path.join(new_dir, os.path.splitext(os.path.basename(fn))[0]+".graphml")
       print "Creating %s ..." % new_fn
       graphml_adapter.csc_to_graphml(loadAnyMat(fn), is_weighted=result.weighted, desikan=True, is_directed=False, save_fn=new_fn)
-
 
 if __name__ == "__main__":
   main()
