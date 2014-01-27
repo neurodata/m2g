af5672ac6d714585d70137ac6cf60be2488fd7a8
Disa Mhembere <dmhembe1@jhu.edu>
2014-01-26 22:38:23 -0500

    stand-alone examples updated
    Former-commit-id: 4f4bcef78ff415685a9fa3ede8e8aedc6da50c91 [formerly 767cfb5f0510ac14c871999e901062408038d2ae] [formerly 1394961ff18d2b18f4c8cfc40f9af908551759a4 [formerly 5df0afacdfe86cba0c56d7c64ae78d8829b6a42f]]
    Former-commit-id: b16a8d67de7cc56ef7a0a04154e8de98d32dba06
    Former-commit-id: 4ce5214b2f4d1be70f5c186186acdf188e47460b

--- MROCPdjango/computation/composite/examples/example.sh
+++ MROCPdjango/computation/composite/examples/example.sh
@@ -2,61 +2,61 @@
 echo "Example downloads ./testdata directory from http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/ ..."
 echo "Example script running ..."
 
-if [ -f ./testdata/test_fiber.dat ]
+# VAR DECL
+TEST_DATA=./testdata
+FIBER=./testdata/test_fiber.dat
+ROI_XML=./testdata/test_roi.xml
+ROI_RAW=./testdata/test_roi.raw
+SMALL_DIR=./testdata/small
+BIG_DIR=./testdata/big
+
+SMALL_GR_FN=test_fiber_small.graphml
+BIG_GR_FN=test_fiber_big.graphml
+
+if [ -f $FIBER ]
 then
-  echo "./testdata/test_fiber.dat exists ..."
+  echo "$FIBER exists ..."
 else
-  if [ ! -d "$mkdir"];
+  if [ ! -d "$mkdir"]
   then
-    echo "making testdata directory ..."
-    mkdir ./testdata
+    echo "making $TEST_DATA directory ..."
+    mkdir $TEST_DATA
   fi
-  echo "downloading test_fiber.dat file..."
-  wget --output-document=./testdata/test_fiber.dat http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_fiber.dat
+  echo "downloading $FIBER file..."
+  wget --output-document=$FIBER http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_fiber.dat
 fi
 
-if [ -f ./testdata/test_roi.raw ]
+if [ -f $ROI_RAW ]
 then
-  echo "./testdata/test_roi.raw exists ..."
+  echo "$ROI_RAW exists ..."
 else
-  echo "downloading test_roi.raw file..."
-  wget --output-document=./testdata/test_roi.raw http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_roi.raw
+  echo "downloading $ROI_RAW file..."
+  wget --output-document=$ROI_RAW http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_roi.raw
 fi
 
-if [ -f ./testdata/test_roi.xml ]
+if [ -f $ROI_XML ]
 then
-  echo "./testdata/test_roi.xml exists ..."
+  echo "$ROI_XML exists ..."
 else
-  echo "downloading test_roi.xml file..."
-  wget --output-document=./testdata/test_roi.xml http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_roi.xml
+  echo "downloading $ROI_XML file..."
+  wget --output-document=$ROI_XML http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_roi.xml
 fi
-
 
 # SMALL
 # Graph generation:
 echo "../graph_exec -h. For help"
 # Build a small graph given fiber streamline: test_fiber, fiber ROI's: test_roi.xml and test_roi.raw
-.././graph_exec ./testdata/test_fiber.dat ./testdata/test_roi.xml ./testdata/test_roi.raw  -S ./testdata/small
+.././graph_exec $FIBER $ROI_XML $ROI_RAW -S $SMALL_DIR -g $SMALL_GR_FN
 
 # Invariants:
 echo "./inv_exec -h. For help"
-.././inv_exec ./testdata/small/test_fiber_70_smgr.mat -D fibergraph -A -S ./testdata/small
-
-# largest connected component & single value:
-echo "./deriv_exec -h. For help"
-# Compute LCC & SVD
-.././deriv_exec ./testdata/small/test_fiber_70_smgr.mat -v -l -S ./testdata/small
+.././inv_exec $SMALL_DIR/$SMALL_GR_FN -A -S $SMALL_DIR 
 
 # BIG
 # Uncomment to Build big graph WARNING: Can take up to 6 hours to complete
-#.././graph_exec ./testdata/test_fiber.dat ./testdata/test_roi.xml ./testdata/test_roi.raw  -S ./testdata/big -b -g bigtest_bggr.mat
-
-# largest connected component & single value:
-#echo "./deriv_exec -h. For help"
-# Compute LCC & SVD
-#.././deriv_exec ./testdata/big/bigtest_bggr.mat -v -l -S ./testdata/big
+.././graph_exec $FIBER $ROI_XML $ROI_RAW -S $BIG_DIR -b -g $BIG_GR_FN
 
 # Invariants:
-#echo "./inv_exec -h. For help"
-#.././inv_exec ./testdata/big/bigtest_bggr.mat -D fibergraph -A -S ./testdata/big -lf ./testdata/big/LCC/bigtest_concomp.npy
+echo "./inv_exec -h. For help"
+.././inv_exec $BIG_DIR/$BIG_GR_FN -A -S $BIG_DIR 
 
