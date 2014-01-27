08672c08125284ee7bbdcd2956823385d0a8e613
Disa Mhembere <dmhembe1@jhu.edu>
2014-01-26 23:25:41 -0500

    Tested stand-alone examples on server. Removed eigs comp for big graphs
    Former-commit-id: 06b67dee8fb5c8ce28cf3b177e43c10046dea744 [formerly 2b8c7e3057e451ce8f1251cc6da88a75be06bd6f] [formerly 8e8758cf8b5b9016c93b7fa3fa006697535c96d7 [formerly 2a3783873a17f1dcd6badaa91c32b55d86d3aefc]]
    Former-commit-id: 7a968bea9d1d702776d510e8f27683d253a62e8d
    Former-commit-id: 71e65589392c4d993f135951b56c6f44fce3eef8

--- MROCPdjango/computation/composite/examples/example.sh
+++ MROCPdjango/computation/composite/examples/example.sh
@@ -4,25 +4,26 @@ echo "Example script running ..."
 
 # VAR DECL
 TEST_DATA=./testdata
-FIBER=./testdata/test_fiber.dat
-ROI_XML=./testdata/test_roi.xml
-ROI_RAW=./testdata/test_roi.raw
-SMALL_DIR=./testdata/small
-BIG_DIR=./testdata/big
+FIBER=$TEST_DATA/test_fiber.dat
+ROI_XML=$TEST_DATA/test_roi.xml
+ROI_RAW=$TEST_DATA/test_roi.raw
+SMALL_DIR=$TEST_DATA/small
+BIG_DIR=$TEST_DATA/big
 
 SMALL_GR_FN=test_fiber_small.graphml
 BIG_GR_FN=test_fiber_big.graphml
+
+if [ ! -d $TEST_DATA ]
+then
+  echo "making $TEST_DATA directory ..."
+  mkdir $TEST_DATA
+fi
 
 if [ -f $FIBER ]
 then
   echo "$FIBER exists ..."
 else
-  if [ ! -d "$mkdir"]
-  then
-    echo "making $TEST_DATA directory ..."
-    mkdir $TEST_DATA
-  fi
-  echo "downloading $FIBER file..."
+  echo "Downloading $FIBER file..."
   wget --output-document=$FIBER http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_fiber.dat
 fi
 
@@ -30,7 +31,7 @@ if [ -f $ROI_RAW ]
 then
   echo "$ROI_RAW exists ..."
 else
-  echo "downloading $ROI_RAW file..."
+  echo "Downloading $ROI_RAW file..."
   wget --output-document=$ROI_RAW http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_roi.raw
 fi
 
@@ -38,7 +39,7 @@ if [ -f $ROI_XML ]
 then
   echo "$ROI_XML exists ..."
 else
-  echo "downloading $ROI_XML file..."
+  echo "Downloading $ROI_XML file..."
   wget --output-document=$ROI_XML http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/test_roi.xml
 fi
 
@@ -46,17 +47,16 @@ fi
 # Graph generation:
 echo "../graph_exec -h. For help"
 # Build a small graph given fiber streamline: test_fiber, fiber ROI's: test_roi.xml and test_roi.raw
-.././graph_exec $FIBER $ROI_XML $ROI_RAW -S $SMALL_DIR -g $SMALL_GR_FN
+../graph_exec $FIBER $ROI_XML $ROI_RAW -S $SMALL_DIR -g $SMALL_GR_FN
 
 # Invariants:
 echo "./inv_exec -h. For help"
-.././inv_exec $SMALL_DIR/$SMALL_GR_FN -A -S $SMALL_DIR 
+../inv_exec $SMALL_DIR/$SMALL_GR_FN -A -S $SMALL_DIR 
 
 # BIG
 # Uncomment to Build big graph WARNING: Can take up to 6 hours to complete
-.././graph_exec $FIBER $ROI_XML $ROI_RAW -S $BIG_DIR -b -g $BIG_GR_FN
+../graph_exec $FIBER $ROI_XML $ROI_RAW -S $BIG_DIR -b -g $BIG_GR_FN
 
 # Invariants:
 echo "./inv_exec -h. For help"
 .././inv_exec $BIG_DIR/$BIG_GR_FN -A -S $BIG_DIR 
-
