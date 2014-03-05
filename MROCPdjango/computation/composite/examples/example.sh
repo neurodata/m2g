5b74373ee005089c312ccef5d0b8600786a80903
Disa Mhembere <dmhembe1@jhu.edu>
2014-03-05 00:06:13 -0500

    Mod in example
    Former-commit-id: ce840451d4df1260ef7c40d86fe8cb86a4c4f6a5 [formerly ae04111a7c429e7e71d8dc32d2ac61c8c48f5499] [formerly e3e2a67281f29459a2ff6f4d88cf0ecc4f41e886 [formerly a692359edbf811fb3c87e97eb83db9ff0c2f78be]]
    Former-commit-id: 90f937ea997eb2b91bbb9f7ffc9e2ba0df9791b8
    Former-commit-id: 0e4b743ecca2b4327f3ed60d932561a25a0a0052

--- MROCPdjango/computation/composite/examples/example.sh
+++ MROCPdjango/computation/composite/examples/example.sh
@@ -2,6 +2,9 @@
 echo "Example downloads ./testdata directory from http://mrbrain.cs.jhu.edu/data/projects/disa/OCPprojects/testdata/ ..."
 echo "Example script running ..."
 
+# Exit script on first error
+set -e 
+
 # VAR DECL
 TEST_DATA=./testdata
 FIBER=$TEST_DATA/test_fiber.dat
@@ -47,16 +50,20 @@ fi
 # Graph generation:
 echo "../graph_exec -h. For help"
 # Build a small graph given fiber streamline: test_fiber, fiber ROI's: test_roi.xml and test_roi.raw
-../graph_exec $FIBER $ROI_XML $ROI_RAW -S $SMALL_DIR -g $SMALL_GR_FN
+python ../graph_exec $FIBER $ROI_XML $ROI_RAW -S $SMALL_DIR -g $SMALL_GR_FN
 
 # Invariants:
 echo "./inv_exec -h. For help"
-../inv_exec $SMALL_DIR/$SMALL_GR_FN -A -S $SMALL_DIR 
+python ../inv_exec $SMALL_DIR/$SMALL_GR_FN -A -S $SMALL_DIR 
 
 # BIG
-# Uncomment to Build big graph WARNING: Can take up to 6 hours to complete
-../graph_exec $FIBER $ROI_XML $ROI_RAW -S $BIG_DIR -b -g $BIG_GR_FN
+# Uncomment to Build big graph WARNING: Can take up to 2 hours to complete
+if [[ $1 == "-b" ]]; then
+  python ../graph_exec $FIBER $ROI_XML $ROI_RAW -S $BIG_DIR -b -g $BIG_GR_FN
 
-# Invariants:
-echo "./inv_exec -h. For help"
-.././inv_exec $BIG_DIR/$BIG_GR_FN -A -S $BIG_DIR 
+  # Invariants:
+  echo "./inv_exec -h. For help"
+  python .././inv_exec $BIG_DIR/$BIG_GR_FN -A -S $BIG_DIR 
+else
+  echo 'To build a big graph pass -b as the 1 argument to the example script'
+fi
