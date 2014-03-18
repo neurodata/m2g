89ab56a7757307851f5651cf8bb0917c863e915d
Disa Mhembere <dmhembe1@jhu.edu>
2014-03-18 15:49:07 -0400

    No centroids for desikan
    Former-commit-id: 5d30b77ae34fabd7ca0f344817ed234b55eac552 [formerly 6cf3128b02c2c2592fd380321f0a46578fe50113] [formerly a15d5fcf6903fc947ae4e032e2314e093cade334 [formerly 0cca179a372c8c5d7c8f093968a9b210cb761942]]
    Former-commit-id: 3717180807575c0dacd1356d760b5136c662e2bb
    Former-commit-id: a47f56ff796f6494c28306a4732c8861ecb5eea6

---
+++ mrcap/atlas.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+# atlas.py
+# Created by Disa Mhembere on 2014-01-08.
+# Email: disa@jhu.edu
+# Copyright (c) 2014. All rights reserved.
+
+from zindex import MortonXYZ
+import scipy.io as sio
+import nibabel as nib
+import os
+
+class Atlas(object):
+  def __init__(self, atlas_fn, label_fn=None):
+    self.data = nib.load(os.path.abspath(atlas_fn)).get_data()
+    if label_fn: 
+      label_file = open(label_fn, "rb")
+      self.region_names = label_file.read().splitlines()
+    else:
+      self.region_names = None
+
+  def get_region_num(self, vertex):
+    x,y,z = MortonXYZ(vertex)
+    return (self.data[x, y, z])
+
+  def get_region_name(self, region_num):
+    assert self.region_names, "No atlas region names file specified"
+    return self.region_names[int(region_num) - 1] # -1 for the 1-based indexing 
+
+  def get_all_mappings(self, vertices):
+    region_nums = []
+    region_names = [] 
+    for vertex in vertices:
+      region_num = self.get_region_num(vertex)
+      region_nums.append(region_num)
+      if self.region_names: region_names.append(self.get_region_name(region_num))
+    
+    return region_nums, region_names
--- mrcap/fibergraph_bg.py
+++ mrcap/fibergraph_bg.py
@@ -1,19 +1,22 @@
 # Class holding big fibergraphs
 # @author Randal Burns, Disa Mhembere
 
-from mrcap.fiber import Fiber
-import mrcap.roi as roi
-import scipy.io as sio
-import zindex
 import math
 import itertools
-import igraph
-from collections import defaultdict
-from mrcap.fibergraph import _FiberGraph
 from time import time
 import os
-from mrcap.desikan import DesMap
+from collections import defaultdict
+
 import numpy as np
+import igraph
+import scipy.io as sio
+
+from mrcap.fiber import Fiber
+import mrcap.roi as roi
+from mrcap.fibergraph import _FiberGraph
+from mrcap.atlas import Atlas
+import zindex
+
 # Class functions documented in fibergraph.py
 
 class FiberGraph(_FiberGraph):
@@ -77,7 +80,7 @@ class FiberGraph(_FiberGraph):
     for list_item in voxel_edges:
       self.edge_dict[tuple(sorted(list_item))] += 1
 
-  def complete(self, ):
+  def complete(self, add_centroids=True, atlases={}):
     super(FiberGraph, self).complete()
     print "Annotating vertices with spatial position .."
     self.spcscmat.vs["position"] = range(self._maxval) # Use position for
@@ -85,19 +88,25 @@ class FiberGraph(_FiberGraph):
     print "Deleting zero-degree nodes..."
     zero_deg_nodes = np.where( np.array(self.spcscmat.degree()) == 0 )[0]
     self.spcscmat.delete_vertices(zero_deg_nodes)
+  
+    for idx, atlas_name in enumerate(atlases.keys()):
+      self.spcscmat["Atlas_"+ os.path.splitext(os.path.basename(atlas_name))[0]+"_index"] = idx
+      print "Adding '%s' region numbers (and names) ..." % atlas_name
+      atlas = Atlas(atlas_name, atlases[atlas_name])
+      region = atlas.get_all_mappings(self.spcscmat.vs["position"])
+      self.spcscmat.vs["atlas_%d_region_num" % idx] = region[0]
 
-    print "Adding desikan labels ..."
-    lf = os.path.join(os.path.abspath(os.path.dirname(__file__)),"utils", "MNI_labels.mat")
-    des_map = DesMap(label_fn=lf)
-    region = des_map.get_all_mappings(self.spcscmat.vs["position"])
-    self.spcscmat.vs["region"] = region
+      if region[1]: self.spcscmat.vs["atlas_%d_region_name" % idx] = region[1]
+    
+    # FIXME: The problem is these centroids are associated with the desikan label ONLY.
+    """
+    if add_centroids:
+      print "Adding centroids ..."
+      cent_map = sio.loadmat(os.path.join(os.path.abspath(os.path.dirname(__file__)),"utils", "centroids.mat"))["centroids"]
+      keys = des_map.get_desikan_keys(self.spcscmat.vs["position"])
+      centroids = []
+      for key in keys:
+         centroids.append(str(list(cent_map[key])))
 
-    print "Adding centroids ..."
-    cent_map = sio.loadmat(os.path.join(os.path.abspath(os.path.dirname(__file__)),"utils", "centroids.mat"))["centroids"]
-    keys = des_map.get_desikan_keys(self.spcscmat.vs["position"])
-    centroids = []
-    for key in keys:
-       centroids.append(str(list(cent_map[key])))
-
-    self.spcscmat.vs["centroid"] = centroids
-
+      self.spcscmat.vs["centroid"] = centroids
+    """
