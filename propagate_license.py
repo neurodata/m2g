0a7194afa3e66ced45523a5251d5b5d8634f3ced
Disa Mhembere <dmhembe1@jhu.edu>
2014-05-20 12:40:04 -0400

    License propagator fixes
    Former-commit-id: 438db4eddbde4e9b744f782a91050f797f012531 [formerly 15e398fcb1b2b7395c721da5d99233e8a07ac7bd] [formerly 50c587c33e63df2ba7ece0dff804305215b8af00 [formerly b67b7b6fdfd111abb0bc7e38f3ceced72d99e0e8]]
    Former-commit-id: 484dac9e0f5b7ed91c318ab68f0d62c7d6e56ebb
    Former-commit-id: b365b070aa8f3ca30be3e9f4ca6954161ae8eb7e

--- propagate_license.py
+++ propagate_license.py
@@ -18,7 +18,7 @@
 # Created by Disa Mhembere on 2014-05-16.
 # Email: disa@jhu.edu
 
-__licence_header__ = """
+__license_header__ = """
 {} Copyright 2014 Open Connectome Project (http://openconnecto.me)
 {}
 {} Licensed under the Apache License, Version 2.0 (the "License");
@@ -35,42 +35,39 @@ __licence_header__ = """
 {}
 """
 COMM_COUNT = 14
-comm = {".py":"#", ".pyx":"#", "": "#", ".html":"", ".sh":"#", ".R":"#", ".mat":"%", ".c":"//",
+comm = {".py":"#", ".pyx":"#", "": "#", ".html":"", ".sh":"#", ".r":"#", ".mat":"%", ".c":"//",
         ".c++":"//", ".java":"//", ".js":"//"}
 
 import argparse
 import os
 import pdb
 
-def add(dirname, file_exts):
-  files_to_edit = []
+def add(files):
+  global __license_header__
 
-  for root, dirnames, filenames in os.walk(os.path.abspath(dirname)):
-    for filename in filenames:
-      full_fn = os.path.join(root, filename)
-      if os.path.isfile(full_fn) and not os.path.basename(full_fn).startswith(".") \
-          and not os.path.basename(root).startswith(".") and ( os.path.splitext(full_fn)[-1].upper().strip() in file_exts ):
-        print "Processing file: %s ..." % full_fn
-        script = open(full_fn, "rb")
-        lines = script.read().splitlines()
-        script.close()
-        # Exception for html
-        if os.path.splitext(full_fn)[1].upper().strip() == ".HTML":
-          __licence_header__ = "<!-- " + __licence_header__ + " -->"
+  for full_fn in files:
+    license_header = __license_header__
 
-        comment_style = comm[os.path.splitext(full_fn)[1]]
-        if lines[0] == "#!/usr/bin/python":
-          if lines[5].startswith("# Copyright"): # get rid of copyright year
-            del lines[5], lines[1]
+    print "Processing file: %s ..." % full_fn
+    script = open(full_fn, "rb")
+    lines = script.read().splitlines()
+    script.close()
+    # Exception for html
 
-          lines.insert(1, __licence_header__.format(*([comment_style]*COMM_COUNT)))
-        else:
-          lines.insert(0, __licence_header__.format(*([comment_style]*COMM_COUNT)))
+    comment_style = comm[os.path.splitext(full_fn)[1]]
+    if lines[0] == "#!/usr/bin/python":
+      if lines[5].startswith("# Copyright"): # get rid of copyright year
+        del lines[5], lines[1]
 
-        script = open(full_fn, "wb")
-        script.write("\n".join(lines))
+      lines.insert(1, license_header.format(*([comment_style]*COMM_COUNT)))
+    else:
+      license_header += "{} Created by Disa Mhembere\n{} Email: disa@jhu.edu".format(*([comment_style]*2))
+      if os.path.splitext(full_fn)[1].lower().strip() == ".html":
+        license_header = "<!-- " + license_header + " -->"
 
-  print "\n".join(files_to_edit)
+      lines.insert(0, license_header.format(*([comment_style]*COMM_COUNT)))
+    script = open(full_fn, "wb")
+    script.write("\n".join(lines))
 
 def rm(dirname):
   pass
@@ -79,13 +76,31 @@ def main():
   parser = argparse.ArgumentParser(description="Add or Update license headers to code")
   parser.add_argument("-r", "--remove", action="store_true", help="Remove the license")
   parser.add_argument("-d", "--dirname", action="store", default=".", help="Directory where to start walk")
+  parser.add_argument("-f", "--files", action="store", nargs="*", help="Files you want license added to")
   parser.add_argument("-e", "--file_exts", nargs="*", action="store", \
       default=[".py", ".pyx", ".html", ".sh", ".R", ".mat", ""], \
       help="File extensions to add to the files altered")
   
   result = parser.parse_args()
   
-  add(result.dirname, result.file_exts)
+  if result.files:
+    print "Licensing individual files ..."
+    add(result.files)
+    exit(1)
+  
+  else:
+    print "Licensing a directory of files ..."
+    files = []
+    for root, dirnames, filenames in os.walk(os.path.abspath(result.dirname)):
+      for filename in filenames:
+        full_fn = os.path.join(root, filename)
+        if os.path.isfile(full_fn) and not os.path.basename(full_fn).startswith(".") \
+            and not os.path.basename(root).startswith(".") and \
+            ( os.path.splitext(full_fn)[-1].lower().strip() in result.file_exts ):
+          files.append(full_fn) 
+
+    print files
+    add(files)
 
 if __name__ == "__main__":
   main()
