506130da1af0cbb593e386c0ea1473f55cd1f3ba
Disa Mhembere <dmhembe1@jhu.edu>
2014-07-10 10:45:47 -0400

    [Bug]: MM writer removed dealloc for commented out variables
    Former-commit-id: ee3c454ee26e7e57b0fc3a4172e05b29727075e0 [formerly 5e8d33d594017fd5045448d6e55bb090fb40a86f] [formerly 7502714a14cdec21f2d1bff6d8adc15b7a1d978e [formerly a789b659e9f77a680720c9458ea43b409203be58]]
    Former-commit-id: ab05c0c4ff441b7cb7364c375b4f783238f7d64c
    Former-commit-id: 0a5559eab94a8852c4fd829368a011e5d70ea704

--- MROCPdjango/computation/utils/write_mm.c
+++ MROCPdjango/computation/utils/write_mm.c
@@ -1,3 +1,18 @@
+/* Copyright 2014 Open Connectome Project (http://openconnecto.me)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 #include <igraph.h>
 #include <stdio.h>
 
@@ -69,11 +84,11 @@ int main(int argc, char* argv[])
   igraph_integer_t i;
   for (i = 0; i < (int long) igraph_ecount(&g); i++)
      printf("Edge %i value: %f\n", i, VECTOR(result)[i]);
-#endif
 
   // Free memory
   igraph_es_destroy(&eids);
   igraph_vector_destroy(&result);
+#endif
   igraph_destroy(&g);
 
   fclose(ofile);
