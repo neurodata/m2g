6f6da710213e98e5834a585a895ead79d312b2e9
Disa Mhembere <dmhembe1@jhu.edu>
2014-08-13 14:50:01 -0700

    [Algs]: Migrated graph algorithms to separate directories to match collaboration spec
    Former-commit-id: 7f21fffbe8fdff5e58a090bc2b0820097489ace2 [formerly c9c59a09418601af3bf2f5f6b1998357e6da0bc5] [formerly 72307d2df793b23f5065da8138ae6fe36d32cd18 [formerly 7cde13b9ceefde78f1ab1d5731bd89c1d3f198dd]]
    Former-commit-id: fb2fea22da6d6d244172507b04e21ce5b470c8a2
    Former-commit-id: 9c95d6fe4a0cd84c2af2169a54ef99db498fe960

---
+++ MROCPdjango/computation/algs/transitivity/transitivity.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+# Copyright 2014 Open Connectome Project (http://openconnecto.me)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# transitivity.py
+# Created by Disa Mhembere on 2014-08-03.
+# Email: disa@jhu.edu
+
+import os
+from computation.utils.file_util import createSave
+import rpy2.robjects as robjects
+from computation.utils.r_utils import r_igraph_set_vertex_attr
+
+def r_igraph_clust_coeff(g, save_fn=None):
+  """
+  Compute clustering coefficient of graph g and save as necessary
+
+  Positional arguments
+  ====================
+  g - The igraph loaded via Rpy2 so an R object
+
+  Optional arguments
+  ==================
+  save_fn - the filename you want to use to save it. If not provided
+  the graph adds a clustcoeff attribute to all nodes and returns.
+  """
+
+  clustcoeff = robjects.r("""
+  require(igraph)
+  fn <- function(g){
+  igraph::transitivity(g, "local")
+  }
+  """)
+  ccvector = clustcoeff(g)
+
+  if save_fn:
+    save_fn = os.path.abspath(save_fn)
+    createSave(save_fn, ccvector) # TODO: Clean input for programmatic access
+    print "Clustering Coefficient saved as %s ..." % save_fn
+  else:
+    g = r_igraph_set_vertex_attr(g, "clustcoeff", ccvector)  # Attribute name may need to change
+
+  return g # return so we can use for other attributes
