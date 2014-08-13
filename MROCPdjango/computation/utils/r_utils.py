dd2b4f33a772c176d6c536cdc9702ebd4c379a20
Disa Mhembere <dmhembe1@jhu.edu>
2014-08-13 14:50:01 -0700

    [Algs]: Migrated graph algorithms to separate directories to match collaboration spec
    Former-commit-id: 7f21fffbe8fdff5e58a090bc2b0820097489ace2 [formerly c9c59a09418601af3bf2f5f6b1998357e6da0bc5] [formerly 72307d2df793b23f5065da8138ae6fe36d32cd18 [formerly 7cde13b9ceefde78f1ab1d5731bd89c1d3f198dd]]
    Former-commit-id: fb2fea22da6d6d244172507b04e21ce5b470c8a2
    Former-commit-id: 9c95d6fe4a0cd84c2af2169a54ef99db498fe960

--- MROCPdjango/computation/utils/r_utils.py
+++ MROCPdjango/computation/utils/r_utils.py
@@ -20,6 +20,8 @@
 # Email: disa@jhu.edu
 
 import rpy2.robjects as robjects
+from rpy2.rinterface import NULL
+from rpy2.robjects.vectors import StrVector
 
 def r_igraph_load_graph(fn, gformat="graphml"):
   """
@@ -61,3 +63,129 @@ def r_igraph_get_adjacency(g):
        } """)
 
   return get_adj(g)
+
+    # =========== Graph, Node, Attr ============= #
+def r_igraph_set_vertex_attr(g, attr_name, value, index=NULL, is_str=False):
+  """
+  Set/Add an R vertex attribute to an R igraph object.
+  The vertex length must equal the number of nodes.
+
+  Positional arguments:
+  =====================
+  g - the R igraph object
+  attr_name - the name of the vetex attribute I want to add. Type: string
+  value - the R FloatVector containing the values to be added as attribute
+  """
+  set_vert_attr = robjects.r("""
+    require(igraph)
+    fn <- function(g, value, index){
+      if (!is.null(index)) {
+        V(g)[index]$%s <- value
+      } else {
+        V(g)$%s <- value
+      }
+      g
+    }
+    """ % (attr_name, attr_name))
+  if is_str:
+    return set_vert_attr(g, robjects.vectors.StrVector(value), index)
+  return set_vert_attr(g, value, index)
+
+def r_igraph_set_graph_attribute(g, attr_name, value):
+  """
+  Set/Add an igraph graph attribute in R.
+
+  g - the R igraph object
+  attr_name - the name of the vetex attribute I want to add. Type: string
+  value - the R FloatVector containing the values to be added as attribute
+  """
+
+  set_graph_attr = robjects.r("""
+  require(igraph)
+  fn <- function(g, value){
+  g$%s <- value
+  g
+  }
+  """ % attr_name)
+
+  return set_graph_attr(g, value)
+
+def r_igraph_ecount(g, set_attr=True):
+  """
+  Compute local triangle count of graph g and save as necessary
+  *Global graph attributes can only be stored in the graph*
+
+  Positional arguments
+  ====================
+  g - The igraph loaded via Rpy2 so an R object
+
+  Returns
+  =======
+  g - Same graph an input but with added invariant as an attribute
+  """
+  ecount = robjects.r("""
+  require(igraph)
+  fn <- function(g){
+  ecount(g)
+  }
+  """)
+  ec = ecount(g)[0]
+  if set_attr:
+    g = r_igraph_set_graph_attribute(g, "ecount", ec)
+    return g # return so we can use for other attributes
+  else:
+    return int(ec)
+
+def r_igraph_vcount(g, set_attr=True):
+  """
+  Get global vertex count of graph g and add as an attribut to graph
+  *Global graph attributes can only be stored in the graph*
+
+  Positional arguments
+  ====================
+  g - The igraph graph loaded via Rpy2 i.e. an R object
+
+  Returns
+  =======
+  g - Same graph an input but with added invariant as an attribute OR
+  vc - the actual vertex count
+  """
+  vcount = robjects.r("""
+  require(igraph)
+  fn <- function(g){
+  vcount(g)
+  }
+  """)
+  vc = vcount(g)[0]
+  if set_attr:
+    g = r_igraph_set_graph_attribute(g, "vcount", vc)
+    return g # return so we can use for other attributes
+
+  else:
+    return int(vc)
+
+def r_create_test_graph(nodes=5, edges=7):
+
+  ctg = robjects.r("""
+  require(igraph)
+  fn <- function(){
+  g <- erdos.renyi.game(%d, %d, type="gnm", directed=FALSE)
+  }
+  """ % (nodes, edges))
+
+def r_igraph_write(g, fn, gformat="graphml"):
+  """
+  Save an R igraph to disk via the R interface
+
+  fn - the file name on disk
+  gformat - the format to read from disk. Default: graphml
+  """
+  print "Writing %s to disk ..." % fn
+
+  write_graph = robjects.r("""
+        require(igraph)
+        fn <- function(g, fn, gformat){
+        write.graph(g, fn, format=gformat)
+       } """)
+
+  write_graph(g, fn, gformat)
