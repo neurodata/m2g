994ace7ad026440946c7bc8b3512501614092454
gkiar <gkiar07@gmail.com>
2015-01-26 15:28:21 -0500

    cleaning format
    Former-commit-id: 0491b0bc59f37a6140990fcb9860165449ea02a5 [formerly 90ea367061b7524112ae37015743024cc76eed67] [formerly 7d952e84eff18f473b7fd6825b0cff523e3da138 [formerly 63fe7af6267968b3d4f563a4a2452f406355902f]]
    Former-commit-id: f4649ee42243236aed2d395a11a00d63c89b221d
    Former-commit-id: e9f566277f95a73b2b9c266086109c1589bae3fb

--- M2G/src/tens2fibs.py
+++ M2G/src/tens2fibs.py
@@ -1,29 +1,53 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+# Copyright 2015 Open Connectome Project (http://openconnecto.me)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # tens2fibs.py
-# Tensors produced from DTI data are tracked into fibers
-# Version 0.1, G. Kiar, 01/09/2015
+# Created by Greg Kiar on 2015-01-09.
+# Email: gkiar@jhu.edu
+# Copyright (c) 2015. All rights reserved.
 
-# Load necessary packages
-from sys import argv
+import argparse
 import string
 import sys
 import re
 import os
 
-# Pull necessary parameters
-params = list(argv)
-dti_tensors = params[1] # The tensors produced from the DTI image (.Bdouble)
-dti_mask = params[2] # The brain mask of the DTI image (.nii, .nii.gz)
-anis = params[3] # Anisotropic voxel threshold value (default 0.2) (val)
-curve = params[4] # Curvature threshold (default 60) (val)
 
-dti_fibers = params[5] # Produced fiber tracts (.Bfloat)(output)
-vtk_fibers = params[6] # Converted fibers in visualizable format (.vtk)(output)
+def make_fibs(tensors, mask, anis, curve, fibers, vtk):
+  # Perfoms fiber tractography in voxelspace on the given tensors
+  os.system('track -inputmodel dt -seedfile '+mask+' -anisthresh '+anis+' -curvethresh '+curve+' -inputfile '+tensors+' > '+fibers+' -outputinvoxelspace 2>/home/gkiar/vbox/stderrorlog.txt')
+
+  # Converts the fibers to an easy-to-view format
+  os.system('vtkstreamlines -colourorient < '+fibers+' > '+vtk)
 
 
-# Perfoms fiber tractography in voxelspace on the given tensors
-os.system('track -inputmodel dt -seedfile '+dti_mask+' -anisthresh '+anis+' -curvethresh '+curve+' -inputfile '+dti_tensors+' > '+dti_fibers+' -outputinvoxelspace 2>/home/gkiar/vbox/stderrorlog.txt')
+def main():
+  parser = argparse.ArgumentParser(description="")
+  parser.add_argument("tensors", action="store", help="The tensors produced from the DTI image (.Bdouble)")
+  parser.add_argument("mask", action="store", help="The brain mask of DTI image (.nii, .nii.gz)")
+  parser.add_argument("anis", action="store", help="The anisotropic threshold value (default =0.2)")
+  parser.add_argument("curve", action="store", help="The curvature threshold value (default =60)") 
+  parser.add_argument("fibers", action="store", help="The produced fiber tracts (.Bfloat)")
+  parser.add_argument("vtk", action="store", help="The fibers in another format (.vtk)")
+  
+  result = parser.parse_args()
 
-# Converts the fibers to an easy-to-view format
-os.system('vtkstreamlines -colourorient < '+dti_fibers+' > '+vtk_fibers)
+  make_fibs(result.tensors, result.mask, result.anis, result.curve, result.fibers, result.vtk)
+
+
+if __name__ == "__main__":
+  main()
