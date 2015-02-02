97ea07e12c1cc170ffb7ae80745ede76fc953712
gkiar <gkiar07@gmail.com>
2015-02-02 16:05:19 -0500

    updating formatting + licensing
    Former-commit-id: 96f270f160f08f398a6180b9280d37427789c172 [formerly edcc9b9602427382e6722e759a98297f20881d54] [formerly 669ab7fa6792486c99f7d88faa21087d6a4e5947 [formerly 0b1633e8608baab196e37fdc49e35697dc521c36]]
    Former-commit-id: b255924add68f78b43748501c3569375d7f42ae3
    Former-commit-id: fb561d62044ddd55011934867e64a3805478b3c4

--- M2G/src/skullextract.py
+++ M2G/src/skullextract.py
@@ -1,24 +1,50 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+# Copyright 2015 Open Connectome Project (http://openconnecto.me)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# skullextract.py
+# Created by Greg Kiar on 2015-01-09.
+# Email: gkiar@jhu.edu
+# Copyright (c) 2015. All rights reserved.
 
 
-from sys import argv
+import argparse
 import os
+from os.path import basename
 
-params = list(argv)
-inimg = params[1]
-f = params[2]
-g = params[3]
-outf = params[4]
-mask = params[5]
+def remove_skull(image, f, g, brain, mask):
+  [root, ext1] = os.path.splitext(basename(outf))
+  [root, ext2] = os.path.splitext(root)
+  
+  os.system('bet '+image+' '+brain+' -f '+f+' -g '+g+' -m')
 
-#print 'input image: ' + inimg
-#print 'output brain image: ' + outf
-#print 'output brain mask image: ' + mask
+  os.system('mv '+root+'_mask'+ext2+ext1+' '+mask)
 
-[root, ext1] = os.path.splitext(outf)
-print ext1
-[root, ext2] = os.path.splitext(root)
-print ext2
-os.system('bet '+inimg+' '+outf+' -f '+f+' -g '+g+' -m')
-print 'mv '+root+'_mask'+ext2+ext1+' '+mask
-os.system('mv '+root+'_mask'+ext2+ext1+' '+mask)
+def main():
+  parser = argparse.ArgumentParser(description="")
+  parser.add_argument("image", action="store", help="The tensors produced from the DTI image (.Bdouble)")
+  parser.add_argument("f", action="store", help="The brain mask of DTI image (.nii, .nii.gz)")
+  parser.add_argument("g", action="store", help="The anisotropic threshold value (default =0.2)")
+  parser.add_argument("brain", action="store", help="The curvature threshold value (default =60)") 
+  parser.add_argument("mask", action="store", help="The produced fiber tracts (.Bfloat)")
+  
+  result = parser.parse_args()
+
+  remove_skull(result.image, result.f, result.g, result.brain, result.mask)
+
+
+if __name__ == "__main__":
+    main()
