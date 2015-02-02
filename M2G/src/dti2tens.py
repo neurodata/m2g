22710f01d717ad95d5f87591a4a8da1820fcc6ec
gkiar <gkiar07@gmail.com>
2015-02-02 13:53:30 -0500

    add license, modularize
    Former-commit-id: e2137673a9d7cb5558f1a9bb3707ea4b933d7460 [formerly be45bd56a391c49adb470b41aad127655b4e2558] [formerly f3c370b2dda8982d287b2919f0ba6c7759dd3a68 [formerly 7ce31dbe31b17442b99b9bd869c17102500dcdf6]]
    Former-commit-id: dff2cf2ff089d11bd811109ec8d34ad9790d0e5b
    Former-commit-id: 5a92bae768b5cc28c9e6543bc02efee1231d481f

--- M2G/src/dti2tens.py
+++ M2G/src/dti2tens.py
@@ -1,22 +1,39 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+# Copyright 2015 Open Connectome Project (http://openconnecto.me)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 # dti2tens.py
-# DTI images are converted to tensor images
-# Version 0.1, G. Kiar, 01/09/2015
+# Created by Greg Kiar on 2015-01-09.
+# Email: gkiar@jhu.edu
+# Copyright (c) 2015. All rights reserved.
 
 # Load necessary packages
-from sys import argv
+import argparse
 import string
 import sys
 import re
 import os
+from os.path import basename
 
 # Pull necessary parameters
 params = list(argv)
-dti_image = params[1] # The DTI image (skull stripped or not? idc) (.nii, .nii.gz)
+dti_image = params[1] # The DTI image, not skull stripped (.nii)
 dti_grad = params[2] # The gradient directions corresponding to the DTI image (.grad)
 bval = params[3] # The bvalue corresponding to the DTI image  (default 700) (val)
-dti_mask = params[4] # The brain mask of the DTI image (which is why I don't care if skull stripped original) (.nii, .nii.gz)
+dti_mask = params[4] # The brain mask of the DTI image (.nii, .nii.gz)
 
 dti_scheme = params[5] # The scheme file (.scheme)(output)
 dti_bfloat = params[6] # The Bfloat format equivalent of the DTI image (.Bfloat)(output)
@@ -25,37 +42,45 @@ fa = params[8] # The fractional anisotropy statistic (.nii)(output)
 md = params[9] # The mean diffusivity statistic (.nii)(output)
 dti_eigs = params[10] # The eigen values of the system (.Bdouble)(output)
 
-print dti_image
-print dti_grad
-print bval
-print dti_mask
-print dti_scheme
-print dti_bfloat
-print dti_tensors
-print fa
-print md
-print dti_eigs
+def make_tens(dti, grad, bval, mask, scheme, dti_bfloat, tensors, fa, md, eigs):
+  # Create scheme file
+  os.system('pointset2scheme -inputfile '+dti_grad+' -bvalue '+bval+' -outputfile '+dti_scheme)
+  
+  # Maps the DTI image to a Camino compatible Bfloat format
+  os.system('image2voxel -4dimage '+dti_image+' -outputfile '+dti_bfloat)
+  
+  # Produce tensors from image
+  os.system('dtfit '+dti_bfloat+' '+dti_scheme+' -bgmask '+dti_mask+' -outputfile '+dti_tensors)
+  
+  # In order to visualize, and just 'cause it's fun anyways, we get some stats
+  a = basename(fa)
+  b = basename(md)
+  [fa_base, ext] = os.path.splitext(a)
+  [md_base, ext] = os.path.splitext(b)
+  os.system('for PROG in '+fa_base+' '+md_base+'; do cat '+dti_tensors+' | ${PROG} | voxel2image -outputroot ${PROG} -header '+dti_image+'; done')
+  os.system('mv '+a+' '+fa)
+  os.system('mv '+b+' '+md)
+
+  # We also need the eigen system to visualize
+  os.system('cat '+dti_tensors+' | dteig > '+dti_eigs)
+
+def main():
+  parser = argparse.ArgumentParser(description="")
+  parser.add_argument("dti", action="store", help="The DTI image, not skull stripped (.nii)")
+  parser.add_argument("grad", action="store", help="The gradient directions corresponding to the DTI image (.grad)")
+  parser.add_argument("bval", action="store", help="The bvalue corresponding to the DTI image  (default 700)")
+  parser.add_argument("mask", action="store", help="The brain mask of the DTI image (.nii, .nii.gz)") 
+  parser.add_argument("scheme", action="store", help="The scheme file (.scheme)")
+  parser.add_argument("dti_bfloat", action="store", help="The Bfloat format equivalent of the DTI image (.Bfloat)")
+  parser.add_argument("tensors", action="store", help="The produced tensors in Bdouble format (.Bdouble)")
+  parser.add_argument("fa", action="store", help="The fractional anisotropy statistic (.nii)")
+  parser.add_argument("md", action="store", help="The mean diffusivity statistic (.nii)")
+  parser.add_argument("eigs", action="store", help="The eigen values of the system (.Bdouble)")
+
+  result = parser.parse_args()
+
+  make_tens(result.dti, result.grad, result.bval, result.mask, result.scheme, result.dti_bfloat, result.tensors, result.fa, result.md, result.eigs)
 
 
-# Create scheme file
-print 'pointset2scheme -inputfile '+dti_grad+' -bvalue '+bval+' -outputfile '+dti_scheme
-os.system('pointset2scheme -inputfile '+dti_grad+' -bvalue '+bval+' -outputfile '+dti_scheme)
-
-
-# Maps the DTI image to a Camino compatible Bfloat format
-print 'image2voxel -4dimage '+dti_image+' -outputfile '+dti_bfloat
-os.system('image2voxel -4dimage '+dti_image+' -outputfile '+dti_bfloat)
-
-# Produce tensors from image
-print 'dtfit '+dti_bfloat+' '+dti_scheme+' -bgmask '+dti_mask+' -outputfile '+dti_tensors
-os.system('dtfit '+dti_bfloat+' '+dti_scheme+' -bgmask '+dti_mask+' -outputfile '+dti_tensors)
-
-# In order to visualize, and just 'cause it's fun anyways, we get some stats
-print 'for PROG in fa md; do cat '+dti_tensors+' | ${PROG} | voxel2image -outputroot ${PROG} -header '+dti_image+'; done'
-os.system('for PROG in '+fa+' '+md+'; do cat '+dti_tensors+' | ${PROG} | voxel2image -outputroot ${PROG} -header '+dti_image+'; done')
-os.system('mv fa.nii '+fa)
-os.system('mv md.nii '+md)
-
-# We also need the eigen system to visualize
-print 'cat '+dti_tensors+' | dteig > '+dti_eigs
-os.system('cat '+dti_tensors+' | dteig > '+dti_eigs)
+if __name__ == '__main__':
+  main()
