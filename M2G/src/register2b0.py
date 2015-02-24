549156bd39bfc6417d5f21396b9c82ef6e3b06ad
gkiar <gkiar07@gmail.com>
2015-02-23 21:16:00 -0500

    initial commit of b0 registration
    Former-commit-id: 9649ecd0fda832fd02bc226f87f68fa2a71c1532 [formerly 280934a6387298c1d68769f1fbd70997c2e8b3c4] [formerly cd715fd3e16b6add34f368857548dce3d6461aa2 [formerly 267d759cfa53740d107157dd71ea02b354722b40]]
    Former-commit-id: 21feab6fdf0b7dbc841cbe5de31de49c825ad1df
    Former-commit-id: ac3d867f8855c2f8661caed1d70049b1cd8e6188

---
+++ M2G/src/register2b0.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+# Copyright 2015 Open Connectome Project (http://openconnecto.me)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# extract_b0.py
+# Created by Greg Kiar on 2015-02-23.
+# Email: gkiar@jhu.edu
+# Copyright (c) 2015. All rights reserved.
+
+from argparse import ArgumentParser
+from nibabel import load, save, Nifti1Image
+from numpy import where, loadtxt
+from os import system
+
+def register_vol(dti_img, bvals, aligned_img, b0_img):
+  print "Loading dti data..."
+  dti_vol = load(dti_img)
+  dti_data = dti_vol.get_data()
+  
+  print "Loading bvals file..."
+  b = loadtxt(bvals)
+  
+  print "Extracting B0 volume..."
+  b0_vol=dti_data[:,:,:,int(where(b==0)[0])]
+  b0_head = dti_vol.get_header()
+  b0_head.set_data_shape(b0_head.get_data_shape()[0:3])
+  b0_out = Nifti1Image( data=b0_vol, affine=dti_vol.get_affine(), header=b0_head )
+  save(b0_out, b0_img)
+  
+  directions = dti_vol.get_shape()[3]
+  print "Number of 3D volumes:", directions
+  
+  aligned_vol = dti_vol
+  aligned_data = aligned_vol.get_data()
+  print "Beginning registration..."
+  temp_in_img = 'temp_in.nii'
+  temp_out_img = 'temp_out.nii'
+  temp_head = dti_vol.get_header()
+  temp_head.set_data_shape(temp_head.get_data_shape()[0:3])
+  
+  for ii in range(directions):
+    print "Volume ", ii, " of ", directions
+    currentvol = aligned_data[:,:,:,ii]
+    #print "Writing current volume to disk..."
+    out = Nifti1Image( data=currentvol, affine=dti_vol.get_affine(), header=temp_head )
+    save(out, temp_in_img)
+    system('antsRegistration -d 3 -o [affine.mat,'+temp_out_img+'] -r ['+b0_img+', '+temp_in_img+',1] -m Mattes[ '+b0_img+', '+temp_in_img+',1,12] -t Affine[0.75] -c [25, 1e-4, 5] --smoothing-sigmas 1 -f 1')
+    temp_vol = load(temp_out_img)
+    currentvol = temp_vol.get_data()
+    aligned_data[:,:,:,ii] = currentvol
+    system('rm -f '+temp_out_img+';rm -f'+temp_in_img)
+    
+  system('rm -f affine0GenerticAffine.mat')
+  out = Nifti1Image( data=aligned_vol, affine=dti_vol.get_affine(), header=dt_vol.get_header() )
+  save(out, aligned_img)
+
+
+def main():
+  parser = ArgumentParser(description="")
+  parser.add_argument("DTI", action="store", help="The DTI image we want to extract B0 from (.nii, .nii.gz)")
+  parser.add_argument("bvals", action="store", help="The b-value file corresponding to the DTI image (.b)")
+  parser.add_argument("alignedDTI", action="store", help="The output file location of the B0 scan (.nii, .nii.gz)")
+
+  result = parser.parse_args()
+
+  register_vol(result.DTI, result.bvals, result.alignedDTI)
+  
+if __name__ == "__main__":
+    main()
+    
