d5f0ed26107af9d900deffb935f17dce79cc9a94
gkiar <gkiar07@gmail.com>
2015-09-08 10:39:39 -0400

    adjusted paths

--- analysis/compute_trt.R
+++ analysis/compute_trt.R
@@ -21,7 +21,7 @@
 source('/Users/gkiar/code/ocp/FlashR/stats/reliability.R') #need to fix hard paths
 source('/Users/gkiar/code/ocp/m2g/analysis/load_graphs.R')
 
-dir <- '/Users/gkiar/code/scratch/smg'
+dir <- '/Users/gkiar/code/ocp/scratch/m2g/smg'
 format <- 'graphml' #graph format
 rois <- 70 #number of regions in graphs
 scans <- 2 #scans per subject
@@ -65,10 +65,11 @@ names(reliability) <-c( 'raw', 'log_raw', stack[[5]] )
 hist <- rbind(rdf, logrdf, stack[[3]])
 
 #plot + compare the results
+library(ggplot2)
 qplot(seq_along(reliability), reliability, ylim=range(c(0.9, 1))) + scale_x_discrete(labels = names(reliability)) +
   ggtitle("MNR of SWU_4 DTI dataset through m2g") + xlab('number of bins')
-
-plot(stats::density(logrdf[which(!is.nan(logrdf))])) +  title('RDF')
-lines(stats::density(stack[[3]][which(!is.nan(stack[[3]]))]), col=2)
-lines(stats::density(logrdf[which(!is.nan(logrdf))]), col=3)
-legend('topleft', c('raw', 'log_raw', 'nbin*'))
+#
+#plot(stats::density(logrdf[which(!is.nan(logrdf))])) +  title('RDF')
+#lines(stats::density(stack[[3]][which(!is.nan(stack[[3]]))]), col=2)
+#lines(stats::density(logrdf[which(!is.nan(logrdf))]), col=3)
+#legend('topleft', c('raw', 'log_raw', 'nbin*'))
