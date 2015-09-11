5471f5bf6a26fff5f7388f8c86bdf32eae4436c7
Disa Mhembere <dmhembe1@jhu.edu>
2015-09-11 17:59:10 -0400

    [admin]: removed unnecessary media path

--- MR-OCP/MROCPdjango/ocpipeline/settings.py
+++ MR-OCP/MROCPdjango/ocpipeline/settings.py
@@ -61,11 +61,6 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-#MEDIA_ROOT = '/data/ocp'
-MEDIA_ROOT = '/mnt/nfs/indata/'
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
