0eeb0d1b180443632dad06d678f463875efd7d4b
Disa Mhembere <dmhembe1@jhu.edu>
2015-09-13 14:39:08 -0400

    [celery]: Added hooks for build graph

--- MR-OCP/MROCPdjango/pipeline/procs/process_ip_data.py
+++ MR-OCP/MROCPdjango/pipeline/procs/process_ip_data.py
@@ -33,61 +33,54 @@ from pipeline.utils.util import getFiberID, get_download_path
 from mrcap.gengraph import genGraph
 
 # From the object
-# 
-def processInputData(session):
+def process_input_data(derivatives, graphs, graphsize, invariants, 
+                        proj_dir, invariant_loc, to_email):
   '''
   Extract File name & determine what file corresponds to what for gengraph
   @param session: the session dictionary object
   '''
+  """
   if isinstance(session, str) or isinstance(session, unicode):
     f = open(session, "rb")
     session = pickle.load(f)
     f.close()
+  """
 
-  filesInUploadDir = os.listdir(session['derivatives'])
+  filesInUploadDir = os.listdir(derivatives)
 
   fiber_fn, data_atlas_fn = checkFileExtGengraph(filesInUploadDir) # Check & sort files
 
   ''' Fully qualify file names '''
-  fiber_fn = os.path.join(session['derivatives'], fiber_fn)
+  fiber_fn = os.path.join(derivatives, fiber_fn)
   
   if not data_atlas_fn:
     data_atlas_fn = settings.ATLASES.keys()[0]
   else:
-    data_atlas_fn = os.path.join(session['derivatives'], data_atlas_fn)
+    data_atlas_fn = os.path.join(derivatives, data_atlas_fn)
 
   print "data_atlas_fn %s ..." % data_atlas_fn
 
-  Gfn = os.path.join(session['graphs'], getFiberID(fiber_fn)) # partial name
-  if (session['graphsize']).lower().startswith("s"):
+  Gfn = os.path.join(graphs, getFiberID(fiber_fn)) # partial name
+  if (graphsize).lower().startswith("s"):
     Gfn += "smgr.graphml"
-  elif (session['graphsize']).lower().startswith("b"):
+    graphsize = False # False is small graph
+  elif graphsize.lower().startswith("b"):
     Gfn+="bggr.graphml"
+    graphsize = True # True is big graph
   else: print '[ERROR]: Graphsize Unkwown' # should never happen
    
   try:
-    session['Gfn']= genGraph(fiber_fn, data_atlas_fn, Gfn, session['graphsize'],\
-                              numfibers=20000, **settings.ATLASES)
+    Gfn= genGraph(fiber_fn, data_atlas_fn, Gfn, graphsize, **settings.ATLASES) # numfibers = 20000 for tests
   except:
     msg = "Hello,\n\nYour most recent job failed either because your fiber streamline file or ROI mask was incorrectly formatted."
-    msg += " Please check both and try again.%s\n\n" % (" "*randint(0,10))
-    sendJobFailureEmail(session['email'], msg)
+    msg += " Please check both and try again.\n\n"
+    sendJobFailureEmail(to_email, msg)
 
   # Run ivariants here
-  if len(session['invariants']) > 0:
+  if len(invariants) > 0:
     print "Computing invariants ..."
 
-    session['invariant_fns'] = run_invariants(session['invariants'],\
-                                session['Gfn'], session['graphInvariants'])
+    invariant_fns = run_invariants(invariants, Gfn, invariant_loc)
 
-  dwnldLoc = get_download_path(session['usrDefProjDir'])
-  sendJobCompleteEmail(session['email'], dwnldLoc)
-
-def main():
-  parser = argparse.ArgumentParser(description="")
-  parser.add_argument("ARG", action="", help="")
-  parser.add_argument("-O", "--OPT", action="", help="")
-  result = parser.parse_args()
-
-if __name__ == "__main__":
-  main()
+  dwnld_loc = get_download_path(proj_dir)
+  sendJobCompleteEmail(to_email, dwnld_loc)
