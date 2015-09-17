a0d72392bd7743ebc6b4cdaf238853767ce7d236
Disa Mhembere <dmhembe1@jhu.edu>
2015-09-16 23:31:39 -0400

    [examples]: efactored prog build

--- MR-OCP/MROCPdjango/examples/buildsubj.py
+++ MR-OCP/MROCPdjango/examples/buildsubj.py
@@ -22,31 +22,23 @@ import sys
 import zipfile
 import tempfile
 
-import webbrowser
-
-
-''' RESTful API '''
-
 def main():
+  parser = argparse.ArgumentParser(description="Build a subjects graph given data. \
+      Base url -> http://openconnecto.me/graph-services/buildgraph")
+  parser.add_argument("url", action="store", help="url must have NO SPACES & \
+      must be in the form  http://openconnecto.me/graph-services/buildgraph/\
+      {projectName}/{site}/{subject}/{session}/{scanID}/{s|b}, where s= smallgraph OR b = biggraph")
+  parser.add_argument("fiberfile", action="store", help="MRI studio fiber file")
+  parser.add_argument("email", action="store", help="your email address")
+  parser.add_argument("-i", "--invariants", action="store", nargs="*", \
+      help="OPTIONAL: a list of invariants types. E.g cc tri deg mad for \
+      clustering coefficient, triangle count, degree & maximum average degree")
 
-  parser = argparse.ArgumentParser(description='Upload a subject to MROCP. \
-      Base url -> http://openconnecto.me/graph-services/upload')
-  parser.add_argument('url', action="store", help='url must have NO SPACES & \
-      must be in the form  http://mrbrain.cs.jhu.edu/graph-services/upload/\
-      {projectName}/{site}/{subject}/{session}/{scanID}/{s|b}, where s= smallgraph OR b = biggraph')
-  parser.add_argument('-i', '--invariants', action="store", \
-      help='OPTIONAL: comma separated list of invariant types. E.g cc,tri,deg,mad for \
-      clustering coefficient, triangle count, degree & maximum average degree')
-
-  parser.add_argument('fiberfile', action="store", help="MRI studio fiber file")
-  parser.add_argument('-a','--atlas', action="store", help="NIFTI format atlas")
-
-  parser.add_argument('-u', '--auto', action="store_true", 
-      help="Use this flag if you want a browser session to open up with the result automatically")
+  parser.add_argument("-a","--atlas", action="store", help="NIFTI format atlas")
 
   result = parser.parse_args()
 
-  result.url = result.url if result.url.endswith('/') else result.url + '/' #
+  result.url = result.url if result.url.endswith("/") else result.url + "/" #
 
   try:
     # Create a temporary file to store zip contents in memory
@@ -63,12 +55,12 @@ def main():
     tmpfile.seek(0)
 
 #  Testing only: check the contents of a zip file.
-#
-#    rzfile = zipfile.ZipFile(tmpfile.name, "r", allowZip64=True)
-#    ret = rzfile.printdir()
-#    ret = rzfile.testzip()
-#    ret = rzfile.namelist()
-#    import pdb; pdb.set_trace()
+    """
+    rzfile = zipfile.ZipFile(tmpfile.name, "r", allowZip64=True)
+    ret = rzfile.printdir()
+    ret = rzfile.testzip()
+    ret = rzfile.namelist()
+    """
 
   except:
     print "Check the filenames: fiber:", result.fiberfile
@@ -78,22 +70,17 @@ def main():
     sys.exit(0)
 
   try:
-    call_url = result.url if result.invariants is None else result.url + result.invariants
+    call_url = result.url + result.email + "/" + "/".join(result.invariants)
     print "Calling url: %s" % call_url
-    req = urllib2.Request ( call_url , tmpfile.read() ) # Important to concatenate these in this order
+    req = urllib2.Request ( call_url ,tmpfile.read() ) # Important to concatenate these in this order
     response = urllib2.urlopen(req)
 
   except urllib2.URLError, e:
     print "Failed URL", result.url
-    print "Error %s" % (e.read())
+    print "Error:", e
     sys.exit(0)
 
-  the_page = response.read()
-  print '\n====> ' + the_page
-
-  if result.auto:
-    ''' Optional: Open up a tab/window in your browser to view results'''
-    webbrowser.open(the_page.split(' ')[5])
+  print "\n====> " + response.read()
 
 if __name__ == "__main__":
   main()
