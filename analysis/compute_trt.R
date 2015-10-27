cc8d3d7c68f086ef07c17929f8020ee8924420cd
gkiar <gkiar07@gmail.com>
2015-10-26 20:05:54 -0400

    progress towards commandline execution of trt computation

--- analysis/compute_trt.R
+++ analysis/compute_trt.R
@@ -18,14 +18,28 @@
 # Email: gkiar@jhu.edu
 # Copyright (c) 2015. All rights reserved.
 
-source('/Users/gkiar/code/ocp/FlashR/stats/reliability.R') #need to fix hard paths
-source('/Users/gkiar/code/ocp/m2g/analysis/load_graphs.R')
+args <- commandArgs(TRUE)
+l <- length(args)
+if (l < 2) {
+	stop(c('Please provide data and script paths in order to run, as follows:\n',
+				' Rscript compute_trt.R m2g_path graph_dir [extension rois scans]'))
+}
+#sets location of FlashR and m2g analysis code
+m2g <- paste(args[1], '/analysis/load_graphs.R', sep="")
+flashr <- paste(args[1],'/analysis/reliability.R', sep="")
 
-dir <- '/Users/gkiar/code/ocp/scratch/m2g/smg'
+print(m2g)
+print(flashr)
+
+source(m2g) #need to fix hard paths
+source(flashr)
+l <- length(args)
+
+dir <- args[2]
 format <- 'graphml' #graph format
 rois <- 70 #number of regions in graphs
 scans <- 2 #scans per subject
-rule <- list('_', 2, 3) #subject id rule in the form of: delimiter, start, end
+rule <- list('_', 1, 2) #subject id rule in the form of: delimiter, start, end
 
 if (format != 'graphml') {
   stop('Currently support only exists for graphml format. Sorry') 
@@ -46,28 +60,28 @@ ids <- clean_ids(pair[[2]], rule)
 #compute RDF and MNR for raw graphs
 dist <- compute_distance(graphs)
 rdf <- compute_rdf(dist, ids, scans)
-mnr <- compute_mnr(rdf)
+mnr <- compute_mnr(rdf, output=TRUE)
 
 #compute RDF and MNR for log graphs
-log_graphs <- log(graphs+array(rep(1, length(graphs)), dim(graphs)), base=10)
-logdist <- compute_distance(log_graphs)
-logrdf <- compute_rdf(logdist, ids, scans)
-logmnr <- compute_mnr(logrdf)
+#log_graphs <- log(graphs+array(rep(1, length(graphs)), dim(graphs)), base=10)
+#logdist <- compute_distance(log_graphs)
+#logrdf <- compute_rdf(logdist, ids, scans)
+#logmnr <- compute_mnr(logrdf, output=TRUE)
 
 #find nbin* through means of optimizing MNR
-rank_graphs <- rank_matrices(graphs, normalize=TRUE)
-stack <- compute_nbinstar(rank_graphs, ids, scans=2, N=16, spacing="linear", lim=4900)
+#rank_graphs <- rank_matrices(graphs, normalize=TRUE)
+#stack <- compute_nbinstar(rank_graphs, ids, scans=2, N=16, spacing="linear", lim=4900)
 
 #compile results
-reliability <- c( mnr, logmnr, stack[[4]] )
-names(reliability) <-c( 'raw', 'log_raw', stack[[5]] )
+#reliability <- c( mnr, logmnr)#, stack[[4]] )
+#names(reliability) <-c( 'raw', 'log_raw')#, stack[[5]] )
 
-hist <- rbind(rdf, logrdf, stack[[3]])
+#hist <- rbind(rdf, logrdf)#, stack[[3]])
 
 #plot + compare the results
-library(ggplot2)
-qplot(seq_along(reliability), reliability, ylim=range(c(0.9, 1))) + scale_x_discrete(labels = names(reliability)) +
-  ggtitle("MNR of SWU_4 DTI dataset through m2g") + xlab('number of bins')
+#library(ggplot2)
+#qplot(seq_along(reliability), reliability, ylim=range(c(0.9, 1))) + scale_x_discrete(labels = names(reliability)) +
+#  ggtitle("MNR of SWU_4 DTI dataset through m2g") + xlab('number of bins')
 #
 #plot(stats::density(logrdf[which(!is.nan(logrdf))])) +  title('RDF')
 #lines(stats::density(stack[[3]][which(!is.nan(stack[[3]]))]), col=2)
