e37b0c2319a16d45903b305b234dbb1c1c90e446
gkiar <gkiar07@gmail.com>
2016-01-13 12:47:32 -0500

    pep8ed setup, added tensor outputs

--- packages/utils/m2g_setup.py
+++ packages/utils/m2g_setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-# Copyright 2015 Open Connectome Project (http://openconnecto.me)
+# Copyright 2014 Open Connectome Project (http://openconnecto.me)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,139 +15,158 @@
 # limitations under the License.
 #
 
-# m2g_setup.py
-# Created by Will Gray Roncal on 2015-10-24.
-# Email: wgr@jhu.edu
-# Copyright (c) 2015. All rights reserved.
+# m2g_startup.py
+# Created by Greg Kiar on 2016-01-12.
+# Email: gkiar@jhu.edu
+# Copyright (c) 2016. All rights reserved
 
-import os, sys
+from argparse import ArgumentParser
+import os
+import sys
 import glob
 
-# parse input files
-inDir = sys.argv[1]
-outputBaseDir = sys.argv[2]
 
-# input list files
-dtiListFile = sys.argv[3]
-bvalListFile = sys.argv[4]
-bvecListFile = sys.argv[5]
-mprageListFile = sys.argv[6]
+def setup(inDir, outputBaseDir, dtiListFile, bvalListFile, bvecListFile,
+          mprageListFile, smgListFile, rdtiListFile, rmriListFile, bgListFile,
+          tensorListFile, fiberListFile, atlasIn):
 
-# output list files
-#Registred DTI data, registered mprage data, sg, bg, fibers
+    # Create output directories
+    outDirs = ['tensors', 'fibers', 'bg', 'graphs', 'reg_mprage', 'reg_dti']
 
-smgListFile = sys.argv[7]# os.path.join(outputBaseDir,'sgListFile.list') #
-rdtiListFile = sys.argv[8]#os.path.join(outputBaseDir,'rdtiListFile.list')#sys.argv[9]
-rmriListFile = sys.argv[9]#os.path.join(outputBaseDir,'rmriListFile.list')#sys.argv[10]
-bgListFile = sys.argv[10]#os.path.join(outputBaseDir,'bgListFile.list')#sys.argv[11]
-fiberListFile = sys.argv[11]#os.path.join(outputBaseDir,'fiberListFile.list')#sys.argv[12]
+    if not os.path.exists(outputBaseDir):
+        os.makedirs(outputBaseDir)
 
-# smg files
-atlasIn = sys.argv[12:] #accomodates all atlases
+    for f in outDirs:
+        d = outputBaseDir + '/' + f
+        if not os.path.exists(d):
+            os.makedirs(d)
 
-print atlasIn
+    atlasList = atlasIn
 
-#make output directory and subdirectories
-outDirs = ['fibers', 'sg', 'bg', 'reg']
+    # Create lists of files
+    dti_types = ('*DTI.nii', '*DTI.nii.gz')
+    dtiFiles = [y for x in os.walk(inDir) for z in dti_types
+                for y in glob.glob(os.path.join(x[0], z))]
 
-if not os.path.exists(outputBaseDir):
-    os.makedirs(outputBaseDir)
-    
-for f in outDirs:
-    d = outputBaseDir + '/' + f
-    if not os.path.exists(d):
-        os.makedirs(d)
+    bval_types = ('*.b', '*.bval')
+    bvalFiles = [y for x in os.walk(inDir) for z in bval_types
+                 for y in glob.glob(os.path.join(x[0], z))]
 
-# Read atlas file    
-#with open(atlasIn) as f:
-#    atlasList = f.read().splitlines()
-atlasList = atlasIn #TODO
-    
-#save list of paths as csvs
+    bvec_types = ('*.bvec', '*.grad')
+    bvecFiles = [y for x in os.walk(inDir) for z in bvec_types
+                 for y in glob.glob(os.path.join(x[0], z))]
 
-dtiFiles = [y for x in os.walk(inDir) for y in glob.glob(os.path.join(x[0], '*DTI.nii'))]
-bvalFiles = [y for x in os.walk(inDir) for y in glob.glob(os.path.join(x[0], '*.b'))]
-bvecFiles = [y for x in os.walk(inDir) for y in glob.glob(os.path.join(x[0], '*.grad'))]
-mprageFiles = [y for x in os.walk(inDir) for y in glob.glob(os.path.join(x[0], '*MPRAGE.nii'))]
+    mprage_types = ('*MPRAGE.nii', '*MPRAGE.nii.gz')
+    mprageFiles = [y for x in os.walk(inDir) for z in mprage_types
+                   for y in glob.glob(os.path.join(x[0], z))]
 
-# DTI in
-with open(dtiListFile,'wb') as thefile:
-    for item in dtiFiles:
-        thefile.write("%s\n" % item)
+    # Writes lists to disk
+    with open(dtiListFile, 'wb') as thefile:
+        for item in dtiFiles:
+            thefile.write("%s\n" % item)
+    with open(bvalListFile, 'wb') as thefile:
+        for item in bvalFiles:
+            thefile.write("%s\n" % item)
+    with open(bvecListFile, 'wb') as thefile:
+        for item in bvecFiles:
+            thefile.write("%s\n" % item)
+    with open(mprageListFile, 'wb') as thefile:
+        for item in mprageFiles:
+            thefile.write("%s\n" % item)
 
-# bval in                                
-with open(bvalListFile,'wb') as thefile:
-    for item in bvalFiles:
-        thefile.write("%s\n" % item)
+    # Create graph list
+    atlas = list()
+    for a in atlasList:
+        [xx, f] = os.path.split(a)
+        [aa, xx] = os.path.splitext(os.path.splitext(f)[0])
+        d = outputBaseDir + '/graphs/' + aa
+        if not os.path.exists(d):
+            os.makedirs(d)
+        atlas.append(aa)
 
-# bvec in
-with open(bvecListFile,'wb') as thefile:
-    for item in bvecFiles:
-        thefile.write("%s\n" % item)
+    sub = list()
+    for s in dtiFiles:
+        [xx, f] = os.path.split(s)
+        [ss, xx] = os.path.splitext(os.path.splitext(f)[0])
+        sub.append(ss.replace('DTI', ''))
 
-# mprage in
-with open(mprageListFile,'wb') as thefile:
-    for item in mprageFiles:
-        thefile.write("%s\n" % item)
+    for f in outDirs:
+        d = outputBaseDir + '/' + f
+        if not os.path.exists(d):
+            os.makedirs(d)
 
-#construct small graph filenames - done differently because of combinatorics
+    # Output smg names
+    smg = list()
+    for s in sub:
+        for a in atlas:
+            smg.append(os.path.join(outputBaseDir + '/graphs/' +
+                       a + '/' + s + a + '.graphml'))
 
-atlas = list()
-for a in atlasList:
-    [xx, f] = os.path.split(a)
-    [aa, xx]  = os.path.splitext(f)
-    atlas.append(aa)
-    
-sub = list()
-for s in dtiFiles:
-    [xx, f] = os.path.split(s)
-    [ss, xx]  = os.path.splitext(f)
-    sub.append(ss.replace('DTI',''))
+    with open(smgListFile, 'wb') as thefile:
+        for item in smg:
+            thefile.write("%s\n" % item)
 
-# output smg names
+    # Create other derivative output lists
+    rdtiName = list()
+    rmriName = list()
+    bgName = list()
+    tensorName = list()
+    fiberName = list()
 
-smg = list()
-for s in sub:
-    for a in atlas:
-       smg.append(os.path.join(outputBaseDir + '/sg/' + s + a + '_sg.graphml'))
+    for s in sub:
+        rdtiName.append(os.path.join(outputBaseDir, 'reg_dti',
+                                     s + 'DTI_reg.nii.gz'))
+        rmriName.append(os.path.join(outputBaseDir, 'reg_mprage',
+                                     s + 'MPRAGE_reg.nii.gz'))
+        bgName.append(os.path.join(outputBaseDir, 'bg',
+                                   s + 'bg.graphml'))
+        tensorName.append(os.path.join(outputBaseDir, 'tensors',
+                                       s + 'tensors.Bdouble'))
+        fiberName.append(os.path.join(outputBaseDir, 'fibers',
+                                      s + 'fibers.dat'))
+
+    # Save names to disk
+    with open(rdtiListFile, 'wb') as thefile:
+        for item in rdtiName:
+            thefile.write("%s\n" % item)
+    with open(rmriListFile, 'wb') as thefile:
+        for item in rmriName:
+            thefile.write("%s\n" % item)
+    with open(bgListFile, 'wb') as thefile:
+        for item in bgName:
+            thefile.write("%s\n" % item)
+    with open(tensorListFile, 'wb') as thefile:
+        for item in tensorName:
+            thefile.write("%s\n" % item)
+    with open(fiberListFile, 'wb') as thefile:
+        for item in fiberName:
+            thefile.write("%s\n" % item)
 
 
-print sub
-print atlas    
-print smg
+def main():
+    parser = ArgumentParser(description="")
+    parser.add_argument("inDir", action="store",
+                        help="Input directory for raw data")
+    parser.add_argument("outputBaseDir", action="store",
+                        help="Output base directory for derivatives")
+    parser.add_argument("dtiListFile", action="store", help="")
+    parser.add_argument("bvalListFile", action="store", help="")
+    parser.add_argument("bvecListFile", action="store", help="")
+    parser.add_argument("mprageListFile", action="store", help="")
+    parser.add_argument("smgListFile", action="store", help="")
+    parser.add_argument("rdtiListFile", action="store", help="")
+    parser.add_argument("rmriListFile", action="store", help="")
+    parser.add_argument("bgListFile", action="store", help="")
+    parser.add_argument("fiberListFile", action="store", help="")
+    parser.add_argument("tensorListFile", action="store", help="")
+    parser.add_argument("atlasIn", action="store", nargs="+", help="")
+    result = parser.parse_args()
 
-with open(smgListFile,'wb') as thefile:
-    for item in smg:
-        thefile.write("%s\n" % item)
-        
-# sub is the base subject directory 
-rdtiName = list()
-rmriName = list()
-bgName = list()
-fiberName = list()
-       
-for s in sub:
-    rdtiName.append(os.path.join(outputBaseDir, 'reg', s + 'dti_reg.nii'))
-    rmriName.append(os.path.join(outputBaseDir, 'reg', s + 'mprage_reg.nii'))
-    bgName.append(os.path.join(outputBaseDir, 'bg', s + 'bg.graphml'))
-    fiberName.append(os.path.join(outputBaseDir, 'fibers', s + 'fiber.dat'))
+    setup(result.inDir, result.outputBaseDir, result.dtiListFile,
+          result.bvalListFile, result.bvecListFile, result.mprageListFile,
+          result.smgListFile, result.rdtiListFile, result.rmriListFile,
+          result.bgListFile, result.tensorListFile, result.fiberListFile,
+          result.atlasIn)
 
-# registered dti names
-with open(rdtiListFile,'wb') as thefile:
-    for item in rdtiName:
-        thefile.write("%s\n" % item)
-
-# registered mprage names
-with open(rmriListFile,'wb') as thefile:
-    for item in rmriName:
-        thefile.write("%s\n" % item)
-
-# bg names
-with open(bgListFile,'wb') as thefile:
-    for item in bgName:
-        thefile.write("%s\n" % item)
-
-# fiber names
-with open(fiberListFile,'wb') as thefile:
-    for item in fiberName:
-        thefile.write("%s\n" % item)
+if __name__ == "__main__":
+    main()
