adcf27e54ab7f86abcaa56e1bcc9a6e647aab94a
gkiar <gkiar07@gmail.com>
2016-01-27 15:10:31 -0500

    created stub for dipy_m2g

---
+++ packages/dipy/run_m2g.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# Copyright 2014 Open Connectome Project (http://openconnecto.me)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# run_m2g.py
+# Created by Greg Kiar and Will Gray Roncal on 2016-01-27.
+# Email: gkiar@jhu.edu, wgr@jhu.edu
+
+from argparse import ArgumentParser
+
+
+def startup():
+    pass
+
+
+def main():
+    parser = ArgumentParser(description="This is an end-to-end connectome
+                            estimation pipeline from sMRI and DTI images")
+    parser.add_argument("dti", action="store", help="Nifti DTI image stack")
+    parser.add_argument("bval", action="store", help="DTI scanner b-values")
+    parser.add_argument("bvec", action="store", help="DTI scanner b-vectors")
+    parser.add_argument("mprage", action="store", help="Nifti T1 MRI image")
+    parser.add_argument("atlas", action="store", help="Nifti T1 MRI atlas")
+    parser.add_argument("mask", action="store", help="Nifti binary mask of
+                        brain space in the atlas")
+    parser.add_argument("labels", action="store", help="Nifti labels of regions
+                        in atlas space")
+    result = parser.parse_args()
+
+    startup(result.dti, result.bval, result.bvec, result.mprage, result.atlas,
+            result.mask, result.labels)
+
+
+if __name__ == "__main__":
+    main()
