4212e50751d1cfe6d88866f5b4fa410fdb09cccf
gkiar <gkiar07@gmail.com>
2016-07-08 17:18:12 -0400

    reduced complexity of code/modularized functionsgpl

--- ndmg/utils/mri_autoingest.py
+++ ndmg/utils/mri_autoingest.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 import ndio.remote.ndingest as NI
+# import mni_dataset
+
 
 def main():
 
@@ -22,16 +24,16 @@ def main():
     Edit the below values, type and default information can be found on the ingesting page of the ndio docs page.
     """
 
-    dataset_name='MNI2'        #(type=str, help='Name of Dataset')
-    imagesize=(218,182,182)           #(type=int[], help='Image size (X,Y,Z)')
+    dataset_name='MNI3'        #(type=str, help='Name of Dataset')
+    imagesize=(182,218,182)           #(type=int[], help='Image size (X,Y,Z)')
     voxelres=(1000000,1000000,1000000)   #(type=float[], help='Voxel scale (X,Y,Z)')
     offset=(0,0,0)              #(type=int[], default=[0, 0, 0], help='Image Offset in X,Y,Z')
     timerange=(0,33)           #(type=int[], default=[0, 0], help='Time Dimensions')
     scalinglevels=0       #(type=int, default=0, help='Required Scaling levels/ Zoom out levels')
     scaling=0             #(type=int, default=0, help='Type of Scaling - Isotropic or Normal')
 
-    project_name='KKI2009vmicro'        #(type=str, help='Name of Project. Has to be unique in OCP. User Defined')
-    token_name='KKI2009vmicro'          #(type=str, default='', help='Token Name. User Defined')
+    project_name='KKI2009_gk2'        #(type=str, help='Name of Project. Has to be unique in OCP. User Defined')
+    token_name='KKI2009_gk2'          #(type=str, default='', help='Token Name. User Defined')
     channel_name='815_1'        #(type=str, help='Name of Channel. Has to be unique in the same project. User Defined.')
     datatype='uint8'            #(type=str, help='Channel Datatype')
     channel_type='timeseries'        #(type=enum, help='Type of channel - image, annotation, timeseries, probmap')
@@ -39,7 +41,7 @@ def main():
     resolution=0          #(type=int, default=0, help='Start Resolution')
     windowrange=(0,0)         #(type=int[], default=[0, 0], help='Window clamp function for 16-bit channels with low max value of pixels')
     readonly=0            #(type=int, default=0, help='Read-only Channel or Not. You can remotely post to channel if it is not readonly and overwrite data')
-    data_url= 'http://openconnecto.me/mrdata/share/ingest/'           #(type=str, help='This url points to the root directory of the files. Dropbox is not an acceptable HTTP Server.')
+    data_url= 'http://openconnecto.me/mrdata/share/ingest'           #(type=str, help='This url points to the root directory of the files. Dropbox is not an acceptable HTTP Server.')
     file_format='SLICE'         #(type=str, help='This is overal the file format type. For now we support only Slice stacks and CATMAID tiles.')
     file_type='png'           #(type=str, help='This is the specific file format type (tiff, tif, png))
 
@@ -57,20 +59,8 @@ def main():
     ni.add_channel(channel_name, datatype, channel_type, data_url, file_format, file_type, exceptions,
             resolution, windowrange, readonly)
 
-    """
-    If you wish to add additional channels to the object, simply call the
-    add_channel function for as many channels as you have
-    """
-
     #Adds metada
     ni.add_metadata(metadata)
-
-    """
-    EDIT ABOVE HERE
-    """
-
-    #Uncomment this line if you wish to get a json file names file_name
-    #ni.output_json("ocp.json")
 
     #Post the data
     ni.post_data()
