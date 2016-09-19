fb6cddde32d96007f04a5be7d42d9cbcb51e0a9d
ebridge2 <ebridge2@jhu.edu>
2016-09-19 00:19:03 -0400

    added latest setup modifications

--- ndmg/__init__.py
+++ ndmg/__init__.py
@@ -7,7 +7,6 @@ from .track.track import track as track
 from .stats import *
 from .stats.fmri_qc import fmri_qc as fmri_qc
 # from .preproc.preproc import preproc as preproc
-from .preproc.preproc import preproc as preproc
 from .utils.utils import utils as utils
 from .scripts import ndmg_pipeline as ndmg_pipeline
 from .scripts import fngs_pipeline as fngs_pipeline
