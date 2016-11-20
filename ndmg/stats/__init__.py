156d2ce380077f20fa335fdf814c731249f66266
Eric Bridgeford <ebridge2@jhu.edu>
2016-11-20 07:10:27 -0500

    updated init for stats

--- ndmg/stats/__init__.py
+++ ndmg/stats/__init__.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
 # Prevent typing multilevel imports
 from . import *
+from .fmri_qc import fmri_qc
+from .qc import qc
 from .plot_metrics import plot_metrics
