49c371039b6b20790e1887cd4b0e62129bc97a45
Eric Bridgeford <ebridge2@jhu.edu>
2017-08-14 02:54:23 -0400

    added __init__ from fmri branch

--- ndmg/__init__.py
+++ ndmg/__init__.py
@@ -2,12 +2,16 @@ from . import *
 
 # so we don't have to type ndg.graph.graph(), etc., to get the classes
 from .utils import utils as utils
-from .utils import bids_utils as bids_utils
 from .graph import graph, biggraph
 from .register.register import register as register
+from .register.register import epi_register as epi_register
+from .stats.qa_func import qa_func as qa_func
+from .stats.group_func import group_func as group_func
 from .track.track import track as track
 from .stats import *
 # from .preproc.preproc import preproc as preproc
+from .timeseries import timeseries as timeseries
 from .scripts import ndmg_dwi_pipeline as ndmg_dwi_pipeline
+from .scripts import ndmg_func_pipeline as ndmg_func_pipeline
 
 version = "0.0.50"
