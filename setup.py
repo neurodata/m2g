2dec62d31934c5c5cf0f6f82d4d04e3ec7877c38
eric bridgeford <ebridge2@jhu.edu>
2018-05-02 17:22:24 -0400

    updated version to 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ ext_modules = cythonize(Extension("ndmg.graph.zindex", # the extension name
                                   sources=["ndmg/graph/zindex.pyx"],
                                   include_dirs=['.'],
                                   language="c"))
-VERSION="0.0.50"
+VERSION="0.2.0"
 
 setup(
     name='ndmg',
