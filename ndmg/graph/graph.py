82dce5ce6102481a988e9434a209c02adc243790
Eric Bridgeford <ebridge2@jhu.edu>
2018-11-26 05:42:52 -0500

    updated header for func

--- ndmg/graph/graph.py
+++ ndmg/graph/graph.py
@@ -162,7 +162,8 @@ class graph(object):
             nx.write_weighted_edgelist(self.g, graphname, delimiter=",")
 
         elif self.modal == 'func':
-            np.savetxt(graphname, self.g, header=','.join(self.n_ids), delimiter=',')
+            np.savetxt(graphname, self.g,
+                header=','.join([str(n) for n in self.n_ids]), delimiter=',')
         else:
             raise ValueError("Unsupported Modality.")
         pass
