2e882be4191bde2a57d513d3c7bab601055411bf
Ganesh Arvapalli <garvapa1@jhu.edu>
2018-12-03 18:31:12 -0500

    Attempted to fix plotting issues

--- ndmg/stats/qa_graphs_plotting.py
+++ ndmg/stats/qa_graphs_plotting.py
@@ -32,25 +32,28 @@ def make_panel_plot(basepath, outf, dataset=None, atlas=None, minimal=True,
     fnames = [name for name in os.listdir(basepath)
               if os.path.splitext(name)[1] == '.pkl']
     fnames = sorted(fnames)
-    paths = [os.path.join(basepath, item) for item in fnames]
-    keys = ["_".join(n.split('.')[0].split('_')[1:]) for n in fnames]
-    labs = ['Betweenness Centrality', 'Clustering Coefficient', 'Degree',
-            'Edge Weight', 'Eigenvalue', 'Locality Statistic-1',
-            'Number of Non-zeros', 'Mean Connectome']
+    store = {}
+    for item in fnames:
+        store[item] = {'path': os.path.join(basepath, item),
+                       'key': "_".join(item.split('.')[0].split('_')[1:]),
+                       'label': " ".join(item.split('.')[0].split('_')[1:]).title()
+                       }
 
     traces = list(())
-    for idx, curr in enumerate(paths):
-        f = open(curr)
-        dat = pickle.load(f)[keys[idx]]
+    # import pdb; pdb.set_trace()
+    for idx, curr in enumerate(store.values()):
+        f = open(curr['path'])
+        dat = pickle.load(f)[curr['key']]
         f.close()
-        if keys[idx] == 'number_non_zeros':
+        if curr['key'] == 'number_non_zeros':
             fig = pp.plot_rugdensity(dat.values())
-        elif keys[idx] == 'edge_weight':
+        elif curr['key'] == 'edge_weight':
             edges = np.max([len(dat[i]) for i in dat.keys()])
             fig = pp.plot_series(dat.values(), sort=True)
-        elif keys[idx] == 'degree_distribution':
+        elif curr['key'] == 'degree_distribution':
             fig = pp.plot_degrees(dat, hemi=hemispheres)
             if hemispheres:
+                dims = len(dat.values()[0])
                 anno = [dict(x=dims/3,
                              y=4*dims/7,
                              xref='x3',
@@ -67,49 +70,47 @@ def make_panel_plot(basepath, outf, dataset=None, atlas=None, minimal=True,
                              showarrow=False,
                              font=dict(color='rgba(0.11,0.62,0.47,0.6)',
                                        size=14))]
-        elif keys[idx] == 'study_mean_connectome':
+        elif curr['key'] == 'study_mean_connectome':
             if log:
                 dat = np.log10(dat+1)
-            fig = pp.plot_heatmap(dat, name=labs[idx])
+            fig = pp.plot_heatmap(dat, name=curr['label'])
         else:
             dims = len(dat.values()[0])
+            # layout = pp.std_layout(ylab=curr['label'], xlab='Graph')
             fig = pp.plot_series(dat.values())
+            fig['layout']['xaxis']['title'] = 'Node'
+            fig['layout']['yaxis']['title'] = curr['label']
+            # fig.layout = layout
         traces += [pp.fig_to_trace(fig)]
 
     multi = pp.traces_to_panels(traces)
-    for idx, curr, in enumerate(paths):
+    for idx, curr, in enumerate(store.values()):
         key = 'axis%d' % (idx+1)
         d = multi.layout['x'+key]['domain']
         multi.layout['x'+key]['domain'] = [d[0], d[1]-0.0125]
         multi.layout['x'+key]['zeroline'] = False
         multi.layout['y'+key]['zeroline'] = False
-        multi.layout['y'+key]['title'] = labs[idx]
+        multi.layout['y'+key]['title'] = curr['label']
         multi.layout['x'+key]['title'] = 'Node'
         multi.layout['x'+key]['nticks'] = 3
         multi.layout['y'+key]['nticks'] = 3
-        if idx in [0, 1, 2, 3, 5]:
-            multi.layout['x'+key]['range'] = [1, dims]
-            multi.layout['x'+key]['tickvals'] = [1, dims/2, dims]
-            if idx in [2]:
-                if hemispheres:
-                    multi.layout['annotations'] = anno
-            elif log:
-                multi.layout['y'+key]['type'] = 'log'
-                multi.layout['y'+key]['title'] += ' (log scale)'
-        if idx in [3]:
+        # Edge weight
+        if curr['key'] in ['edge_weight']:
             multi.layout['x'+key]['range'] = [1, edges]
             multi.layout['x'+key]['tickvals'] = [1, edges/2, edges]
             multi.layout['x'+key]['title'] = 'Edge'
-        if idx in [4]:
+        # Eigenvalue
+        elif curr['key'] in ['eigen_sequence']:
             multi.layout['x'+key]['range'] = [1, dims]
             multi.layout['x'+key]['tickvals'] = [1, dims/2, dims]
             multi.layout['x'+key]['title'] = 'Dimension'
-        if idx in [6]:
-            multi.layout['y'+key]['title'] = 'Relative Probability'
-            multi.layout['x'+key]['title'] = labs[idx]
-        if idx in [7]:
+        # Number of Non-zeros
+        elif curr['key'] in ['number_non_zeros']:
+            multi.layout['x'+key]['title'] = curr['label']
+        # Mean Connectome
+        elif curr['key'] in ['study_mean_connectome']:
             multi.layout['y'+key]['title'] = None
-            multi.layout['x'+key]['title'] = labs[idx]
+            multi.layout['x'+key]['title'] = curr['label']
             multi.layout['y'+key]['autorange'] = 'reversed'
             multi.layout['x'+key]['tickvals'] = [0, dims/2-1, dims-1]
             multi.layout['y'+key]['tickvals'] = [0, dims/2-1, dims-1]
@@ -117,6 +118,16 @@ def make_panel_plot(basepath, outf, dataset=None, atlas=None, minimal=True,
             multi.layout['y'+key]['ticktext'] = [1, dims/2, dims]
             if log:
                 multi.layout['x'+key]['title'] += ' (log10)'
+        else:
+            multi.layout['x'+key]['range'] = [1, dims]
+            multi.layout['x'+key]['tickvals'] = [1, dims/2, dims]
+            # Degree
+            if curr['key'] in ['degree_distribution']:
+                if hemispheres:
+                    multi.layout['annotations'] = anno
+            elif log:
+                multi.layout['y'+key]['type'] = 'log'
+                multi.layout['y'+key]['title'] += ' (log scale)'        
 
     if dataset is not None and atlas is not None:
         if atlas == 'desikan':
