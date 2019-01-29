a1ae3f733500cffab74ba5b2a0410245b0306973
Eric Bridgeford <ebridge2@jhu.edu>
2019-01-29 17:10:52 -0500

    Update __init__.py

--- ndmg/__init__.py
+++ ndmg/__init__.py
@@ -1,6 +1,7 @@
 from . import *
 
 # so we don't have to type ndg.graph.graph(), etc., to get the classes
+from .utils import utils as utils
 from .graph import graph, biggraph
 from .register.register import dmri_reg as register
 from .register.register import epi_register as epi_register
