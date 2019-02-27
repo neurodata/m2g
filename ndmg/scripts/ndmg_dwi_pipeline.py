9fded1f58c07fdb0d6c6024c1eb4ddfe5399bae6
dPys <dpisner@utexas.edu>
2019-02-27 21:59:30 +0000

    add --super flag to apply warp

--- ndmg/scripts/ndmg_dwi_pipeline.py
+++ ndmg/scripts/ndmg_dwi_pipeline.py
@@ -206,11 +206,11 @@ def ndmg_dwi_worker(dwi, bvals, bvecs, t1w, atlas, mask, labels, outdir,
         if track_type == 'eudx':
 	    #seeds = int(1000000)
 	    seeds_wm_gm_int = mgt.build_seed_list(reg.wm_gm_int_in_dwi, stream_affine, dens=3)
-	    seeds_wm = mgt.build_seed_list(reg.wm_in_dwi_bin, stream_affine, dens=1)
+	    seeds_wm = mgt.build_seed_list(reg.wm_in_dwi_bin, stream_affine, dens=2)
 	    seeds = np.vstack((seeds_wm_gm_int, seeds_wm))
         else:
             seeds_wm_gm_int = mgt.build_seed_list(reg.wm_gm_int_in_dwi, stream_affine, dens=3)
-	    seeds_wm = mgt.build_seed_list(reg.wm_in_dwi_bin, stream_affine, dens=1)
+	    seeds_wm = mgt.build_seed_list(reg.wm_in_dwi_bin, stream_affine, dens=2)
 	    seeds = np.vstack((seeds_wm_gm_int, seeds_wm))
 	print('Using ' + str(len(seeds)) + ' seeds...')
 
