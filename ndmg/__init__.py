9809ad5b808131f7d9576c825887b7724bf7d1c3
dPys <dpisner@utexas.edu>
2019-03-01 03:38:26 +0000

    tweak atlas reg

--- ndmg/__init__.py
+++ ndmg/__init__.py
@@ -5,7 +5,7 @@ warnings.simplefilter("ignore")
 # so we don't have to type ndg.graph.graph(), etc., to get the classes
 #from .graph import graph, biggraph
 from .register.gen_reg import dmri_reg as register
-#from .register.gen_reg import epi_register as epi_register
+from .register.gen_reg import epi_register as epi_register
 #from .stats.qa_mri import qa_mri as qa_mri
 #from .stats.group_func import group_func as group_func
 from .track.gen_track import run_track as track
