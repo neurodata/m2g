fba29c2fd1879b81c5f25258d8cd202a8f25f09a
dPys <dpisner@utexas.edu>
2019-04-10 02:33:40 +0000

    tweaks and DSN

--- ndmg/scripts/ndmg_dwi_pipeline.py
+++ ndmg/scripts/ndmg_dwi_pipeline.py
@@ -45,7 +45,7 @@ from ndmg.utils.bids_utils import name_resource
 import sys
 from dipy.tracking.streamline import Streamlines
 from dipy.tracking.utils import move_streamlines
-
+from nilearn.image import new_img_like
 os.environ["MPLCONFIGDIR"] = "/tmp/"
 
 
@@ -164,44 +164,44 @@ def ndmg_dwi_worker(dwi, bvals, bvecs, t1w, atlas, mask, labels, outdir,
     hdr = dwi_prep_img.header
     print("%s%s%s" % ('Preprocessing runtime: ', str(np.round(time.time() - start_time, 1)), 's'))
     # -------- Registration Steps ----------------------------------- #
-#    if len(os.listdir(namer.dirs['output']['prep_anat'])) != 0:
-#	print('Pre-existing preprocessed t1w files found. Deleting these...')
-#        shutil.rmtree(namer.dirs['output']['prep_anat'])
-#	os.mkdir(namer.dirs['output']['prep_anat'])
-#    if len(os.listdir(namer.dirs['output']['reg_anat'])) != 0:
-#	print('Pre-existing registered t1w files found. Deleting these...')
-#        shutil.rmtree(namer.dirs['output']['reg_anat'])
-#	os.mkdir(namer.dirs['output']['reg_anat'])
-#    if (len(os.listdir(namer.dirs['tmp']['reg_a'])) != 0) or (len(os.listdir(namer.dirs['tmp']['reg_m'])) != 0):
-#        print('Pre-existing temporary files found. Deleting these...')
-#        shutil.rmtree(namer.dirs['tmp']['reg_a'])
-#        os.mkdir(namer.dirs['tmp']['reg_a'])
-#        shutil.rmtree(namer.dirs['tmp']['reg_m'])
-#        os.mkdir(namer.dirs['tmp']['reg_m'])
+    if len(os.listdir(namer.dirs['output']['prep_anat'])) != 0:
+	print('Pre-existing preprocessed t1w files found. Deleting these...')
+        shutil.rmtree(namer.dirs['output']['prep_anat'])
+	os.mkdir(namer.dirs['output']['prep_anat'])
+    if len(os.listdir(namer.dirs['output']['reg_anat'])) != 0:
+	print('Pre-existing registered t1w files found. Deleting these...')
+        shutil.rmtree(namer.dirs['output']['reg_anat'])
+	os.mkdir(namer.dirs['output']['reg_anat'])
+    if (len(os.listdir(namer.dirs['tmp']['reg_a'])) != 0) or (len(os.listdir(namer.dirs['tmp']['reg_m'])) != 0):
+        print('Pre-existing temporary files found. Deleting these...')
+        shutil.rmtree(namer.dirs['tmp']['reg_a'])
+        os.mkdir(namer.dirs['tmp']['reg_a'])
+        shutil.rmtree(namer.dirs['tmp']['reg_m'])
+        os.mkdir(namer.dirs['tmp']['reg_m'])
 
     # Check orientation (t1w)
-#    start_time = time.time()
-#    t1w = mgu.reorient_t1w(t1w, namer)
-#    print("%s%s%s" % ('Reorienting runtime: ', str(np.round(time.time() - start_time, 1)), 's'))
+    start_time = time.time()
+    t1w = mgu.reorient_t1w(t1w, namer)
+    print("%s%s%s" % ('Reorienting runtime: ', str(np.round(time.time() - start_time, 1)), 's'))
 
     if reg_style == 'native':
 	print('Running tractography in native space...')
        # Instantiate registration
         reg = mgr.dmri_reg(namer, nodif_B0, nodif_B0_mask, t1w, vox_size, simple=False)
         # Perform anatomical segmentation
-#        start_time = time.time()
-#        reg.gen_tissue()
-#        print("%s%s%s" % ('gen_tissue runtime: ', str(np.round(time.time() - start_time, 1)), 's'))
+        start_time = time.time()
+        reg.gen_tissue()
+        print("%s%s%s" % ('gen_tissue runtime: ', str(np.round(time.time() - start_time, 1)), 's'))
 
         # Align t1w to dwi
-#        start_time = time.time()
-#        reg.t1w2dwi_align()
-#        print("%s%s%s" % ('t1w2dwi_align runtime: ', str(np.round(time.time() - start_time, 1)), 's'))
+        start_time = time.time()
+        reg.t1w2dwi_align()
+        print("%s%s%s" % ('t1w2dwi_align runtime: ', str(np.round(time.time() - start_time, 1)), 's'))
 
         # Align tissue classifiers
-#        start_time = time.time()
-#        reg.tissue2dwi_align()
-#        print("%s%s%s" % ('tissue2dwi_align runtime: ', str(np.round(time.time() - start_time, 1)), 's'))
+        start_time = time.time()
+        reg.tissue2dwi_align()
+        print("%s%s%s" % ('tissue2dwi_align runtime: ', str(np.round(time.time() - start_time, 1)), 's'))
 
         # -------- Tensor Fitting and Fiber Tractography ---------------- #
         if track_type == 'eudx':
@@ -226,11 +226,11 @@ def ndmg_dwi_worker(dwi, bvals, bvecs, t1w, atlas, mask, labels, outdir,
             return streams
 
 	streamlines = Streamlines([sl for sl in streamlines if len(sl) > 60])
-	tract_affine = np.eye(4)*np.array([-1,1,1,1])
+        tract_affine = np.eye(4)*np.array([1,1,1,1])
+	#tract_affine = np.eye(4)*np.array([-1,1,1,1])
 	trk_affine = np.eye(4)*np.array([1,1,1,1])
         iso_affine = np.eye(4)*np.array([-zoom_set[0],zoom_set[1],zoom_set[2],1])
 	B0_img = nib.load(nodif_B0)
-	from nilearn.image import new_img_like
 	B0_iso_img = new_img_like(B0_img, B0_img.get_data(), affine=iso_affine)
 	nib.save(B0_iso_img, nodif_B0_iso_path)
 
