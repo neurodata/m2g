9604ab356b9bc5e9e4814ee6c1cfdf56e44cda4e
loftusa <alexloftus2004@gmail.com>
2019-08-01 19:34:02 -0400

    self.rois

--- ndmg/graph/gen_graph.py
+++ ndmg/graph/gen_graph.py
@@ -167,7 +167,7 @@ class graph_tools(object):
         mx = len(np.unique(self.rois.astype(np.int64))) - 1
         self.g = nx.Graph(ecount=0, vcount=mx)
         edge_dict = defaultdict(int)
-        node_dict = dict(zip(np.unique(rois) + 1, np.arange(mx) + 1))
+        node_dict = dict(zip(np.unique(self.rois) + 1, np.arange(mx) + 1))
 
         # Add empty vertices
         for node in range(1, mx+1):
