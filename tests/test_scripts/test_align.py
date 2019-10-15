ef02bb24accac2275c58d303d1c5830f93330b03
FangCai-fifi <54511589+FangCai-fifi@users.noreply.github.com>
2019-10-15 16:43:38 -0400

    changes

--- tests/test_scripts/test_align.py
+++ tests/test_scripts/test_align.py
@@ -1,6 +1,6 @@
 import warnings
-import ndmg
 warnings.simplefilter("ignore")
+import ndmg
 from ndmg.utils import gen_utils as mgu
 from ndmg.utils import reg_utils as mgr
 import nibabel as nib
@@ -10,38 +10,32 @@ import os
 import os.path as op
 import pytest
 
-input_dir = r"../test_data/inputs/sub-0025864_ses-1_T1w.nii.gz"
-ref_dir = r"../test_data/inputs/MNI152_T1_2mm_brain"
-# out_dir = r"/mnt/e/."
-
 def test_align(tmp_path):
     d = tmp_path / "sub"
     d.mkdir()
     temp_out1 = d / "omat.data"
     temp_out2 = d / "outnii.nii.gz"
-
-    inp = input_dir
-    ref = ref_dir
+    
+    # set input/ouput data paths
+    align_in_path = r"../test_data/inputs/align/sub-0025864_ses-1_T1w.nii.gz"
+    ref_in_path = r"../test_data/inputs/align/MNI152_T1_2mm_brain.nii.gz"
+    omat_out_cntrl_path = r"../test_data/outputs/align/omat.mat"
+    
+    # call function
+    inp = align_in_path
+    ref = ref_in_path
     xfm = temp_out1
     out = temp_out2
-
     mgr.align(inp, ref, xfm, out)
-    # result_nii = nib.load(str(out))
-    result_mat = np.loadtxt(str(xfm))
-    result_mat = np.array(result_mat)
-
-    ''' output_mat = np.array([[0.8271407155,  -0.04727642977,  0.006816218756,  15.99814178],
-                           [-0.005617838577,  0.8770838128,  -0.000978902222,  -0.3486071619],
-                           [-0.006664295282,  -0.04974933234,  0.8076537939,  -18.14272133],
-                           [0,  0,  0,  1]])  '''
-
-    output_mat = np.array([[ 0.8271407155, -0.04727642977, 0.006816218756, 15.99814178],
-    [ -0.005617838577, 0.8770838128, -0.000978902222, -0.3486071619],
-    [ -0.006664295282, -0.04974933234, 0.8076537939, -18.14272133],
-    [ 0., 0., 0., 1.]])
-
-      
-    assert np.allclose(result_mat[3,:], output_mat[3,:]) 
+    
+    # load function outputs
+    omat_out_temp = np.loadtxt(str(xfm))
+    
+    # load output data
+    omat_out_cntrl = np.loadtxt(str(omat_out_cntrl_path))
+    
+    # assert
+    assert np.allclose(omat_out_temp, omat_out_cntrl) 
 
 
     
--- tests/test_scripts/test_align_nonlinear.py
+++ tests/test_scripts/test_align_nonlinear.py
@@ -1,6 +1,6 @@
 import warnings
-import ndmg
 warnings.simplefilter("ignore")
+import ndmg
 from ndmg.utils import gen_utils as mgu
 from ndmg.utils import reg_utils as mgr
 import nibabel as nib
@@ -10,33 +10,30 @@ import os
 import os.path as op
 import pytest
 
-input_dir = r"../test_data/inputs/t1w_brain_nores.nii.gz"
-ref_dir = r"../test_data/ref_inputs/MNI152_T1_2mm_brain.nii.gz"
-xfm_dir = r"../test_data/temp_outputs/xfm_t1w2mni_init.mat"
-# input_dir = '../ndmg_outputs/anat/preproc/t1w_brain_nores.nii.gz'
-# ref_dir = '/usr/local/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz'
-# xfm_dir = '../ndmg_outputs/tmp/reg_m/xfm_t1w2mni_init.mat'
-
 def test_align_nonlinear(tmp_path):
     d = tmp_path / "sub"
     d.mkdir()
     temp_out1 = d / "outimage.nii.gz"
     temp_out2 = d / "outcoefficients.nii.gz"
 
-    inp = input_dir
-    ref = ref_dir
-    xfm = xfm_dir
+    # set input data
+    alignnonlinear_in_path = r"../test_data/inputs/alignnonlinear/t1w_brain_nores.nii.gz"
+    ref_in_path = r"../test_data/inputs/alignnonlinear/MNI152_T1_2mm_brain.nii.gz"
+    xfm_in_path = r"../test_data/inputs/alignnonlinear/xfm_t1w2mni_init.mat"
+    alignnonlinear_out_cntrl_path = r"../test_data/outputs/alignnonlinear/outcoefficients.nii.gz"
+
+    # call function
+    inp = alignnonlinear_in_path
+    ref = ref_in_path
+    xfm = xfm_in_path
     out = temp_out1
     warp = temp_out2
-
     mgr.align_nonlinear(inp, ref, xfm, out, warp)
-    img = nib.load(str(warp))
-    img_1 = img.get_fdata()
-    result_warp = np.array(img_1)
-    # result_out = np.array(nib.load(out))
-    np.set_printoptions(precision=8)
-    
-    out_warp = result_warp.shape
-    ref_warp = (25, 30, 25, 3)
 
-    assert out_warp == ref_warp
+    # load function data
+    alignnonlinear_out_temp = nib.load(str(warp)).get_fdata()
+    
+    # load output data
+    alignnonlinear_out_cntrl = nib.load(str(alignnonlinear_out_cntrl_path)).get_fdata()
+
+    assert np.allclose(alignnonlinear_out_cntrl, alignnonlinear_out_temp)
