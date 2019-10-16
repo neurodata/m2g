39c6b91d21e11b4050d4071c68f8532a26da8ab7
jialin kang <kjlyayst@163.com>
2019-10-16 19:20:18 -0400

    code review segment_t1w

--- tests/kjl_testcode/test_segment_t1w.py
+++ tests/kjl_testcode/test_segment_t1w.py
@@ -1,13 +1,31 @@
 from ndmg.utils.reg_utils import segment_t1w
 import pytest
+import nibabel as nib
+import numpy as np
 
-
-t1w = '../ndmg_outputs/anat/preproc/t1w_brain.nii.gz'
-basename = '../ndmg_outputs/anat/preproc/t1w_seg'
-
+t1w = '../test_data/inputs/segment_t1w/t1w_brain.nii.gz'
 
 def test_segment_t1w(tmp_path):
-    result = segment_t1w(t1w, basename, opts='')
-    assert result == {'csf_prob': '../ndmg_outputs/anat/preproc/t1w_seg_pve_0.nii.gz',
-                      'gm_prob': '../ndmg_outputs/anat/preproc/t1w_seg_pve_1.nii.gz',
-                      'wm_prob': '../ndmg_outputs/anat/preproc/t1w_seg_pve_2.nii.gz'}
+    # make a temporary path
+    d = tmp_path / "sub"
+    d.mkdir()
+    d_path = d / "t1w_seg"
+
+    # this is the result when run segment_t1w, and store in the tem_path
+    result = segment_t1w(t1w, d_path, opts='')
+    output1_data = nib.load(result['csf_prob'])
+    output1_data = output1_data.get_fdata()
+    output2_data = nib.load(result['gm_prob'])
+    output2_data = output2_data.get_fdata()
+    output3_data = nib.load(result['wm_prob'])
+    output3_data = output3_data.get_fdata()
+
+    # this is the result load from the test_data which run the pipeline produce
+    test_data1 = nib.load('../test_data/outputs/segment_t1w/t1w_seg_pve_0.nii.gz').get_fdata()
+    test_data2 = nib.load('../test_data/outputs/segment_t1w/t1w_seg_pve_1.nii.gz').get_fdata()
+    test_data3 = nib.load('../test_data/outputs/segment_t1w/t1w_seg_pve_2.nii.gz').get_fdata()
+
+    assert np.allclose(output1_data, test_data1)
+    assert np.allclose(output2_data, test_data2)
+    assert np.allclose(output3_data, test_data3)
+
