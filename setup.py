332e1314bc60175bff899f955cdb47367d55de9f
loftusa <alexloftus2004@gmail.com>
2019-10-24 14:35:16 -0400

    revert setup, decided the version I made was too weird

--- setup.py
+++ setup.py
@@ -18,69 +18,48 @@ See our README for more details on package installation : https://github.com/neu
 from setuptools import setup, find_packages
 from ndmg import __version__
 
-# initial setup
-kwargs = {}
-
-# add metadata
-kwargs.update(
-    dict(
-        name="ndmg",
-        # version=__version__,
-        description="Neuro Data MRI to Graphs Pipeline",
-        author="Derek Pisner, Alex Loftus, Greg Kiar, Eric Bridgeford, and Will Gray Roncal",
-        author_email="dpisner@utexas.edu, aloftus2@jhu.edu, gkiar@jhu.edu, wgr@jhu.edu, ebridge2@jhu.edu",
-        url="https://github.com/neurodata/ndmg",
-        download_url="https://github.com/neurodata/ndmg/tarball/" + __version__,
-        keywords=["connectome", "mri", "pipeline"],
-        classifiers=["Programming Language :: Python :: 3.6"],
-    )
-)
-
-# add utility info
-kwargs.update(
-    dict(
-        # packages=find_packages(),
-        package_data={"templates": ["*.json"]},
-        include_package_data=False,  # only include the ndmg_cloud template jsons
-        entry_points={
-            "console_scripts": [
-                "ndmg=ndmg.scripts.ndmg_bids:main",
-                "ndmg_dwi_pipeline=ndmg.scripts.ndmg_dwi_pipeline:main",
-                "ndmg_cloud=ndmg.scripts.ndmg_cloud:main",
-                "ndmg_bids=ndmg.scripts.ndmg_bids:main",  # for backwards compatibility
-            ]
-        },
-        python_requires=">=3.6",
-    )
-)
-
-# add requirements
-kwargs.update(
-    dict(
-        install_requires=[
-            "nibabel",
-            "numpy",
-            "dipy",
-            "scipy",
-            # "python-dateutil",
-            "boto3",
-            "awscli",
-            "matplotlib",
-            "nilearn",
-            # "sklearn",
-            "vtk",
-            "pyvtk",
-            "fury",
-            "requests",
-            "plotly",
-            "pybids",
-            "scikit-image",
-            "networkx",
-            "configparser>=3.7.4",
-            "pytest",
+setup(
+    name="ndmg",
+    packages=["ndmg", "ndmg.stats", "ndmg.utils", "ndmg.scripts"],
+    include_package_data=True,
+    version=__version__,
+    entry_points={
+        "console_scripts": [
+            "ndmg_bids=ndmg.scripts.ndmg_bids:main",  # for backwards compatibility
+            "ndmg=ndmg.scripts.ndmg_bids:main",
+            "ndmg_dwi_pipeline=ndmg.scripts.ndmg_dwi_pipeline:main",
+            "ndmg_cloud=ndmg.scripts.ndmg_cloud:main",
         ]
-    )
+    },
+    description="Neuro Data MRI to Graphs Pipeline",
+    author="Derek Pisner, Alex Loftus, Greg Kiar, Eric Bridgeford, and Will Gray Roncal",
+    author_email="dpisner@utexas.edu, aloftus2@jhu.edu, gkiar@jhu.edu, wgr@jhu.edu, ebridge2@jhu.edu",
+    url="https://github.com/neurodata/ndmg",
+    download_url="https://github.com/neurodata/ndmg/tarball/" + __version__,
+    keywords=["connectome", "mri", "pipeline"],
+    classifiers=["Programming Language :: Python :: 3.6"],
+    install_requires=[
+        "numpy",
+        "nibabel",
+        "dipy",
+        "scipy",
+        "python-dateutil",
+        "pandas",
+        "boto3",
+        "awscli",
+        "matplotlib",
+        "nilearn",
+        "sklearn",
+        "vtk",
+        "pyvtk",
+        "fury",
+        "requests",
+        "plotly",
+        "pybids",
+        "setuptools>=40.0",
+        "scikit-image",
+        "networkx",
+        "configparser>=3.7.4",
+        "pytest",
+    ],
 )
-
-# run setup
-setup(**kwargs)
