d674dc080920c7fb8854675a26fd29e26a96d634
Lawreros <lawreros@gmail.com>
2020-02-04 16:26:42 -0500

    fix _init_ merge conflict

--- m2g/stats/__init__.py
+++ m2g/stats/__init__.py
@@ -1,22 +1,10 @@
 """
-ndmg.scripts
+m2g.stats
 ~~~~~~~~~~~~
+"""
 
-<<<<<<< HEAD:m2g/stats/__init__.py
 warnings.simplefilter("ignore")
 # Prevent typing multilevel imports
 
 # TODO: fix below
 from . import *
-=======
-Contains top-level, self-contained scripts.
-
-ndmg_bids : top-level pipeline entrypoint
-ndmg_dwi_pipeline : the pipeline itself
-ndmg_cloud : for performing batch processing on AWS
-"""
-
-from . import ndmg_bids
-from . import ndmg_cloud
-from . import ndmg_dwi_pipeline
->>>>>>> deploy:ndmg/scripts/__init__.py
