1e7255ddcade4380a8d37efe35b83380ccef9e1e
Ross Lawrence <52179159+Lawreros@users.noreply.github.com>
2020-04-08 22:02:32 -0400

    fix cpac terminal call typo

--- m2g/functional/m2g_func.py
+++ m2g/functional/m2g_func.py
@@ -53,7 +53,7 @@ def make_script(input_dir, output_dir, subject, session, data_config, pipeline_c
     with open(cpac_script,'w+',encoding='utf8') as script:
         script.write(f'''#! /bin/bash
         . /venv/bin/activate
-        python /code/run.py --data_config_file {data_config} --pipeline_file {pipeline_config} --n_cpus {n_cpus}--mem_gb {mem_gb} {input_dir} {output_dir} participant
+        python /code/run.py --data_config_file {data_config} --pipeline_file {pipeline_config} --n_cpus {n_cpus} --mem_gb {mem_gb} {input_dir} {output_dir} participant
         ''')
     
     run(f'chmod +x {cpac_script}')
